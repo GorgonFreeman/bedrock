771a8478cd8888f0ddf15329d003371a7308f922
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-23 17:44:01 +1000

    printifyShopsGet complete function

--- printify/printifyShopsGet.js
+++ printify/printifyShopsGet.js
@@ -1,40 +1,38 @@
 const { respond, mandateParam, logDeep } = require('../utils');
-const { printifyClient } = require('../printify/printify.utils');
+const { printifyGet } = require('../printify/printify.utils');
 
 const printifyShopsGet = async (
-  arg,
   {
     credsPath,
-    option,
   } = {},
 ) => {
 
-  const response = await printifyClient.fetch({
-    url: '/things.json', 
-    verbose: true,
-    credsPath,
-  });
+  const response = await printifyGet(
+    '/shops.json', 
+    {
+      verbose: true,
+      credsPath,
+    },
+  );
 
   logDeep(response);
   return response;
-  
+
 };
 
 const printifyShopsGetApi = async (req, res) => {
   const { 
-    arg,
     options,
   } = req.body;
 
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'arg', arg),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
+  // const paramsValid = await Promise.all([
+  //   mandateParam(res, 'arg', arg),
+  // ]);
+  // if (paramsValid.some(valid => valid === false)) {
+  //   return;
+  // }
 
   const result = await printifyShopsGet(
-    arg,
     options,
   );
   respond(res, 200, result);
@@ -45,4 +43,4 @@ module.exports = {
   printifyShopsGetApi,
 };
 
-// curl localhost:8000/printifyShopsGet -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/printifyShopsGet
