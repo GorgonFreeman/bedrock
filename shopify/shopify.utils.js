8016bd28c18778aeabbf08bf5e8fbf76280a04be
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-23 21:42:44 +1000

    shopifyRequestSetup platform utility

---
+++ shopify/shopify.utils.js
@@ -0,0 +1,27 @@
+const { credsByPath } = require('../utils');
+
+const shopifyRequestSetup = ({ 
+  credsPath,
+  apiVersion = '2025-10', 
+} = {}) => {
+  // returns { baseUrl, headers }
+  
+  const creds = credsByPath(['shopify', credsPath]);
+
+  const { STORE_URL, SHOPIFY_API_KEY } = creds;
+
+  const baseUrl = `https://${ STORE_URL }.myshopify.com/admin/api/${ apiVersion }/graphql.json`;
+
+  const headers = {
+    'X-Shopify-Access-Token': SHOPIFY_API_KEY,
+  };
+
+  return { 
+    baseUrl,
+    headers,
+  }; 
+};
+
+module.exports = {
+  shopifyRequestSetup,
+};
