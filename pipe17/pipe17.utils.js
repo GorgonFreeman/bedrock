28a80338c862bdaf08a2ed96a221e3d4a7e6c79b
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-24 16:46:59 +1000

    speculative pipe17Getter

--- pipe17/pipe17.utils.js
+++ pipe17/pipe17.utils.js
@@ -1,4 +1,4 @@
-const { credsByPath, CustomAxiosClient, logDeep, askQuestion } = require('../utils');
+const { credsByPath, CustomAxiosClient, logDeep, askQuestion, Getter, getterAsGetFunction } = require('../utils');
 
 const pipe17RequestSetup = ({ credsPath } = {}) => {
 
@@ -27,6 +27,80 @@ const pipe17Client = new CustomAxiosClient({
   },
 });
 
+const pipe17GetterPaginator = async (customAxiosPayload, response) => {
+  logDeep('paginator: decide when done and make next payload', customAxiosPayload, response);
+  await askQuestion('?');
+
+  const { success, result } = response;
+  if (!success) { // Return if failed
+    return [true, null]; 
+  }
+
+  // 1. Extract necessary pagination info
+  // const { 
+  //   current_page: currentPage, 
+  //   last_page: lastPage,
+  // } = result;
+
+  // 2. Supplement payload with next pagination info
+  // const paginatedPayload = {
+  //   ...customAxiosPayload,
+  //   params: {
+  //     ...customAxiosPayload?.params,
+  //     page: currentPage + 1,
+  //   },
+  // };
+  
+  // 3. Logic to determine done
+  // const done = currentPage === lastPage;
+  
+  return [done, paginatedPayload];
+};
+
+const pipe17GetterDigester = async (response) => {
+  // logDeep('digester: get items from response', response);
+  // await askQuestion('?');
+
+  const { success, result } = response;
+  if (!success) { // Return if failed
+    return [true, null]; 
+  }
+
+  // const items = result?.data;
+  // return items;
+};
+
+const pipe17Getter = async (
+  url,
+  {
+    credsPath,
+    params,
+    ...getterOptions
+  } = {},
+) => {
+  return new Getter(
+    {
+      url,
+      payload: {
+        params,
+      },
+      paginator: pipe17GetterPaginator,
+      digester: pipe17GetterDigester,
+
+      client: pipe17Client,
+      clientArgs: {
+        factoryArgs: [credsPath],
+      },
+
+      ...getterOptions
+    },
+  );
+};
+
+const pipe17Get = getterAsGetFunction(pipe17Getter);
+
 module.exports = {
   pipe17Client,
+  pipe17Getter,
+  pipe17Get,
 };
