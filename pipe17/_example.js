076caeacfc7af49ee6beabbfff07d9564d53d62c
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-25 02:22:43 +1000

    pipe17 example function

---
+++ pipe17/_example.js
@@ -0,0 +1,53 @@
+const { respond, mandateParam, logDeep } = require('../utils');
+const { pipe17Client } = require('../pipe17/pipe17.utils');
+
+const FUNC = async (
+  receiptId,
+  {
+    credsPath,
+  } = {},
+) => {
+
+  const response = await pipe17Client.fetch({
+    url: `/receipts/${ receiptId }`,
+    factoryArgs: [credsPath],
+    interpreter: (response) => {
+      return {
+        ...response,
+        ...response.result ? {
+          result: response.result.receipt,
+        } : {},
+      };
+    },
+  });
+  
+  logDeep(response);
+  return response;
+};
+
+const FUNCApi = async (req, res) => {
+  const { 
+    receiptId,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'receiptId', receiptId),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await FUNC(
+    receiptId,
+    options,
+  );
+  respond(res, 200, result);
+};
+
+module.exports = {
+  FUNC,
+  FUNCApi,
+};
+
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "receiptId": "b9d03991a844e340" }'
