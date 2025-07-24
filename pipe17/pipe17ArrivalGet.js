1332c995624afd92387f8881f6bd6a3a59ecdf7b
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-25 02:47:52 +1000

    notes about identifier pattern

--- pipe17/pipe17ArrivalGet.js
+++ pipe17/pipe17ArrivalGet.js
@@ -4,6 +4,14 @@ const { respond, mandateParam, logDeep } = require('../utils');
 const { pipe17GetSingle } = require('../pipe17/pipe17GetSingle');
 
 const pipe17ArrivalGet = async (
+  /* 
+    This "identifier" pattern allows us to have a mandatory arg that can take multiple alternative forms.
+    A good example is fetching a customer by id, email, or phone number.
+    We pass an object that has at least one of the keys inside.
+    The validator for this object checks if it has any of the required keys.
+    In the function, we cascade through the keys and use the first one we find,
+    ideally with the most specific being at the top (ie. the id).
+  */
   {
     arrivalId,
     extArrivalId,
@@ -38,6 +46,7 @@ const pipe17ArrivalGetApi = async (req, res) => {
   } = req.body;
 
   const paramsValid = await Promise.all([
+    // This validator allows us to identify an arrival by various means, mandating at least one be provided.
     mandateParam(res, 'arrivalIdentifier', arrivalIdentifier, p => p.arrivalId || p.extArrivalId || p.extArrivalApiId || p.extReferenceId),
   ]);
   if (paramsValid.some(valid => valid === false)) {
