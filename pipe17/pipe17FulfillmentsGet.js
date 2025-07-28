cf3f2f007281e49f50cbe603ba4763298e5d5bb3
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-28 16:47:06 +1000

    pipe17FulfillmentsGet complete function

--- pipe17/pipe17FulfillmentsGet.js
+++ pipe17/pipe17FulfillmentsGet.js
@@ -1,45 +1,40 @@
 const { respond, mandateParam, logDeep } = require('../utils');
-const { pipe17Client } = require('../pipe17/pipe17.utils');
+const { pipe17Get } = require('../pipe17/pipe17.utils');
 
 const pipe17FulfillmentsGet = async (
-  receiptId,
   {
     credsPath,
+    // TODO: Add query params
+    ...getterOptions
   } = {},
 ) => {
 
-  const response = await pipe17Client.fetch({
-    url: `/receipts/${ receiptId }`,
-    factoryArgs: [credsPath],
-    interpreter: (response) => {
-      return {
-        ...response,
-        ...response.result ? {
-          result: response.result.receipt,
-        } : {},
-      };
+  const response = await pipe17Get(
+    '/fulfillments', 
+    'fulfillments', 
+    {
+      credsPath,
+      ...getterOptions,
     },
-  });
-  
+  );
+
   logDeep(response);
   return response;
 };
 
 const pipe17FulfillmentsGetApi = async (req, res) => {
   const { 
-    receiptId,
     options,
   } = req.body;
 
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'receiptId', receiptId),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
+  // const paramsValid = await Promise.all([
+  //   mandateParam(res, 'arg', arg),
+  // ]);
+  // if (paramsValid.some(valid => valid === false)) {
+  //   return;
+  // }
 
   const result = await pipe17FulfillmentsGet(
-    receiptId,
     options,
   );
   respond(res, 200, result);
@@ -50,4 +45,4 @@ module.exports = {
   pipe17FulfillmentsGetApi,
 };
 
-// curl localhost:8000/pipe17FulfillmentsGet -H "Content-Type: application/json" -d '{ "receiptId": "b9d03991a844e340" }'
+// curl localhost:8000/pipe17FulfillmentsGet -H "Content-Type: application/json" -d '{ "options": { "limit": 300 } }'
