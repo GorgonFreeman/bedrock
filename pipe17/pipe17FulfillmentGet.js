216921c8a68ebd80844d83b326ab142705e06caf
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-28 17:02:29 +1000

    convert pipe17FulfillmentGet to support extFulfillmentId

--- pipe17/pipe17FulfillmentGet.js
+++ pipe17/pipe17FulfillmentGet.js
@@ -2,15 +2,21 @@ const { respond, mandateParam, logDeep } = require('../utils');
 const { pipe17GetSingle } = require('../pipe17/pipe17GetSingle');
 
 const pipe17FulfillmentGet = async (
-  fulfillmentId,
+  {
+    fulfillmentId,
+    extFulfillmentId,
+  },
   {
     credsPath,
   } = {},
 ) => {
 
+  const fulfillmentIdentifier = fulfillmentId 
+  || (extFulfillmentId ? `ext:${ extFulfillmentId }` : null);
+
   const response = await pipe17GetSingle(
     'fulfillment',
-    fulfillmentId,
+    fulfillmentIdentifier,
     {
       credsPath,
     },
@@ -21,19 +27,19 @@ const pipe17FulfillmentGet = async (
 
 const pipe17FulfillmentGetApi = async (req, res) => {
   const { 
-    fulfillmentId,
+    fulfillmentIdentifier,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'fulfillmentId', fulfillmentId),
+    mandateParam(res, 'fulfillmentIdentifier', fulfillmentIdentifier),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await pipe17FulfillmentGet(
-    fulfillmentId,
+    fulfillmentIdentifier,
     options,
   );
   respond(res, 200, result);
@@ -44,4 +50,5 @@ module.exports = {
   pipe17FulfillmentGetApi,
 };
 
-// curl localhost:8000/pipe17FulfillmentGet -H "Content-Type: application/json" -d '{ "fulfillmentId": "4ce308990d48d54f" }'
+// curl localhost:8000/pipe17FulfillmentGet -H "Content-Type: application/json" -d '{ "fulfillmentIdentifier": { "fulfillmentId": "4ce308990d48d54f" } }'
+// curl localhost:8000/pipe17FulfillmentGet -H "Content-Type: application/json" -d '{ "fulfillmentIdentifier": { "extFulfillmentId": "b2a89a0e8d79e996:288133826938" } }'
