8c0bf8a10da6ac86df7d9f2f4185afd53c3e7ada
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-28 16:46:10 +1000

    pipe17FulfillmentsGet stub

---
+++ pipe17/pipe17FulfillmentsGet.js
@@ -0,0 +1,53 @@
+const { respond, mandateParam, logDeep } = require('../utils');
+const { pipe17Client } = require('../pipe17/pipe17.utils');
+
+const pipe17FulfillmentsGet = async (
+  receiptId,
+  {
+    credsPath,
+  } = {},
+) => {
+
+  const response = await pipe17Client.fetch({
+    url: `/receipts/${ receiptId }`,
+    factoryArgs: [credsPath],
+    interpreter: (response) => {
+      return {
+        ...response,
+        ...response.result ? {
+          result: response.result.receipt,
+        } : {},
+      };
+    },
+  });
+  
+  logDeep(response);
+  return response;
+};
+
+const pipe17FulfillmentsGetApi = async (req, res) => {
+  const { 
+    receiptId,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'receiptId', receiptId),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await pipe17FulfillmentsGet(
+    receiptId,
+    options,
+  );
+  respond(res, 200, result);
+};
+
+module.exports = {
+  pipe17FulfillmentsGet,
+  pipe17FulfillmentsGetApi,
+};
+
+// curl localhost:8000/pipe17FulfillmentsGet -H "Content-Type: application/json" -d '{ "receiptId": "b9d03991a844e340" }'
