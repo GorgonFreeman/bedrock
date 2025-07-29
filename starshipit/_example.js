749c11f7521387f436eb8fd0e0e272ea33ee6ef1
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-29 13:56:58 +1000

    starshipit platform example function

---
+++ starshipit/_example.js
@@ -0,0 +1,47 @@
+const { respond, mandateParam, logDeep } = require('../utils');
+const { starshipitClient } = require('../starshipit/starshipit.utils');
+
+const FUNC = async (
+  credsPath,
+  arg,
+) => {
+
+  const response = await starshipitClient.fetch({
+    url: '/things',
+    params: {
+      arg_value: arg,
+    },
+    factoryArgs: [{ credsPath }],
+  });
+
+  logDeep(response);
+  return response;
+};
+
+const FUNCApi = async (req, res) => {
+  const { 
+    credsPath,
+    arg,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'credsPath', credsPath),
+    mandateParam(res, 'arg', arg),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await FUNC(
+    credsPath,
+    arg,
+  );
+  respond(res, 200, result);
+};
+
+module.exports = {
+  FUNC,
+  FUNCApi,
+};
+
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "credsPath": "wf", "arg": "408418809" }' 
