78c10d01b6f8273a00176328432715725f76cf6d
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-29 16:19:51 +1000

    update example command to reflect that you are required to submit at least one option

--- starshipit/starshipitAddressesGet.js
+++ starshipit/starshipitAddressesGet.js
@@ -59,4 +59,4 @@ module.exports = {
   starshipitAddressesGetApi,
 };
 
-// curl localhost:8000/starshipitAddressesGet -H "Content-Type: application/json" -d '{ "credsPath": "wf" }' 
+// curl localhost:8000/starshipitAddressesGet -H "Content-Type: application/json" -d '{ "credsPath": "wf", "options": { "page": 1 } }' 
