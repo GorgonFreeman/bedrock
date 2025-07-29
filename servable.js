77bb99d4857a98e9ed282f290759ba080454527a
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-29 13:57:11 +1000

    starshipitOrderDelete stub

--- servable.js
+++ servable.js
@@ -3,6 +3,7 @@ module.exports = {
   upstashSet: require('./upstash/upstash.utils').upstashSetApi,
   upstashDel: require('./upstash/upstash.utils').upstashDelApi,
   upstashExists: require('./upstash/upstash.utils').upstashExistsApi,
+  starshipitOrderDelete: require('./starshipit/starshipitOrderDelete').starshipitOrderDeleteApi,
   starshipitOrderGet: require('./starshipit/starshipitOrderGet').starshipitOrderGetApi,
   shopifyCustomerGet: require('./shopify/shopifyCustomerGet').shopifyCustomerGetApi,
   shopifyGetSingle: require('./shopify/shopifyGetSingle').shopifyGetSingleApi,
