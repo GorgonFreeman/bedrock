2eb93c573c59704daa18bc948c65959ce7b6ca57
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-29 17:38:20 +1000

    fix starshipitAddressUpdate

--- starshipit/starshipitAddressUpdate.js
+++ starshipit/starshipitAddressUpdate.js
@@ -1,24 +1,20 @@
-// https://api-docs.starshipit.com/#4f93a04a-c4db-40bf-86d5-f4f8fd3fb265
+// https://api-docs.starshipit.com/#4ff9ae6f-fadb-4c50-9087-467c2e336f93
 
 const { respond, mandateParam, logDeep } = require('../utils');
 const { starshipitClient } = require('../starshipit/starshipit.utils');
 
 const starshipitAddressUpdate = async (
   credsPath,
-  {
-    addressId,
-  },
+  addressId,
   updatePayload,
 ) => {
 
   const response = await starshipitClient.fetch({
-    url: '/addressbook',
-    method: 'put',
+    url: '/addressbook/update',
+    method: 'post',
     body: {
-      address: {
-        address_id: addressId,
-        ...updatePayload,
-      },
+      id: addressId,
+      address: updatePayload,
     },
     factoryArgs: [{ credsPath }],
     interpreter: (response) => {
@@ -38,13 +34,13 @@ const starshipitAddressUpdate = async (
 const starshipitAddressUpdateApi = async (req, res) => {
   const { 
     credsPath,
-    addressIdentifier,
+    addressId,
     updatePayload,
   } = req.body;
 
   const paramsValid = await Promise.all([
     mandateParam(res, 'credsPath', credsPath),
-    mandateParam(res, 'addressIdentifier', addressIdentifier, p => p.addressId),
+    mandateParam(res, 'addressId', addressId),
     mandateParam(res, 'updatePayload', updatePayload),
   ]);
   if (paramsValid.some(valid => valid === false)) {
@@ -53,7 +49,7 @@ const starshipitAddressUpdateApi = async (req, res) => {
 
   const result = await starshipitAddressUpdate(
     credsPath,
-    addressIdentifier,
+    addressId,
     updatePayload,
   );
   respond(res, 200, result);
@@ -64,4 +60,4 @@ module.exports = {
   starshipitAddressUpdateApi,
 };
 
-// curl localhost:8000/starshipitAddressUpdate -H "Content-Type: application/json" -d '{ "credsPath": "wf", "addressIdentifier": { "addressId": "123" }, "updatePayload": { "name": "Jane Doe", "phone": "0498765432" } }' 
+// curl localhost:8000/starshipitAddressUpdate -H "Content-Type: application/json" -d '{ "credsPath": "wf", "addressId": 14824685, "updatePayload": { "name": "Batman" } }'
