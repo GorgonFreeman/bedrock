66e16254cfb40b852374194834a965f0ab0317f6
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-07-31 15:45:58 +1000

    peoplevoxOrdersGet complete function

--- peoplevox/peoplevoxOrdersGet.js
+++ peoplevox/peoplevoxOrdersGet.js
@@ -2,7 +2,7 @@ const { respond, mandateParam, logDeep } = require('../utils');
 const { peoplevoxClient, peoplevoxStandardInterpreter } = require('../peoplevox/peoplevox.utils');
 
 const peoplevoxOrdersGet = async (
-  salesOrderNumber,
+  searchClause,
   {
     credsPath,
   } = {},
@@ -18,14 +18,14 @@ const peoplevoxOrdersGet = async (
     body: {
       getRequest: {
         TemplateName: 'Sales orders',
-        SearchClause: `SalesOrderNumber.Equals("${ salesOrderNumber }")`,
+        SearchClause: searchClause,
       },
     },
     context: { 
       credsPath,
       action,
      },
-    interpreter: peoplevoxStandardInterpreter({ expectOne: true }),
+    interpreter: peoplevoxStandardInterpreter(),
   });
   logDeep(response);
   return response;
@@ -34,19 +34,19 @@ const peoplevoxOrdersGet = async (
 
 const peoplevoxOrdersGetApi = async (req, res) => {
   const { 
-    salesOrderNumber,
+    searchClause,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'salesOrderNumber', salesOrderNumber),
+    mandateParam(res, 'searchClause', searchClause),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await peoplevoxOrdersGet(
-    salesOrderNumber,
+    searchClause,
     options,
   );
   respond(res, 200, result);
@@ -57,4 +57,6 @@ module.exports = {
   peoplevoxOrdersGetApi,
 };
 
-// curl localhost:8000/peoplevoxOrdersGet -H "Content-Type: application/json" -d '{ "salesOrderNumber": "5977690603592" }'
+// curl localhost:8000/peoplevoxOrdersGet -H "Content-Type: application/json" -d '{ "searchClause": "SalesOrderNumber.Equals(\"5977690603592\")" }'
+// curl localhost:8000/peoplevoxOrdersGet -H "Content-Type: application/json" -d '{ "searchClause": "CustomerPurchaseOrderReferenceNumber.StartsWith(\"#547\")" }'
+// curl localhost:8000/peoplevoxOrdersGet -H "Content-Type: application/json" -d '{ "searchClause": "ServiceType.Equals(\"3J35\") AND Status.Equals(5)" }'
