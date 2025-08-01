1e531ffcf18540897760ffe84855cf5f3772ecc8
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-01 13:55:16 +1000

    peoplevoxDespatchGet function initial
    sales order number option not working

--- peoplevox/peoplevoxDespatchGet.js
+++ peoplevox/peoplevoxDespatchGet.js
@@ -1,12 +1,22 @@
-const { respond, mandateParam, logDeep } = require('../utils');
+const { respond, mandateParam, logDeep, objHasAny } = require('../utils');
 const { peoplevoxClient, peoplevoxStandardInterpreter } = require('../peoplevox/peoplevox.utils');
 
 const peoplevoxDespatchGet = async (
-  salesOrderNumber,
+  {
+    despatchNumber,
+    salesOrderNumber,
+  },
   {
     credsPath,
   } = {},
 ) => {
+
+  let searchClause;
+  if (despatchNumber) {
+    searchClause = `despatchNumber.Equals("${ despatchNumber }")`;
+  } else if (salesOrderNumber) {
+    searchClause = `salesOrderNumber.Equals("${ salesOrderNumber }")`;
+  }
 
   const action = 'GetData';
 
@@ -17,8 +27,8 @@ const peoplevoxDespatchGet = async (
     method: 'post',
     body: {
       getRequest: {
-        TemplateName: 'Sales orders',
-        SearchClause: `SalesOrderNumber.Equals("${ salesOrderNumber }")`,
+        TemplateName: 'Despatches',
+        SearchClause: `despatchNumber.Equals("${ despatchNumber }")`,
       },
     },
     context: { 
@@ -29,24 +39,23 @@ const peoplevoxDespatchGet = async (
   });
   logDeep(response);
   return response;
-  
 };
 
 const peoplevoxDespatchGetApi = async (req, res) => {
   const { 
-    salesOrderNumber,
+    despatchIdentifier,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'salesOrderNumber', salesOrderNumber),
+    mandateParam(res, 'despatchIdentifier', despatchIdentifier, p => objHasAny(p, ['despatchNumber', 'salesOrderNumber'])),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await peoplevoxDespatchGet(
-    salesOrderNumber,
+    despatchIdentifier,
     options,
   );
   respond(res, 200, result);
@@ -57,4 +66,5 @@ module.exports = {
   peoplevoxDespatchGetApi,
 };
 
-// curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "salesOrderNumber": "5977690603592" }'
+// curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "despatchIdentifier": { "despatchNumber": "DES9423117" } }'
+// curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "despatchIdentifier": { "salesOrderNumber": "BPR-2181" } }'
