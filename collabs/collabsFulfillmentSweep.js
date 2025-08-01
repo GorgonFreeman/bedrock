ab8c674ca603718bfa09a2e9feadd0ec475a8771
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-01 16:59:53 +1000

    refactor for readability and dynamically get dispatches from a week ago

--- collabs/collabsFulfillmentSweep.js
+++ collabs/collabsFulfillmentSweep.js
@@ -1,4 +1,4 @@
-const { respond, mandateParam, logDeep, gidToId, askQuestion } = require('../utils');
+const { respond, mandateParam, logDeep, gidToId, askQuestion, dateTimeFromNow, weeks } = require('../utils');
 const { REGIONS_PVX } = require('../constants');
 const { shopifyRegionToStarshipitAccount } = require('../mappings');
 
@@ -6,44 +6,55 @@ const { shopifyOrdersGet } = require('../shopify/shopifyOrdersGet');
 
 const { peoplevoxOrdersGetById } = require('../peoplevox/peoplevoxOrdersGetById');
 const { peoplevoxReportGet } = require('../peoplevox/peoplevoxReportGet');
+const { peoplevoxDateFormatter } = require('../peoplevox/peoplevox.utils');
+
 const { starshipitOrderGet } = require('../starshipit/starshipitOrderGet');
 
 const collabsFulfillmentSweep = async (
   {
     shopifyRegions = REGIONS_PVX,
+    peoplevoxReportWindowWeeksAgo = 1,
   } = {},
 ) => {
   
   // 1. Fetch unfulfilled orders for each region
+  const getShopifyOrdersPerRegion = (region) => shopifyOrdersGet(
+    region,
+    {
+      attrs: `
+        id
+        shippingLine {
+          title
+        }
+      `,
+      queries: [
+        'created_at:>2024-01-01',
+        'fulfillment_status:unfulfilled',
+        'status:Open',
+      ],
+    },
+  );
+
   // 1a. Also prefetch any other useful data, e.g. pvx recent dispatches
+  let pvxReportWindowStart = dateTimeFromNow({ minus: weeks(peoplevoxReportWindowWeeksAgo), dateOnly: true });
+  pvxReportWindowStart = peoplevoxDateFormatter(pvxReportWindowStart);
+  const getPeoplevoxRecentDispatches = () => peoplevoxReportGet('Despatch summary', { 
+    columns: ['Salesorder number', 'Carrier', 'Tracking number', 'Despatch date'], 
+    searchClause: `([Despatch date] >= ${ pvxReportWindowStart })`, 
+  });
+
   const [
     pvxRecentDispatchesResponse,
     ...shopifyOrderResponses
   ] = await Promise.all([
-    peoplevoxReportGet('Despatch summary', { 
-      columns: ['Salesorder number', 'Carrier', 'Tracking number', 'Despatch date'], 
-      perPage: 4,
-      searchClause: `([Despatch date] >= DateTime(2025,07,01,00,00,00)) AND ([Despatch date] <= DateTime(2025,08,01,00,00,00))`, 
-    }),
-    ...shopifyRegions.map(region => shopifyOrdersGet(
-      region,
-      {
-        attrs: `
-          id
-          shippingLine {
-            title
-          }
-        `,
-        queries: [
-          'created_at:>2024-01-01',
-          'fulfillment_status:unfulfilled',
-          'status:Open',
-        ],
-      },
-    )),
+    getPeoplevoxRecentDispatches(),
+    ...shopifyRegions.map(region => getShopifyOrdersPerRegion(region)),
   ]);
 
   logDeep(pvxRecentDispatchesResponse);
+  await askQuestion('?');
+
+  logDeep(shopifyOrderResponses);
   await askQuestion('?');
 
   // 2. For each region, deplete array of unfulfilled orders by retrieving tracking info from other platforms specific to that region
