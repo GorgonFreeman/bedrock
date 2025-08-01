77af53c48a555cce1dd0074eebadc63b42d95766
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-01 15:29:19 +1000

    wip getting despatch from report

--- peoplevox/peoplevoxDespatchGet.js
+++ peoplevox/peoplevoxDespatchGet.js
@@ -1,5 +1,6 @@
-const { respond, mandateParam, logDeep, objHasAny } = require('../utils');
-const { peoplevoxClient, peoplevoxGetSingle } = require('../peoplevox/peoplevox.utils');
+const { respond, mandateParam, logDeep, objHasAny, askQuestion } = require('../utils');
+const { peoplevoxGetSingle } = require('../peoplevox/peoplevox.utils');
+const { peoplevoxReportGet } = require('../peoplevox/peoplevoxReportGet');
 
 const peoplevoxDespatchGet = async (
   {
@@ -22,14 +23,28 @@ const peoplevoxDespatchGet = async (
     logDeep(response);
     return response;
   }
-
-  response = await peoplevoxGetSingle(
-    'Despatches', 
-    { searchClause: `salesOrderNumber.Equals("${ salesOrderNumber }")` }, 
-    { credsPath },
+  
+  const reportResponse = await peoplevoxReportGet(
+    'Despatch summary',
+    {
+      credsPath,
+      searchClause: `([Salesorder number].Equals(\"${ salesOrderNumber }\"))`,
+      perPage: 50,
+    },
   );
-  logDeep(response);
-  return response;
+  logDeep('reportResponse', reportResponse);
+  await askQuestion('?');
+
+
+  // response = await peoplevoxGetSingle(
+  //   'Despatches', 
+  //   { searchClause: `salesOrderNumber.Equals("${ salesOrderNumber }")` }, 
+  //   { credsPath },
+  // );
+  // logDeep(response);
+  // return response;
+
+  return;
 };
 
 const peoplevoxDespatchGetApi = async (req, res) => {
@@ -58,4 +73,4 @@ module.exports = {
 };
 
 // curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "despatchIdentifier": { "despatchNumber": "DES9423117" } }'
-// curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "despatchIdentifier": { "salesOrderNumber": "BPR-2181" } }'
+// curl localhost:8000/peoplevoxDespatchGet -H "Content-Type: application/json" -d '{ "despatchIdentifier": { "salesOrderNumber": "  PRFB-1634 " } }'
