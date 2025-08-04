7f7102238ee4ad05565e35e4634f1901cea94609
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-04 16:15:20 +1000

    logiwaAuthGet initial

--- logiwa/logiwaAuthGet.js
+++ logiwa/logiwaAuthGet.js
@@ -1,12 +1,37 @@
-const { respond, mandateParam } = require('../utils');
+const { respond, mandateParam, credsByPath, customAxios, logDeep } = require('../utils');
 
 const logiwaAuthGet = async (
   {
     credsPath,
+    apiVersion = 'v3.1',
   } = {},
 ) => {
 
-  return true;
+  const creds = credsByPath(['logiwa', credsPath]);
+
+  const { 
+    BASE_URL,
+    LOGIN_EMAIL,
+    LOGIN_PASSWORD,
+  } = creds;
+
+  const body = {
+    email: LOGIN_EMAIL,
+    password: LOGIN_PASSWORD,
+  };
+
+  const params = {
+    apiVersion,
+  };
+
+  const url = `${ BASE_URL }/${ apiVersion }/Authorize/token`;
+
+  const response = await customAxios(url, {
+    method: 'post',
+    body,
+  });
+  logDeep(response);
+  return response;
   
 };
 
