3334878ddb6ef8051573c939c7be345b3dfb2915
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-04 21:13:36 +1000

    logiwaOrdersList initial - pagination is in url

--- logiwa/logiwaOrdersList.js
+++ logiwa/logiwaOrdersList.js
@@ -4,7 +4,6 @@ const { respond, mandateParam, logDeep } = require('../utils');
 const { logiwaClient } = require('../logiwa/logiwa.utils');
 
 const logiwaOrdersList = async (
-  orderId,
   {
     credsPath,
     apiVersion = 'v3.1',
@@ -13,7 +12,7 @@ const logiwaOrdersList = async (
 
   const response = await logiwaClient.fetch({
     method: 'get',
-    url: `/ShipmentOrder/${ orderId }`,
+    url: `/ShipmentOrder/list/i/1/s/20`,
   });
   logDeep(response);
   return response;
@@ -21,19 +20,17 @@ const logiwaOrdersList = async (
 
 const logiwaOrdersListApi = async (req, res) => {
   const { 
-    orderId,
     options,
   } = req.body;
 
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'orderId', orderId),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
+  // const paramsValid = await Promise.all([
+  //   mandateParam(res, 'orderId', orderId),
+  // ]);
+  // if (paramsValid.some(valid => valid === false)) {
+  //   return;
+  // }
 
   const result = await logiwaOrdersList(
-    orderId,
     options,
   );
   respond(res, 200, result);
@@ -44,4 +41,4 @@ module.exports = {
   logiwaOrdersListApi,
 };
 
-// curl localhost:8000/logiwaOrdersList -H "Content-Type: application/json" -d '{ "orderId": "9ce5f6f0-c461-4d1c-93df-261a2188d652" }'
+// curl localhost:8000/logiwaOrdersList
