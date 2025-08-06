fae21065e339cf5786295fa8f3bdfee8966f0b62
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-06 12:51:18 +1000

    bleckmannInventoryGet stub

--- servable.js
+++ servable.js
@@ -87,6 +87,7 @@ module.exports = {
   etsyMeGet: require('./etsy/etsyMeGet').etsyMeGetApi,
   etsyPing: require('./etsy/etsyPing').etsyPingApi,
   collabsFulfillmentSweep: require('./collabs/collabsFulfillmentSweep').collabsFulfillmentSweepApi,
+  bleckmannInventoryGet: require('./bleckmann/bleckmannInventoryGet').bleckmannInventoryGetApi,
   bleckmannSkuGet: require('./bleckmann/bleckmannSkuGet').bleckmannSkuGetApi,
   bleckmannSkusGet: require('./bleckmann/bleckmannSkusGet').bleckmannSkusGetApi,
 };
