9a672b86251f6b2fd28f05934afb6e73caa7cee3
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-06 13:27:05 +1000

    replace with getter template

--- bleckmann/bleckmannEventsGet.js
+++ bleckmann/bleckmannEventsGet.js
@@ -1,46 +1,62 @@
-// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/SKU/getSkuForId
+// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/INVENTORY/getAdjustments
 
 const { respond, mandateParam, logDeep } = require('../utils');
-const { bleckmannClient } = require('../bleckmann/bleckmann.utils');
+const { bleckmannGet } = require('../bleckmann/bleckmann.utils');
 
-const bleckmannEventsGet = async (
-  sku,
+const bleckmannInventoryAdjustmentsGet = async (
+  createdFrom,
+  createdTo,
   {
     credsPath,
+    skip,
+    perPage,
+    ...getterOptions
   } = {},
 ) => {
 
-  const response = await bleckmannClient.fetch({
-    url: `/skus/${ encodeURIComponent(sku) }`,
-  });
-
+  const response = await bleckmannGet(
+    '/inventory/adjustments',
+    {
+      credsPath,
+      params: {
+        createdFrom,
+        createdTo,
+        ...(skip && { skip }),
+      },
+      ...(perPage && { perPage }),
+      ...getterOptions,
+    },
+  );
   logDeep(response);
   return response;
 };
 
-const bleckmannEventsGetApi = async (req, res) => {
-  const { 
-    sku,
+const bleckmannInventoryAdjustmentsGetApi = async (req, res) => {
+  const {
+    createdFrom,
+    createdTo,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'sku', sku),
+    mandateParam(res, 'createdFrom', createdFrom),
+    mandateParam(res, 'createdTo', createdTo),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
-  const result = await bleckmannEventsGet(
-    sku,
+  const result = await bleckmannInventoryAdjustmentsGet(
+    createdFrom,
+    createdTo,
     options,
   );
   respond(res, 200, result);
 };
 
 module.exports = {
-  bleckmannEventsGet,
-  bleckmannEventsGetApi,
+  bleckmannInventoryAdjustmentsGet,
+  bleckmannInventoryAdjustmentsGetApi,
 };
 
-// curl localhost:8000/bleckmannEventsGet -H "Content-Type: application/json" -d '{ "sku": "EXD1224-3-3XS/XXS" }'
+// curl localhost:8000/bleckmannInventoryAdjustmentsGet -H "Content-Type: application/json" -d '{ "createdFrom": "2025-07-01T00:00:00+01:00", "createdTo": "2025-07-02T00:00:00+01:00" }'
