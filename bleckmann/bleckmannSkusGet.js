94accbc2233601d601e2e15e9924f477dc9db88a
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-06 12:39:44 +1000

    apply now as the createdTo date if using createdFrom to satisfy API requirements

--- bleckmann/bleckmannSkusGet.js
+++ bleckmann/bleckmannSkusGet.js
@@ -12,6 +12,10 @@ const bleckmannSkusGet = async (
   } = {},
 ) => {
 
+  if (createdFrom && !createdTo) {
+    createdTo = new Date().toISOString();
+  }
+
   const response = await bleckmannGet(
     '/skus',
     {
@@ -54,4 +58,4 @@ module.exports = {
 
 // curl localhost:8000/bleckmannSkusGet
 // curl localhost:8000/bleckmannSkusGet -H "Content-Type: application/json" -d '{ "options": { "limit": 100 } }'
-// curl localhost:8000/bleckmannSkusGet -H "Content-Type: application/json" -d '{ "options": { "createdFrom": "2024-01-01T00:00:00+01:00", "createdTo": "2025-12-31T23:59:59+01:00" } }'
+// curl localhost:8000/bleckmannSkusGet -H "Content-Type: application/json" -d '{ "options": { "createdFrom": "2024-01-01T00:00:00+01:00" } }'
