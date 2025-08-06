5fc2d6bdbdbc99edcbed938349b8b0cc8c9eeac2
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-06 13:29:54 +1000

    getter template and docs link

--- bleckmann/bleckmannReturnsGet.js
+++ bleckmann/bleckmannReturnsGet.js
@@ -1,38 +1,54 @@
-// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/SKU/getSkuForId
+// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/RETURN/getReturns
 
 const { respond, mandateParam, logDeep } = require('../utils');
-const { bleckmannClient } = require('../bleckmann/bleckmann.utils');
+const { bleckmannGet } = require('../bleckmann/bleckmann.utils');
 
 const bleckmannReturnsGet = async (
-  sku,
+  createdFrom,
+  createdTo,
   {
     credsPath,
+    skip,
+    perPage,
+    ...getterOptions
   } = {},
 ) => {
 
-  const response = await bleckmannClient.fetch({
-    url: `/skus/${ encodeURIComponent(sku) }`,
-  });
-
+  const response = await bleckmannGet(
+    '/events',
+    {
+      credsPath,
+      params: {
+        createdFrom,
+        createdTo,
+        ...(skip && { skip }),
+      },
+      ...(perPage && { perPage }),
+      ...getterOptions,
+    },
+  );
   logDeep(response);
   return response;
 };
 
 const bleckmannReturnsGetApi = async (req, res) => {
-  const { 
-    sku,
+  const {
+    createdFrom,
+    createdTo,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'sku', sku),
+    mandateParam(res, 'createdFrom', createdFrom),
+    mandateParam(res, 'createdTo', createdTo),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await bleckmannReturnsGet(
-    sku,
+    createdFrom,
+    createdTo,
     options,
   );
   respond(res, 200, result);
@@ -43,4 +59,4 @@ module.exports = {
   bleckmannReturnsGetApi,
 };
 
-// curl localhost:8000/bleckmannReturnsGet -H "Content-Type: application/json" -d '{ "sku": "EXD1224-3-3XS/XXS" }'
+// curl localhost:8000/bleckmannReturnsGet -H "Content-Type: application/json" -d '{ "createdFrom": "2025-07-01T00:00:00+01:00", "createdTo": "2025-07-02T00:00:00+01:00" }'
