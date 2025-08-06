96afc0d587cd84e696e6a2cd1aca40f9cec4f1c6
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-06 13:27:49 +1000

    add link to docs

--- bleckmann/bleckmannEventsGet.js
+++ bleckmann/bleckmannEventsGet.js
@@ -1,4 +1,4 @@
-// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/INVENTORY/getAdjustments
+// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/EVENT/getEvents
 
 const { respond, mandateParam, logDeep } = require('../utils');
 const { bleckmannGet } = require('../bleckmann/bleckmann.utils');
