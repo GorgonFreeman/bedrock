f6fcab1fac040cf77928dcf036db286ca69363e6
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-07 22:34:11 +1000

    generate pkce
    I’m not gonna lie, this is very random, not particularly trying to make it readable.

--- etsy/etsyAuthCodeRequest.js
+++ etsy/etsyAuthCodeRequest.js
@@ -1,4 +1,8 @@
-const { respond, mandateParam, logDeep } = require('../utils');
+const { v4: uuidv4 } = require('uuid');
+const { exec } = require('child_process');
+const crypto = require('crypto');
+
+const { respond, mandateParam, logDeep, credsByPath } = require('../utils');
 const { etsyClient } = require('../etsy/etsy.utils');
 
 const etsyAuthCodeRequest = async (
@@ -6,14 +10,48 @@ const etsyAuthCodeRequest = async (
     credsPath,
   } = {},
 ) => {
-  const response = await etsyClient.fetch({ 
-    url: `/application/things/${ arg }`,
-    context: {
-      credsPath,
-    },
-  });
-  logDeep(response);
-  return response;
+  
+  const creds = credsByPath(['etsy', credsPath]);
+  const { 
+    API_KEY,
+    OAUTH_URL, 
+    OAUTH_REDIRECT_URL,
+  } = creds;
+
+  const state = uuidv4();
+  // console.log('state', state);
+
+  const generateRandomString = (length) => {
+    return crypto
+      .randomBytes(Math.ceil(length * 3 / 4))
+      .toString('base64')
+      .slice(0, length)
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+    ;
+  };
+
+  const generatePkce = () => {
+    const codeVerifier = generateRandomString(64);
+    const codeChallenge = crypto
+      .createHash('sha256')
+      .update(codeVerifier)
+      .digest('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=+$/, '')
+    ;
+  
+    return {
+      codeVerifier,
+      codeChallenge,
+    };
+  };
+
+  const { codeVerifier, codeChallenge } = generatePkce();
+  // console.log('codeVerifier, codeChallenge', codeVerifier, codeChallenge);
+
+  return { codeVerifier, codeChallenge };
 };
 
 const etsyAuthCodeRequestApi = async (req, res) => {
