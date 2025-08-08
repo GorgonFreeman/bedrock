06058a316e66aa118bc187d7c99f76b16fe0637c
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-08 22:28:26 +1000

    iwishPaginator logic

--- iwish/iwish.utils.js
+++ iwish/iwish.utils.js
@@ -26,8 +26,33 @@ const iwishClient = new CustomAxiosClient({
 });
 
 const iwishPaginator = async (customAxiosPayload, response, additionalPaginationData) => {
-  console.log('iwishPaginator', customAxiosPayload, response, additionalPaginationData);
-  await askQuestion('?');
+  // console.log('iwishPaginator', customAxiosPayload, response, additionalPaginationData);
+  // await askQuestion('?');
+
+  const { success, result } = response;
+  if (!success) { // Return if failed
+    return [true, null]; 
+  }
+  
+  // 1. Extract necessary pagination info
+  const { 
+    current_page: currentPage, 
+    next_page: nextPage,
+  } = result;
+
+  // 2. Supplement payload with next pagination info
+  const paginatedPayload = {
+    ...customAxiosPayload,
+    params: {
+      ...customAxiosPayload?.params,
+      page: currentPage + 1,
+    },
+  };
+  
+  // 3. Logic to determine done
+  const done = !nextPage || nextPage === null || nextPage === 'null';
+
+  return [done, paginatedPayload];
 };
 
 const iwishDigester = async (response) => {
