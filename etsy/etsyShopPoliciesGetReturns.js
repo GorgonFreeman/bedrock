d09bbb3a68095d1710f965a10599146100ad5971
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-08 10:36:51 +1000

    add docs link

--- etsy/etsyShopPoliciesGetReturns.js
+++ etsy/etsyShopPoliciesGetReturns.js
@@ -1,3 +1,5 @@
+// https://developers.etsy.com/documentation/reference/#operation/getShopReturnPolicies
+
 const { respond, mandateParam, logDeep } = require('../utils');
 const { etsyGetShopIdAndUserId, etsyGet } = require('../etsy/etsy.utils');
 
