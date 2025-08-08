475a5b5282f13072f23dddac415b2a8ab3f1f268
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-08 21:31:52 +1000

    iwishCustomerWishlistCountGet complete function

--- iwish/iwishCustomerWishlistCountGet.js
+++ iwish/iwishCustomerWishlistCountGet.js
@@ -10,7 +10,7 @@ const iwishCustomerWishlistCountGet = async (
 ) => {
 
   const response = await iwishClient.fetch({
-    url: `/V2/fetchWishlistData/${ customerId }`,
+    url: `/V2/count/${ customerId }`,
     context: {
       credsPath,
     },
@@ -47,4 +47,4 @@ module.exports = {
   iwishCustomerWishlistCountGetApi,
 };
 
-// curl localhost:8000/iwishCustomerWishlistCountGet -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerId": "2700981665864" }'
+// curl localhost:8000/iwishCustomerWishlistCountGet -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerId": "8575963103304" }'
