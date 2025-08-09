8ab87ba66b8493d658a6b7f787ee2fb30a01a065
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-09 16:36:09 +1000

    compose backblazeAuthGet
    it works!

--- servable.js
+++ servable.js
@@ -145,5 +145,6 @@ module.exports = {
   bleckmannSkusGet: require('./bleckmann/bleckmannSkusGet').bleckmannSkusGetApi,
   bedrock_unlisted_shopifyStaffCustomerOffboard: require('./bedrock_unlisted/bedrock_unlisted_shopifyStaffCustomerOffboard').bedrock_unlisted_shopifyStaffCustomerOffboardApi,
   bedrock_unlisted_shopifyStaffCustomerOnboard: require('./bedrock_unlisted/bedrock_unlisted_shopifyStaffCustomerOnboard').bedrock_unlisted_shopifyStaffCustomerOnboardApi,
+  backblazeAuthGet: require('./backblaze/backblazeAuthGet').backblazeAuthGetApi,
   backblazeBucketsGet: require('./backblaze/backblazeBucketsGet').backblazeBucketsGetApi,
 };
