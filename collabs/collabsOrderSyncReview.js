73092e2c21c84e5c001fca9ed9cf2737f01f17a9
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-13 08:06:41 +1000

    get orders for region

--- collabs/collabsOrderSyncReview.js
+++ collabs/collabsOrderSyncReview.js
@@ -1,34 +1,51 @@
 const { respond, mandateParam } = require('../utils');
+const { shopifyOrdersGet } = require('../shopify/shopifyOrdersGet');
 
 const collabsOrderSyncReview = async (
-  arg,
+  region,
   {
     option,
   } = {},
 ) => {
 
+  const shopifyOrders = await shopifyOrdersGet(
+    region, 
+    {
+      attrs: `
+        id
+      `,
+      queries: [
+        'created_at:>2025-07-01',
+        'fulfillment_status:unshipped',
+        'status:open',
+        'delivery_method:shipping',
+      ],
+      limit: 50, // TODO: Remove after testing
+    },
+  );
+
   return { 
-    arg, 
-    option,
+    success: true,
+    result: shopifyOrders,
   };
   
 };
 
 const collabsOrderSyncReviewApi = async (req, res) => {
   const { 
-    arg,
+    region,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'arg', arg),
+    mandateParam(res, 'region', region),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await collabsOrderSyncReview(
-    arg,
+    region,
     options,
   );
   respond(res, 200, result);
@@ -39,4 +56,4 @@ module.exports = {
   collabsOrderSyncReviewApi,
 };
 
-// curl localhost:8000/collabsOrderSyncReview -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsOrderSyncReview -H "Content-Type: application/json" -d '{ "region": "au" }'
