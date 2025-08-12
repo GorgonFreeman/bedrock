75bb83741270fedc6cd1c6da5470a2a9a80408ac
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-12 21:58:32 +1000

    compose yotpoCustomerPointsAdjust

--- yotpo/yotpoCustomerPointsAdjust.js
+++ yotpo/yotpoCustomerPointsAdjust.js
@@ -1,15 +1,44 @@
-const { respond, mandateParam, logDeep } = require('../utils');
+// https://loyaltyapi.yotpo.com/reference/adjust-a-customers-point-balance
+
+const { respond, mandateParam, logDeep, objHasAny } = require('../utils');
 const { yotpoClient } = require('../yotpo/yotpo.utils');
 
 const yotpoCustomerPointsAdjust = async ( 
   credsPath,
   {
+    customerId,
+    customerEmail,
+    customerPhone,
+    posAccountId,
+    pointAdjustmentAmount,
+    applyAdjustmentToPointsEarned,
+    reason,
+    orderId,
+    externalReferenceId,
+    notes,
+  },
+  {
     apiVersion,
   } = {},
 ) => {
 
+  const data = {
+    point_adjustment_amount: pointAdjustmentAmount,
+    ...(customerId && { customer_id: customerId }),
+    ...(customerEmail && { customer_email: customerEmail }),
+    ...(customerPhone && { phone_number: customerPhone }),
+    ...(posAccountId && { pos_account_id: posAccountId }),
+    ...(applyAdjustmentToPointsEarned !== undefined && { apply_adjustment_to_points_earned: applyAdjustmentToPointsEarned }),
+    ...(reason && { reason }),
+    ...(orderId && { order_id: orderId }),
+    ...(externalReferenceId && { external_reference_id: externalReferenceId }),
+    ...(notes && { notes }),
+  };
+
   const response = await yotpoClient.fetch({
-    url: `/endpoint`,
+    url: `/points/adjust`,
+    method: 'POST',
+    data,
     context: {
       credsPath,
       apiVersion,
@@ -22,11 +51,15 @@ const yotpoCustomerPointsAdjust = async (
 const yotpoCustomerPointsAdjustApi = async (req, res) => {
   const { 
     credsPath,
+    customerIdentifier,
+    adjustmentData,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
     mandateParam(res, 'credsPath', credsPath),
+    mandateParam(res, 'customerIdentifier', customerIdentifier, p => objHasAny(p, ['customerId', 'customerEmail', 'customerPhone', 'posAccountId'])),
+    mandateParam(res, 'adjustmentData', adjustmentData, p => p.pointAdjustmentAmount !== undefined),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
@@ -34,6 +67,7 @@ const yotpoCustomerPointsAdjustApi = async (req, res) => {
 
   const result = await yotpoCustomerPointsAdjust(
     credsPath,
+    { ...customerIdentifier, ...adjustmentData },
     options,
   );
   respond(res, 200, result);
@@ -44,4 +78,4 @@ module.exports = {
   yotpoCustomerPointsAdjustApi,
 };
 
-// curl localhost:8000/yotpoCustomerPointsAdjust -H "Content-Type: application/json" -d '{ "credsPath": "au" }'
+// curl localhost:8000/yotpoCustomerPointsAdjust -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerIdentifier": { "customerEmail": "customer@example.com" }, "adjustmentData": { "pointAdjustmentAmount": 100, "applyAdjustmentToPointsEarned": true, "reason": "Reward customer" } }'
