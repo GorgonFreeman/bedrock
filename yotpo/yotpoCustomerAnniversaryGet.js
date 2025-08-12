6ae765a1f9745473899d1d8c1d2a86ad530db1d1
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-12 23:57:35 +1000

    yotpoCustomerAnniversaryGet complete function

--- yotpo/yotpoCustomerAnniversaryGet.js
+++ yotpo/yotpoCustomerAnniversaryGet.js
@@ -1,15 +1,23 @@
-const { respond, mandateParam, logDeep } = require('../utils');
+const { respond, mandateParam, logDeep, objHasAny } = require('../utils');
 const { yotpoClient } = require('../yotpo/yotpo.utils');
 
 const yotpoCustomerAnniversaryGet = async ( 
   credsPath,
+  {
+    customerId,
+    customerEmail,
+  },
   {
     apiVersion,
   } = {},
 ) => {
 
   const response = await yotpoClient.fetch({
-    url: `/endpoint`,
+    url: `/customer_anniversary`,
+    body: {
+      ...customerId && { customer_id: customerId },
+      ...customerEmail && { customer_email: customerEmail },
+    },
     context: {
       credsPath,
       apiVersion,
@@ -22,11 +30,13 @@ const yotpoCustomerAnniversaryGet = async (
 const yotpoCustomerAnniversaryGetApi = async (req, res) => {
   const { 
     credsPath,
+    customerIdentifier,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
     mandateParam(res, 'credsPath', credsPath),
+    mandateParam(res, 'customerIdentifier', customerIdentifier, p => objHasAny(p, ['customerId', 'customerEmail'])),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
@@ -34,6 +44,7 @@ const yotpoCustomerAnniversaryGetApi = async (req, res) => {
 
   const result = await yotpoCustomerAnniversaryGet(
     credsPath,
+    customerIdentifier,
     options,
   );
   respond(res, 200, result);
@@ -44,4 +55,4 @@ module.exports = {
   yotpoCustomerAnniversaryGetApi,
 };
 
-// curl localhost:8000/yotpoCustomerAnniversaryGet -H "Content-Type: application/json" -d '{ "credsPath": "au" }'
+// curl localhost:8000/yotpoCustomerAnniversaryGet -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerIdentifier": { "customerEmail": "john+testing@whitefoxboutique.com" } }'
