68cf2a5a3a9a36de6905e042e457d364fa3cedf0
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-14 01:00:48 +1000

    bleckmannParcelGet complete function

--- bleckmann/bleckmannParcelGet.js
+++ bleckmann/bleckmannParcelGet.js
@@ -1,17 +1,21 @@
-// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/SKU/getSkuForId
+// https://app.swaggerhub.com/apis-docs/Bleckmann/warehousing/1.5.2#/PICKTICKET/getParcelForId
 
 const { respond, mandateParam, logDeep } = require('../utils');
 const { bleckmannClient } = require('../bleckmann/bleckmann.utils');
 
 const bleckmannParcelGet = async (
-  sku,
+  pickticketId,
+  parcelId,
   {
     credsPath,
   } = {},
 ) => {
 
   const response = await bleckmannClient.fetch({
-    url: `/skus/${ encodeURIComponent(sku) }`,
+    url: `/warehousing/picktickets/${ pickticketId }/parcels/${ parcelId }`,
+    context: {
+      credsPath,
+    },
   });
 
   logDeep(response);
@@ -20,19 +24,22 @@ const bleckmannParcelGet = async (
 
 const bleckmannParcelGetApi = async (req, res) => {
   const { 
-    sku,
+    pickticketId,
+    parcelId,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'sku', sku),
+    mandateParam(res, 'pickticketId', pickticketId),
+    mandateParam(res, 'parcelId', parcelId),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await bleckmannParcelGet(
-    sku,
+    pickticketId,
+    parcelId,
     options,
   );
   respond(res, 200, result);
@@ -43,4 +50,4 @@ module.exports = {
   bleckmannParcelGetApi,
 };
 
-// curl localhost:8000/bleckmannParcelGet -H "Content-Type: application/json" -d '{ "sku": "EXD1224-3-3XS/XXS" }'
+// curl localhost:8000/bleckmannParcelGet -H "Content-Type: application/json" -d '{ "pickticketId": ..., "parcelId": ... }'
