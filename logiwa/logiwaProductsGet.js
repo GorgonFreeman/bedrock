6521539c1f2ae2e8f8348f2efb65b6e2c51735b9
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-14 09:58:20 +1000

    logiwaProductsGet working fetch

--- logiwa/logiwaProductsGet.js
+++ logiwa/logiwaProductsGet.js
@@ -1,39 +1,47 @@
 // https://mydeveloper.logiwa.com/#tag/Product/paths/~1v3.1~1Product~1list~1i~1%7Bindex%7D~1s~1%7Bsize%7D/get
 
 const { respond, mandateParam, logDeep } = require('../utils');
-const { logiwaClient } = require('../logiwa/logiwa.utils');
+const { logiwaGet } = require('../logiwa/logiwa.utils');
+const { MAX_PER_PAGE } = require('../logiwa/logiwa.constants');
 
 const logiwaProductsGet = async (
-  orderId,
   {
     credsPath,
     apiVersion = 'v3.1',
+
+    page = 0,
+    perPage = MAX_PER_PAGE,
+
+    ...getterOptions
   } = {},
 ) => {
 
-  const response = await logiwaClient.fetch({
-    method: 'get',
-    url: `/ShipmentOrder/${ orderId }`,
-  });
+  const response = await logiwaGet(
+    `/Product/list/i/${ page }/s/${ perPage }`,
+    {
+      credsPath,
+      apiVersion,
+      // params,
+      ...getterOptions,
+    },
+  );
   logDeep(response);
   return response;
 };
 
 const logiwaProductsGetApi = async (req, res) => {
   const { 
-    orderId,
     options,
   } = req.body;
 
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'orderId', orderId),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
+  // const paramsValid = await Promise.all([
+  //   mandateParam(res, 'orderId', orderId),
+  // ]);
+  // if (paramsValid.some(valid => valid === false)) {
+  //   return;
+  // }
 
   const result = await logiwaProductsGet(
-    orderId,
     options,
   );
   respond(res, 200, result);
@@ -44,4 +52,5 @@ module.exports = {
   logiwaProductsGetApi,
 };
 
-// curl localhost:8000/logiwaProductsGet -H "Content-Type: application/json" -d '{ "orderId": "9ce5f6f0-c461-4d1c-93df-261a2188d652" }'
+// curl localhost:8000/logiwaProductsGet
+// curl localhost:8000/logiwaProductsGet -H "Content-Type: application/json" -d '{ "options": { "limit": 10 } }'
