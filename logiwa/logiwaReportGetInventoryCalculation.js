254740e7c6fd8e7b4929a358ff548f110e838e48
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-14 11:06:50 +1000

    logiwaReportGetInventoryCalculation attempt

--- logiwa/logiwaReportGetInventoryCalculation.js
+++ logiwa/logiwaReportGetInventoryCalculation.js
@@ -1,17 +1,11 @@
-// https://mydeveloper.logiwa.com/#tag/Report/paths/~1v3.1~1Report~1InventorySnapshot~1i~1%7Bindex%7D~1s~1%7Bsize%7D/get
+// https://mydeveloper.logiwa.com/#tag/Report/paths/~1v3.1~1Report~1InventoryCalculation~1i~1%7Bindex%7D~1s~1%7Bsize%7D/get
 
 const { respond, mandateParam, logDeep, objHasAny } = require('../utils');
 const { logiwaGet } = require('../logiwa/logiwa.utils');
 const { MAX_PER_PAGE } = require('../logiwa/logiwa.constants');
 
 const logiwaReportGetInventoryCalculation = async (
-  {
-    warehouseIdentifier_eq,
-    clientIdentifier_eq,
-    locationIdentifier_eq,
-    sku_eq,
-    snapshotDate_bt,
-  },
+  warehouseIdentifier_eq,
   {
     credsPath,
     apiVersion = 'v3.1',
@@ -24,15 +18,11 @@ const logiwaReportGetInventoryCalculation = async (
 ) => {
 
   const params = {
-    ...(warehouseIdentifier_eq && { 'WarehouseIdentifier.eq': warehouseIdentifier_eq }),
-    ...(clientIdentifier_eq && { 'ClientIdentifier.eq': clientIdentifier_eq }),
-    ...(locationIdentifier_eq && { 'LocationIdentifier.eq': locationIdentifier_eq }),
-    ...(sku_eq && { 'Sku.eq': sku_eq }),
-    ...(snapshotDate_bt && { 'SnapshotDate.bt': snapshotDate_bt }),
+    'WarehouseIdentifier.eq': warehouseIdentifier_eq,
   };
 
   const response = await logiwaGet(
-    `/Report/InventorySnapshot/i/${ page }/s/${ perPage }`,
+    `/Report/InventoryCalculation/i/${ page }/s/${ perPage }`,
     {
       credsPath,
       apiVersion,
@@ -46,19 +36,19 @@ const logiwaReportGetInventoryCalculation = async (
 
 const logiwaReportGetInventoryCalculationApi = async (req, res) => {
   const { 
-    criteria,
+    warehouseIdentifier_eq,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
-    mandateParam(res, 'criteria', criteria, p => objHasAny(p, ['warehouseIdentifier_eq', 'clientIdentifier_eq', 'locationIdentifier_eq', 'sku_eq', 'snapshotDate_bt'])),
+    mandateParam(res, 'warehouseIdentifier_eq', warehouseIdentifier_eq),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
   }
 
   const result = await logiwaReportGetInventoryCalculation(
-    criteria,
+    warehouseIdentifier_eq,
     options,
   );
   respond(res, 200, result);
@@ -69,4 +59,4 @@ module.exports = {
   logiwaReportGetInventoryCalculationApi,
 };
 
-// curl localhost:8000/logiwaReportGetInventoryCalculation -H "Content-Type: application/json" -d '{ "criteria":{ "clientIdentifier_eq": "9f1ea39a-fccc-48af-8986-a35c34fcef8b" } }'
+// curl localhost:8000/logiwaReportGetInventoryCalculation -H "Content-Type: application/json" -d '{ "warehouseIdentifier_eq": "c347b530-a9cd-45e7-b07e-f3f51d553f47" }'
