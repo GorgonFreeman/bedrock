f1d55e42cbcfe505d13507f655943d525dd0c512
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-18 14:28:52 +1000

    command to deploy all - start looping over functions

--- _deploy_scripts/deployFromHostingYml.js
+++ _deploy_scripts/deployFromHostingYml.js
@@ -3,4 +3,16 @@ const { readFileYaml } = require('../utils');
 (async() => {
   const hostingYml = await readFileYaml('.hosting.yml');
   console.log(hostingYml);
+
+  const { 
+    google_cloud_info: gcloudInfo,
+    functions,
+    groups,
+  } = hostingYml;
+
+  if (process.argv.includes('all')) {
+    for (const [functionName, functionConfig] of Object.entries(functions)) {
+      console.log(functionName, functionConfig);
+    }
+  }
 })();
