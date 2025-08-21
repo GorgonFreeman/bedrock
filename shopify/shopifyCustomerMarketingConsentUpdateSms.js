6a3cc0486fe78ad01fee482be1c116751618e1dc
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-22 09:33:10 +1000

    update docs links

--- shopify/shopifyCustomerMarketingConsentUpdateSms.js
+++ shopify/shopifyCustomerMarketingConsentUpdateSms.js
@@ -1,4 +1,4 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageCreate
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/customersmsmarketingconsentupdate
 
 const { respond, mandateParam, logDeep } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
