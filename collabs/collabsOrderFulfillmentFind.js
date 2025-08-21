fa3807f3e46af2af748886b2d49b52c6bc464c39
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-21 14:55:27 +1000

    args setup, funcApi implementation and shopify order get

--- collabs/collabsOrderFulfillmentFind.js
+++ collabs/collabsOrderFulfillmentFind.js
@@ -1,42 +1,45 @@
-const { respond, mandateParam } = require('../utils');
+const {
+  REGIONS_LOGIWA,
+} = require('../constants');
+
+const { funcApi, logDeep } = require('../utils');
+
+const { shopifyOrderGet } = require('../shopify/shopifyOrderGet');
 
 const collabsOrderFulfillmentFind = async (
-  arg,
+  region,
+  orderId,
   {
     option,
   } = {},
 ) => {
 
-  return { 
-    arg, 
-    option,
-  };
+  const logiwaRelevant = REGIONS_LOGIWA.includes(region);
+  const anyRelevant = [logiwaRelevant].some(Boolean);
+  if (!anyRelevant) {
+    return {
+      success: false,
+      message: 'Region not supported',
+    };
+  }
+
+  const shopifyOrderResponse = await shopifyOrderGet(region, { orderId });
+  logDeep(shopifyOrderResponse);
+  return shopifyOrderResponse;
   
 };
 
-const collabsOrderFulfillmentFindApi = async (req, res) => {
-  const { 
-    arg,
-    options,
-  } = req.body;
-
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'arg', arg),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
-
-  const result = await collabsOrderFulfillmentFind(
-    arg,
-    options,
-  );
-  respond(res, 200, result);
-};
+const collabsOrderFulfillmentFindApi = funcApi(collabsOrderFulfillmentFind, {
+  argNames: ['region', 'orderId', 'options'],
+  validatorsByArg: {
+    region: Boolean,
+    orderId: Boolean,
+  },
+});
 
 module.exports = {
   collabsOrderFulfillmentFind,
   collabsOrderFulfillmentFindApi,
 };
 
-// curl localhost:8000/collabsOrderFulfillmentFind -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsOrderFulfillmentFind -H "Content-Type: application/json" -d '{ "region": "us", "orderId": "5979642789948" }'
