ec0b133784a3c57dde9c818cde431ef27b02ca6c
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-21 13:42:20 +1000

    collabsFulfillmentSweepV2 funcApi and args setup

--- collabs/collabsFulfillmentSweepV2.js
+++ collabs/collabsFulfillmentSweepV2.js
@@ -1,42 +1,28 @@
-const { respond, mandateParam } = require('../utils');
+const { funcApi } = require('../utils');
 
 const collabsFulfillmentSweepV2 = async (
-  arg,
+  region,
   {
     option,
   } = {},
 ) => {
 
-  return { 
-    arg, 
-    option,
+  return {
+    success: true,
   };
   
 };
 
-const collabsFulfillmentSweepV2Api = async (req, res) => {
-  const { 
-    arg,
-    options,
-  } = req.body;
-
-  const paramsValid = await Promise.all([
-    mandateParam(res, 'arg', arg),
-  ]);
-  if (paramsValid.some(valid => valid === false)) {
-    return;
-  }
-
-  const result = await collabsFulfillmentSweepV2(
-    arg,
-    options,
-  );
-  respond(res, 200, result);
-};
+const collabsFulfillmentSweepV2Api = funcApi(collabsFulfillmentSweepV2, {
+  argNames: ['region', 'options'],
+  validatorsByArg: {
+    region: Boolean,
+  },
+});
 
 module.exports = {
   collabsFulfillmentSweepV2,
   collabsFulfillmentSweepV2Api,
 };
 
-// curl localhost:8000/collabsFulfillmentSweepV2 -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsFulfillmentSweepV2 -H "Content-Type: application/json" -d '{ "region": "us" }'
