c5d51c7a4dc76c8e4efed433d8b034732b0b4a66
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-22 10:27:50 +1000

    shopifyCustomerMarketingConsentUpdateSms complete function

--- shopify/shopifyCustomerMarketingConsentUpdateSms.js
+++ shopify/shopifyCustomerMarketingConsentUpdateSms.js
@@ -3,27 +3,42 @@
 const { respond, mandateParam, logDeep } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
 
-const defaultAttrs = `id title handle`;
+const defaultAttrs = `defaultPhoneNumber { marketingState }`;
 
 const shopifyCustomerMarketingConsentUpdateSms = async (
   credsPath,
-  pageInput,
+  customerId,
+  marketingState, // SUBSCRIBED, UNSUBSCRIBED
   {
     apiVersion,
     returnAttrs = defaultAttrs,
+
+    marketingOptInLevel = 'UNKNOWN', // CONFIRMED_OPT_IN, SINGLE_OPT_IN, UNKNOWN
+    consentUpdatedAt,
+    sourceLocationId,
   } = {},
 ) => {
 
+  const customerGid = `gid://shopify/Customer/${ customerId }`;
+
   const response = await shopifyMutationDo(
     credsPath,
-    'pageCreate',
+    'customerSmsMarketingConsentUpdate',
     {
-      page: {
-        type: 'PageCreateInput!',
-        value: pageInput,
+      input: {
+        type: 'CustomerSmsMarketingConsentUpdateInput!',
+        value: {
+          customerId: customerGid,
+          smsMarketingConsent: {
+            ...(marketingState && { marketingState }),
+            ...(marketingOptInLevel && { marketingOptInLevel }),
+            ...(consentUpdatedAt && { consentUpdatedAt }),
+            ...(sourceLocationId && { sourceLocationId }),
+          },
+        },
       },
     },
-    `page { ${ returnAttrs } }`,
+    `customer { ${ returnAttrs } }`,
     { 
       apiVersion,
     },
@@ -35,13 +50,15 @@ const shopifyCustomerMarketingConsentUpdateSms = async (
 const shopifyCustomerMarketingConsentUpdateSmsApi = async (req, res) => {
   const {
     credsPath,
-    pageInput,
+    customerId,
+    marketingState,
     options,
   } = req.body;
 
   const paramsValid = await Promise.all([
     mandateParam(res, 'credsPath', credsPath),
-    mandateParam(res, 'pageInput', pageInput),
+    mandateParam(res, 'customerId', customerId),
+    mandateParam(res, 'marketingState', marketingState),
   ]);
   if (paramsValid.some(valid => valid === false)) {
     return;
@@ -49,7 +66,8 @@ const shopifyCustomerMarketingConsentUpdateSmsApi = async (req, res) => {
 
   const result = await shopifyCustomerMarketingConsentUpdateSms(
     credsPath,
-    pageInput,
+    customerId,
+    marketingState,
     options,
   );
   respond(res, 200, result);
@@ -60,4 +78,5 @@ module.exports = {
   shopifyCustomerMarketingConsentUpdateSmsApi,
 };
 
-// curl http://localhost:8000/shopifyCustomerMarketingConsentUpdateSms -H 'Content-Type: application/json' -d '{ "credsPath": "au", "pageInput": { "title": "Batarang Blueprints", "body": "<strong>Good page!</strong>" }, "options": { "returnAttrs": "id" } }'
+// curl http://localhost:8000/shopifyCustomerMarketingConsentUpdateSms -H 'Content-Type: application/json' -d '{ "credsPath": "au", "customerId": "8575963103304", "marketingState": "SUBSCRIBED" }'
+// curl http://localhost:8000/shopifyCustomerMarketingConsentUpdateSms -H 'Content-Type: application/json' -d '{ "credsPath": "au", "customerId": "8575963103304", "marketingState": "UNSUBSCRIBED" }'
