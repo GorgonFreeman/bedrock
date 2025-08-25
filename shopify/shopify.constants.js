ce347838ff0dd658626081b867923c82ac073aeb
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-25 14:39:19 +1000

    hostname to creds mapping in Shopify constants

--- shopify/shopify.constants.js
+++ shopify/shopify.constants.js
@@ -1,3 +1,8 @@
 module.exports = {
   REGIONS_WF: ['au', 'us', 'uk'],
+  HOSTNAME_TO_CREDSPATH: {
+    'whitefoxboutique.com.au': 'au',
+    'whitefoxboutique.com': 'us',
+    'whitefoxboutique.co.uk': 'uk',
+  },
 };
