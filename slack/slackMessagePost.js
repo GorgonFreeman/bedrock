df0acf6b1fad8dccb6cfd5243aa846eac868e38a
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-27 00:01:18 +1000

    groom slackMessagePost

--- slack/slackMessagePost.js
+++ slack/slackMessagePost.js
@@ -1,6 +1,6 @@
 // https://docs.slack.dev/reference/methods/chat.postmessage
 
-const { funcApi, objHasAny, credsByPath, customAxios } = require('../utils');
+const { funcApi, objHasAny, credsByPath, customAxios, logDeep } = require('../utils');
 
 const slackMessagePost = async (
   channel,
@@ -18,56 +18,25 @@ const slackMessagePost = async (
   const { 
     BASE_URL,
     BOT_TOKEN,
-    SIGNING_SECRET,
+    // SIGNING_SECRET,
   } = creds;
- 
-  // Build the message payload
-  const messagePayload = {};
   
-  if (text) {
-    messagePayload.text = text;
-  }
-  
-  if (blocks) {
-    messagePayload.blocks = blocks;
-  }
-  
-  if (markdownText) {
-    messagePayload.text = markdownText;
-    messagePayload.mrkdwn = true;
-  }
-  
-  // Ensure we have a channel and at least one message type
-  if (!channel) {
-    throw new Error('Channel is required');
-  }
-  
-  if (!objHasAny(messagePayload, ['text', 'blocks'])) {
-    throw new Error('At least one of text, blocks, or markdownText is required');
-  }
-  
-  // Set the channel
-  messagePayload.channel = channel;
-  
-  console.log('slackMessagePost payload', messagePayload);
-  
-  // Make the API call to Slack
   const response = await customAxios(`${ BASE_URL }/chat.postMessage`, {
     method: 'post',
     headers: {
       'Authorization': `Bearer ${ BOT_TOKEN }`,
       'Content-Type': 'application/json',
     },
-    body: messagePayload,
+    body: {
+      channel,
+      ...text && { text },
+      ...blocks && { blocks },
+      ...markdownText && { markdown_text: markdownText },
+    },
   });
   
-  if (!response.success) {
-    console.error('slackMessagePost error', response.error);
-    throw new Error('Failed to post message to Slack');
-  }
-  
-  console.log('slackMessagePost response', response.result);
-  return response.result;
+  logDeep(response);
+  return response;
 };
 
 const slackMessagePostApi = funcApi(slackMessagePost, {
