9a7e574917f63dc712cd8aa7b872c1858e3121bf
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-27 12:07:07 +1000

    set up structure for routing based on slack command
    consider not tying functions to commands, since they can be triggered other ways

---
+++ slack/routed/test.js
@@ -0,0 +1,8 @@
+const { respond } = require('../../utils');
+
+const slackInteractiveTest = async (req, res) => {
+  logDeep('slackInteractiveTest', req.body);
+  return respond(res, 200, { ok: true });
+};
+
+module.exports = slackInteractiveTest;
--- slack/slackWebhookRouter.js
+++ slack/slackWebhookRouter.js
@@ -4,15 +4,20 @@ const { TEAM_DOMAIN_TO_CREDSPATH } = require('../slack/slack.constants');
 const slackWebhookRouterApi = async (req, res) => {
   // TODO: Verify request is from Slack
 
-  const { team_domain: teamDomain } = req.body;
+  logDeep(req.body);
+
+  const { 
+    team_domain: teamDomain,
+    command,
+  } = req.body;
   const credsPath = TEAM_DOMAIN_TO_CREDSPATH[teamDomain];
 
   if (!credsPath) {
     return respond(res, 400, { error: `No creds path found for team domain` });
   }
 
-  logDeep(req.body);
-  respond(res, 200, { ok: true });
+  const interactivityFunc = require(`../slack/routed/${ command }`);
+  return interactivityFunc(req, res);
 };
 
 module.exports = {
