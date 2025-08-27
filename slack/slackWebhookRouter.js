d4e0609e20842d11a310f803a2157f55015f9047
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-27 11:36:57 +1000

    fix slackWebhookRouter not giving a response

--- slack/slackWebhookRouter.js
+++ slack/slackWebhookRouter.js
@@ -1,8 +1,9 @@
-const { logDeep } = require('../utils');
+const { respond, logDeep } = require('../utils');
 
 const slackWebhookRouterApi = async (req, res) => {
   // TODO: Verify request is from Slack
   logDeep(req.body);
+  respond(res, 200, { ok: true });
 };
 
 module.exports = {
