fe3347cb8b695be6b38a5bea2fe8c5c2d72aa559
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-08-27 13:25:07 +1000

    prepare to receive payloads initial

--- slack/slackWebhookRouter.js
+++ slack/slackWebhookRouter.js
@@ -8,24 +8,50 @@ const slashCommandToFunc = {
 const slackWebhookRouterApi = async (req, res) => {
   // TODO: Verify request is from Slack
 
-  logDeep(req.body);
+  let credsPath;
+  let slashCommand;
 
-  const { 
-    team_domain: teamDomain,
-    command,
-  } = req.body;
-  const credsPath = TEAM_DOMAIN_TO_CREDSPATH[teamDomain];
-  console.log(teamDomain, credsPath);
+  const { body } = req;
+  
+  if (body?.payload) {
+
+    console.log('Received payload - handling as interactivity step');
+
+    const payload = JSON.parse(body.payload);
+    logDeep('payload', payload);
+
+    const { team } = payload;
+    const { domain: teamDomain } = team;
+
+    credsPath = TEAM_DOMAIN_TO_CREDSPATH[teamDomain];
+
+    // TODO: Get slashCommand from action_id
+
+  } else {
+
+    console.log('No payload - handling as slash command');
+    logDeep('body', body);
+
+    const { 
+      team_domain: teamDomain,
+      command,
+    } = body;
+
+    credsPath = TEAM_DOMAIN_TO_CREDSPATH[teamDomain];
+
+    slashCommand = command;
+
+  }
 
   if (credsPath === null) { // '' is a valid return for base path
     return respond(res, 400, { error: `No creds path found for team domain` });
   }
 
-  const interactivityFunc = slashCommandToFunc[command];
+  const interactivityFunc = slashCommandToFunc[slashCommand];
   console.log('interactivityFunc', interactivityFunc);
 
   if (!interactivityFunc) {
-    return respond(res, 400, { error: `No function found for command "${ command }"` });
+    return respond(res, 400, { error: `No function found for command "${ slashCommand }"` });
   }
 
   return interactivityFunc(req, res);
