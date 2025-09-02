4b021d6f72a7f6045e6c858c15a83d094e90e998
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-03 00:38:54 +1000

    get listings and compare inventory quantity

--- etsy/etsyListingsInventoryReplenishArbitrary.js
+++ etsy/etsyListingsInventoryReplenishArbitrary.js
@@ -1,4 +1,5 @@
 const { funcApi, logDeep } = require('../utils');
+const { etsyShopListingsGet } = require('../etsy/etsyShopListingsGet');
 
 const etsyListingsInventoryReplenishArbitrary = async (
   {
@@ -8,9 +9,25 @@ const etsyListingsInventoryReplenishArbitrary = async (
     credsPath,
   } = {},
 ) => {
-  const response = true;
-  logDeep(response);
-  return response;
+
+  const listingsResponse = await etsyShopListingsGet({ credsPath });
+  const { success: listingsSuccess, result: listings } = listingsResponse;
+  if (!listingsSuccess) {
+    return listingsResponse;
+  }
+
+  const eligibleListings = listings.filter(listing => listing.quantity <= eligibleStockMax);
+
+  if (!eligibleListings?.length) {
+    return {
+      success: true,
+      result: 'No eligible listings found',
+    };
+  }
+
+  console.log(eligibleListings.length);
+  
+  return true;
 };
 
 const etsyListingsInventoryReplenishArbitraryApi = funcApi(etsyListingsInventoryReplenishArbitrary, {
@@ -23,3 +40,4 @@ module.exports = {
 };
 
 // curl localhost:8000/etsyListingsInventoryReplenishArbitrary
+// curl localhost:8000/etsyListingsInventoryReplenishArbitrary -H "Content-Type: application/json" -d '{ "options": { "eligibleStockMax": 4 } }'
