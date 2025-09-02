e35914929da1f654aa8666f619f0e9d7041bd599
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-03 01:50:18 +1000

    etsyShopListingsUpdatePrice complete function

--- etsy/etsyShopListingsUpdatePrice.js
+++ etsy/etsyShopListingsUpdatePrice.js
@@ -1,4 +1,4 @@
-const { funcApi, logDeep } = require('../utils');
+const { funcApi, logDeep, askQuestion, arrayStandardResponse } = require('../utils');
 const { etsyShopListingsGet } = require('../etsy/etsyShopListingsGet');
 const { etsyListingInventoryUpdate } = require('../etsy/etsyListingInventoryUpdate');
 
@@ -8,9 +8,42 @@ const etsyShopListingsUpdatePrice = async (
     credsPath,
   } = {},
 ) => {
-  const response = true;
-  logDeep(response);
-  return response;
+
+  const listingsResponse = await etsyShopListingsGet({ credsPath, includes: ['Inventory'] });
+  const { success: listingsSuccess, result: listings } = listingsResponse;
+  if (!listingsSuccess) {
+    return listingsResponse;
+  }
+
+  const responses = [];
+  for (const listing of listings) {
+
+    const { 
+      listing_id: listingId, 
+      inventory,
+    } = listing;
+    const { products } = inventory;
+
+    const priceUpdatePayload = {
+      products: products.map(({ offerings }) => ({
+        offerings: offerings.map(({ quantity, is_enabled, readiness_state_id }) => ({
+          price: priceDecimal,
+          is_enabled,
+          readiness_state_id,
+          quantity,
+        })),
+      })),
+    };
+    logDeep(priceUpdatePayload);
+    await askQuestion('?');
+
+    const response = await etsyListingInventoryUpdate(listingId, priceUpdatePayload, { credsPath });
+    logDeep(response);
+    await askQuestion('?');
+    responses.push(response);
+  }
+
+  return arrayStandardResponse(responses);
 };
 
 const etsyShopListingsUpdatePriceApi = funcApi(etsyShopListingsUpdatePrice,  {
