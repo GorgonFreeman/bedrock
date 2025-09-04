73fe66dff60ca464206fdfce201b936d20fa3dd8
Zwe <zwe@whitefoxboutique.com>
2025-09-04 11:13:33 +1000

    shopifyCustomerDataErasureRequest stub

---
+++ shopify/shopifyCustomerDataErasureRequest.js
@@ -0,0 +1,63 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageCreate
+
+const { respond, mandateParam, logDeep } = require('../utils');
+const { shopifyMutationDo } = require('../shopify/shopify.utils');
+
+const defaultAttrs = `id title handle`;
+
+const shopifyCustomerDataErasureRequest = async (
+  credsPath,
+  pageInput,
+  {
+    apiVersion,
+    returnAttrs = defaultAttrs,
+  } = {},
+) => {
+
+  const response = await shopifyMutationDo(
+    credsPath,
+    'pageCreate',
+    {
+      page: {
+        type: 'PageCreateInput!',
+        value: pageInput,
+      },
+    },
+    `page { ${ returnAttrs } }`,
+    { 
+      apiVersion,
+    },
+  );
+  logDeep(response);
+  return response;
+};
+
+const shopifyCustomerDataErasureRequestApi = async (req, res) => {
+  const {
+    credsPath,
+    pageInput,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'credsPath', credsPath),
+    mandateParam(res, 'pageInput', pageInput),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await shopifyCustomerDataErasureRequest(
+    credsPath,
+    pageInput,
+    options,
+  );
+  respond(res, 200, result);
+};
+
+module.exports = {
+  shopifyCustomerDataErasureRequest,
+  shopifyCustomerDataErasureRequestApi,
+};
+
+// curl http://localhost:8000/shopifyCustomerDataErasureRequest -H 'Content-Type: application/json' -d '{ "credsPath": "au", "pageInput": { "title": "Batarang Blueprints", "body": "<strong>Good page!</strong>" }, "options": { "returnAttrs": "id" } }'
