d7bded1755c0969720c8d5cef932bb32e11b9529
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-09 23:43:15 +1000

    attempt to get theme by name

--- shopify/shopifyThemeGet.js
+++ shopify/shopifyThemeGet.js
@@ -1,7 +1,8 @@
 // https://shopify.dev/docs/api/admin-graphql/latest/queries/theme
 
-const { logDeep, funcApi } = require('../utils');
+const { logDeep, funcApi, objHasAny, standardInterpreters } = require('../utils');
 const { shopifyGetSingle } = require('../shopify/shopifyGetSingle');
+const { shopifyThemesGet } = require('../shopify/shopifyThemesGet');
 
 const defaultAttrs = `id name role`;
 
@@ -9,6 +10,7 @@ const shopifyThemeGet = async (
   credsPath,
   {
     themeId,
+    themeName,
   },
   {
     attrs = defaultAttrs,
@@ -16,26 +18,44 @@ const shopifyThemeGet = async (
   } = {},
 ) => {
 
-  const response = await shopifyGetSingle(
-    credsPath,
-    'theme',
-    themeId,
-    {
-      gidType: 'OnlineStoreTheme',
+  if (themeId) {
+    const response = await shopifyGetSingle(
+      credsPath,
+      'theme',
+      themeId,
+      {
+        gidType: 'OnlineStoreTheme',
+        apiVersion,
+        attrs,
+      },
+    );
+  
+    logDeep(response);
+    return response;
+  }
+
+  if (themeName) {
+    const themesResponse = await shopifyThemesGet(credsPath, {
       apiVersion,
       attrs,
-    },
-  );
+      names: [themeName],
+    });
+    const response = standardInterpreters.expectOne(themesResponse);
+    logDeep(response);
+    return response;
+  }
 
-  logDeep(response);
-  return response;
+  return {
+    success: false,
+    error: ['No theme identifier provided'],
+  }
 };
 
 const shopifyThemeGetApi = funcApi(shopifyThemeGet, {
   argNames: ['credsPath', 'themeIdentifier', 'options'],
   validatorsByArg: {
     credsPath: Boolean,
-    themeIdentifier: Boolean,
+    themeIdentifier: p => objHasAny(p, ['themeId', 'themeName']),
   },
 });
 
@@ -45,3 +65,4 @@ module.exports = {
 };
 
 // curl localhost:8000/shopifyThemeGet -H "Content-Type: application/json" -d '{ "credsPath": "us", "themeIdentifier": { "themeId": "142724202556" } }'
+// curl localhost:8000/shopifyThemeGet -H "Content-Type: application/json" -d '{ "credsPath": "us", "themeIdentifier": { "themeName": "Post Sale- Wk 36" } }'
