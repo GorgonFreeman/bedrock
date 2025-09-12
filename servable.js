68b1e4689aff7de0c1bb3910fb8bcdb0ad9371bd
Zwe <zwe@whitefoxboutique.com>
2025-09-12 11:02:06 +1000

    shopifyCustomerSegmentMembersGet stub

--- servable.js
+++ servable.js
@@ -42,6 +42,7 @@ module.exports = {
   shopifyCustomerGet: require('./shopify/shopifyCustomerGet').shopifyCustomerGetApi,
   shopifyCustomerMarketingConsentUpdateEmail: require('./shopify/shopifyCustomerMarketingConsentUpdateEmail').shopifyCustomerMarketingConsentUpdateEmailApi,
   shopifyCustomerMarketingConsentUpdateSms: require('./shopify/shopifyCustomerMarketingConsentUpdateSms').shopifyCustomerMarketingConsentUpdateSmsApi,
+  shopifyCustomerSegmentMembersGet: require('./shopify/shopifyCustomerSegmentMembersGet').shopifyCustomerSegmentMembersGetApi,
   shopifyCustomerStoreCreditZero: require('./shopify/shopifyCustomerStoreCreditZero').shopifyCustomerStoreCreditZeroApi,
   shopifyCustomerUpdate: require('./shopify/shopifyCustomerUpdate').shopifyCustomerUpdateApi,
   shopifyCustomerUpsert: require('./shopify/shopifyCustomerUpsert').shopifyCustomerUpsertApi,
