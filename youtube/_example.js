6201ca5321ec82b34e2e832a781880e6967c9bb2
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-14 17:53:35 +1000

    compose youtube _example.js

---
+++ youtube/_example.js
@@ -0,0 +1,46 @@
+const { funcApi, logDeep, customAxios, credsByPath } = require('../utils');
+
+const FUNC = async (
+  query,
+  {
+    credsPath,
+    maxResults = 5,
+    order = 'relevance',
+    type = 'video',
+  } = {},
+) => {
+  const creds = credsByPath(['youtube', credsPath]);
+  const { BASE_URL, API_KEY } = creds;
+  
+  const url = `${ BASE_URL }/search`;
+  const params = {
+    part: 'snippet',
+    q: query,
+    maxResults,
+    order,
+    type,
+    key: API_KEY,
+  };
+
+  const response = await customAxios(url, {
+    method: 'get',
+    params,
+  });
+  
+  logDeep(response);
+  return response;
+};
+
+const FUNCApi = funcApi(FUNC, {
+  argNames: ['query', 'options'],
+  validatorsByArg: {
+    query: Boolean,
+  },
+});
+
+module.exports = {
+  FUNC,
+  FUNCApi,
+};
+
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "query": "machine learning" }'
