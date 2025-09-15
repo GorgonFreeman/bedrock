d7ba83e485c032610e00e5f4c8e98c476cbb2bdd
Zwe <zwe@whitefoxboutique.com>
2025-09-15 12:24:59 +1000

    shopifyMetafieldGet stub

---
+++ shopify/shopifyMetafieldGet.js
@@ -0,0 +1,53 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/orders
+
+const { respond, mandateParam, logDeep } = require('../utils');
+const { shopifyGet } = require('../shopify/shopify.utils');
+
+const defaultAttrs = `id name`;
+
+const shopifyMetafieldGet = async (
+  credsPath,
+  {
+    attrs = defaultAttrs,
+    ...options
+  } = {},
+) => {
+
+  const response = await shopifyGet(
+    credsPath, 
+    'order', 
+    {
+      attrs,
+      ...options,
+    },
+  );
+
+  return response;
+};
+
+const shopifyMetafieldGetApi = async (req, res) => {
+  const { 
+    credsPath,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'credsPath', credsPath),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await shopifyMetafieldGet(
+    credsPath,
+    options,
+  );
+  respond(res, 200, result);
+};
+
+module.exports = {
+  shopifyMetafieldGet,
+  shopifyMetafieldGetApi,
+};
+
+// curl localhost:8000/shopifyMetafieldGet -H "Content-Type: application/json" -d '{ "credsPath": "au", "options": { "limit": 2 } }'
