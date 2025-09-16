1bef03a54bed1d8cd793cdd8ddb107fc747ef2b5
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-16 14:00:00 +1000

    check platform relevance

--- collabs/collabsFulfillmentSweepRecent.js
+++ collabs/collabsFulfillmentSweepRecent.js
@@ -1,6 +1,12 @@
+// Action fulfillments for any recently fulfilled orders. Purely platform > Shopify.
+
 const { funcApi } = require('../utils');
 const {
   REGIONS_ALL,
+  REGIONS_PVX,
+  REGIONS_LOGIWA,
+  REGIONS_BLECKMANN,
+  REGIONS_STARSHIPIT,
 } = require('../constants');
 
 const collabsFulfillmentSweepRecent = async (
@@ -8,6 +14,19 @@ const collabsFulfillmentSweepRecent = async (
     regions = REGIONS_ALL,
   } = {},
 ) => {
+  
+  const peoplevoxRelevant = regions.some(region => REGIONS_PVX.includes(region));
+  const logiwaRelevant = regions.some(region => REGIONS_LOGIWA.includes(region));
+  const bleckmannRelevant = regions.some(region => REGIONS_BLECKMANN.includes(region));
+  const starshipitRelevant = regions.some(region => REGIONS_STARSHIPIT.includes(region));
+  
+  const anyRelevant = [peoplevoxRelevant, logiwaRelevant, bleckmannRelevant, starshipitRelevant].some(Boolean);
+  if (!anyRelevant) {
+    return {
+      success: false,
+      message: ['No regions supported'],
+    };
+  }
 
   return { 
     regions, 
