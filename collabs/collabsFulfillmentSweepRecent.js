d58fe2b5273ec2b4df14a518201658cf723d851d
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-16 14:10:55 +1000

    get recent peoplevox dispatches

--- collabs/collabsFulfillmentSweepRecent.js
+++ collabs/collabsFulfillmentSweepRecent.js
@@ -1,6 +1,6 @@
 // Action fulfillments for any recently fulfilled orders. Purely platform > Shopify.
 
-const { funcApi } = require('../utils');
+const { funcApi, dateTimeFromNow, days, logDeep, askQuestion } = require('../utils');
 const {
   REGIONS_ALL,
   REGIONS_PVX,
@@ -9,17 +9,53 @@ const {
   REGIONS_STARSHIPIT,
 } = require('../constants');
 
+const { peoplevoxReportGet } = require('../peoplevox/peoplevoxReportGet');
+const { peoplevoxDateFormatter } = require('../peoplevox/peoplevox.utils');
+
 const collabsFulfillmentSweepRecent = async (
   {
     regions = REGIONS_ALL,
   } = {},
 ) => {
+
+  const recentWindowStartDate = dateTimeFromNow({ minus: days(2), dateOnly: true });
+  const now = dateTimeFromNow({ dateOnly: true });
+
+  const peoplevoxGetRecent = async () => {
+    const peoplevoxRecentDispatchesResponse = await peoplevoxReportGet('Despatch summary', { 
+      columns: ['Salesorder number', 'Carrier', 'Tracking number', 'Despatch date'], 
+      searchClause: `([Despatch date] >= ${ peoplevoxDateFormatter(recentWindowStartDate) })`, 
+    });
+
+    const { 
+      success: peoplevoxRecentDispatchesSuccess, 
+      result: peoplevoxRecentDispatches,
+    } = peoplevoxRecentDispatchesResponse;
+
+    if (!peoplevoxRecentDispatchesSuccess) {
+      return peoplevoxRecentDispatchesResponse;
+    }
+
+    return peoplevoxRecentDispatches;
+  };
+
+  const logiwaGetRecent = async () => {
+    
+  };
+
+  const bleckmannGetRecent = async () => {
+
+  };
+
+  const starshipitGetRecent = async () => {
+
+  };
   
   const peoplevoxRelevant = regions.some(region => REGIONS_PVX.includes(region));
   const logiwaRelevant = regions.some(region => REGIONS_LOGIWA.includes(region));
   const bleckmannRelevant = regions.some(region => REGIONS_BLECKMANN.includes(region));
   const starshipitRelevant = regions.some(region => REGIONS_STARSHIPIT.includes(region));
-  
+
   const anyRelevant = [peoplevoxRelevant, logiwaRelevant, bleckmannRelevant, starshipitRelevant].some(Boolean);
   if (!anyRelevant) {
     return {
@@ -27,6 +63,27 @@ const collabsFulfillmentSweepRecent = async (
       message: ['No regions supported'],
     };
   }
+
+  const [
+    peoplevoxRecentDispatches,
+    starshipitRecentDispatches,
+    logiwaRecentDispatches,
+    bleckmannRecentDispatches,
+  ] = await Promise.all([
+    ...(peoplevoxRelevant ? [peoplevoxGetRecent()] : [false]),
+    ...(starshipitRelevant ? [starshipitGetRecent()] : [false]),
+    ...(logiwaRelevant ? [logiwaGetRecent()] : [false]),
+    ...(bleckmannRelevant ? [bleckmannGetRecent()] : [false]),
+  ]);
+
+  logDeep(peoplevoxRecentDispatches);
+  await askQuestion('?');
+  logDeep(starshipitRecentDispatches);
+  await askQuestion('?');
+  logDeep(logiwaRecentDispatches);
+  await askQuestion('?');
+  logDeep(bleckmannRecentDispatches);
+  await askQuestion('?');
 
   return { 
     regions, 
@@ -43,4 +100,4 @@ module.exports = {
   collabsFulfillmentSweepRecentApi,
 };
 
-// curl localhost:8000/collabsFulfillmentSweepRecent -H "Content-Type: application/json" -d '{ "options": { "regions": ["au", "us"] } }'
+// curl localhost:8000/collabsFulfillmentSweepRecent -H "Content-Type: application/json" -d '{ "options": { "regions": ["au"] } }'
