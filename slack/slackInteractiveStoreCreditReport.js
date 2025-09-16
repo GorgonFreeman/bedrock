7b56e4002a7ba245a83bff3812e4263677289f80
Zwe <zwe@whitefoxboutique.com>
2025-09-16 10:33:11 +1000

    add imports

--- slack/slackInteractiveStoreCreditReport.js
+++ slack/slackInteractiveStoreCreditReport.js
@@ -1,19 +1,22 @@
 // https://docs.slack.dev/reference/methods/chat.delete
 
-const { funcApi, logDeep } = require('../utils');
-const { slackClient } = require('../slack/slack.utils');
+const { respond, logDeep, customAxios } = require('../utils');
+
+const ACTION_NAME = 'store_credit_report';
+
+const 
 
 const slackInteractiveStoreCreditReport = async (req, res) => {
   console.log('slackInteractiveStoreCreditReport');
 };
 
-const slackInteractiveStoreCreditReportInitiator = async (req, res) => {
+const slackInteractiveStoreCreditReportInitiator = async (credsPath, customer) => {
   console.log('slackInteractiveStoreCreditReportInitiator');
 };
 
 module.exports = {
+  initiator: slackInteractiveStoreCreditReportInitiator,
   slackInteractiveStoreCreditReport,
-  slackInteractiveStoreCreditReportApi,
 };
 
 // curl localhost:8000/slackInteractiveStoreCreditReport -H "Content-Type: application/json" -d '{ "channelId": "C06GAG30145", "timestamp": "1756218276.372199" }'
