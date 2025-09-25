6f2491aacba7f04223b3c28daf9b8692177b7890
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-09-25 14:02:09 +1000

    shopify get single example function

---
+++ shopify/_example.getsingle.js
@@ -0,0 +1,64 @@
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/order
+
+const { funcApi, logDeep, actionMultipleOrSingle } = require('../utils');
+const { shopifyGetSingle } = require('../shopify/shopifyGetSingle');
+
+const defaultAttrs = `id name`;
+
+const FUNCSingle = async (
+  credsPath,
+  thingId,
+  {
+    apiVersion,
+    attrs = defaultAttrs,
+  } = {},
+) => {
+  
+  const response = await shopifyGetSingle(
+    credsPath,
+    'thing',
+    thingId,
+    {
+      apiVersion,
+      attrs,
+    },
+  );
+  
+  logDeep(response);
+  return response;
+};
+
+const FUNC = async (
+  credsPath,
+  thingId,
+  {
+    queueRunOptions,
+    ...options
+  } = {},
+) => {
+  const response = await actionMultipleOrSingle(
+    thingId,
+    FUNCSingle,
+    (thingId) => ({
+      args: [credsPath, thingId],
+      options,
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
+    },
+  );
+  
+  logDeep(response);
+  return response;
+};
+
+const FUNCApi = funcApi(FUNC, {
+  argNames: ['credsPath', 'thingId', 'options'],
+});
+
+module.exports = {
+  FUNC,
+  FUNCApi,
+};
+
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "credsPath": "au", "thingId": "7012222266312" }'
