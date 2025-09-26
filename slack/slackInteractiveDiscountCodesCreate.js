d22cec1cd81980de333a4e2438b6facded4f7010
Zwe <zwe@whitefoxboutique.com>
2025-09-26 13:54:00 +1000

    first form view of extracting discount id from url

--- slack/slackInteractiveDiscountCodesCreate.js
+++ slack/slackInteractiveDiscountCodesCreate.js
@@ -1,37 +1,144 @@
-// https://docs.slack.dev/reference/methods/chat.delete
+const { respond, logDeep, customAxios } = require('../utils');
 
-const { funcApi, logDeep } = require('../utils');
-const { slackClient } = require('../slack/slack.utils');
+const ACTION_NAME = 'discount_codes_create';
 
-const slackInteractiveDiscountCodesCreate = async (
-  channelId,
-  timestamp,
-  {
-    credsPath,
-  } = {},
-) => {
-  const response = await slackClient.fetch({
-    url: '/chat.delete',
+// Messages
+const AU_DISCOUNT_URL_LABEL = 'AU Discount URL';
+const AU_DISCOUNT_URL_PLACEHOLDER = 'https://whitefoxboutique.com.au/discount/....';
+const FETCH_DISCOUNT_SETTINGS_BUTTON_TEXT = 'Fetch Discount Settings';
+const CANCEL_BUTTON_TEXT = 'Cancel';
+
+// Blocks
+const dividerBlock = {
+  type: 'divider',
+};
+
+const headerBlock = {
+  type: 'header',
+  text: {
+    type: 'plain_text',
+    text: 'Create Discount Code',
+  },
+};
+
+const auDiscountUrlInputBlock = {
+  type: 'input',
+  block_id: 'discount_url_input',
+  element: {
+    type: 'plain_text_input',
+    placeholder: {
+      type: 'plain_text',
+      text: AU_DISCOUNT_URL_PLACEHOLDER,
+    },
+  },
+  label: {
+    type: 'plain_text',
+    text: AU_DISCOUNT_URL_LABEL,
+  },
+};
+
+const discountSettingsFetchActionBlock = {
+  type: 'actions',
+  elements: [
+    {
+      type: 'button',
+      text: {
+        type: 'plain_text',
+        text: FETCH_DISCOUNT_SETTINGS_BUTTON_TEXT,
+      },
+      value: 'fetch_discount_settings',
+      action_id: `${ ACTION_NAME }:fetch`,
+      style: 'primary',
+    },
+    {
+      type: 'button',
+      text: {
+        type: 'plain_text',
+        text: CANCEL_BUTTON_TEXT,
+      },
+      value: 'cancel',
+      action_id: `${ ACTION_NAME }:cancel`,
+    },
+  ],
+}
+
+const fetchBlocks = [
+  headerBlock,
+  dividerBlock,
+  auDiscountUrlInputBlock,
+  discountSettingsFetchActionBlock,
+];
+
+const slackInteractiveDiscountCodesCreate = async (req, res) => {
+
+  console.log('slackInteractiveDiscountCodesCreate');
+
+  const { body } = req;
+
+  if (!body?.payload) {
+    console.log(`Initiation, e.g. slash command`);
+
+    logDeep('fetchBlocks', fetchBlocks);
+
+    return respond(res, 200, {
+      response_type: 'ephemeral',
+      blocks: fetchBlocks,
+    });
+  }
+  
+  console.log(`Received payload - handling as interactive step`);
+
+  respond(res, 200); // Acknowledgement - we'll provide the next step to the response_url later
+
+  const payload = JSON.parse(body.payload);
+  logDeep('payload', payload);
+
+  const {
+    response_url: responseUrl,
+    state,
+    actions,
+  } = payload;
+
+  const action = actions?.[0];
+  const {
+    action_id: actionId,
+    value: actionValue,
+  } = action;
+
+  let response;
+  let auDiscountUrl;
+
+  switch (actionId) {
+    case `${ ACTION_NAME }:fetch`:
+
+      auDiscountUrl = Object.values(state?.values?.discount_url_input || {})?.[0]?.value;
+      auDiscountId = auDiscountUrl.split('?').shift().split('/').pop();
+
+      console.log('auDiscountUrl', auDiscountUrl);
+      console.log('auDiscountId', auDiscountId);
+      break;
+      
+    case `${ ACTION_NAME }:cancel`:
+      response = {
+        response_type: 'ephemeral',
+        replace_original: 'true',
+        text: `Cancelled`,
+      };
+      break;
+    default:
+      response = {
+        response_type: 'ephemeral',
+        replace_original: 'true',
+        text: `Errored: Unknown action: ${ actionId }`,
+      };
+      break;
+  }
+
+  // logDeep('response', response);
+  return customAxios(responseUrl, {
     method: 'post',
-    body: {
-      channel: channelId,
-      ts: timestamp,
-    },
-    context: {
-      credsPath,
-    },
+    body: response,
   });
-  logDeep(response);
-  return response;
 };
 
-const slackInteractiveDiscountCodesCreateApi = funcApi(slackInteractiveDiscountCodesCreate, {
-  argNames: ['channelId', 'timestamp', 'options'],
-});
-
-module.exports = {
-  slackInteractiveDiscountCodesCreate,
-  slackInteractiveDiscountCodesCreateApi,
-};
-
-// curl localhost:8000/slackInteractiveDiscountCodesCreate -H "Content-Type: application/json" -d '{ "channelId": "C06GAG30145", "timestamp": "1756218276.372199" }'
+module.exports = slackInteractiveDiscountCodesCreate;
--- slack/slackWebhookRouter.js
+++ slack/slackWebhookRouter.js
@@ -4,6 +4,7 @@ const { TEAM_DOMAIN_TO_CREDSPATH } = require('../slack/slack.constants');
 const actionNameToFunc = {
   'test': require('../slack/slackInteractiveTest'),
   'pizza': require('../slack/slackInteractiveTestMulti'),
+  'discount_codes_create': require('../slack/slackInteractiveDiscountCodesCreate'),
 };
 
 const slackWebhookRouterApi = async (req, res) => {
