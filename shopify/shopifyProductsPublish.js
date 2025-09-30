647134a55e917275af02c90d2e173c5c55cec818
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-10-01 01:20:37 +1000

    args setup and gut function

--- shopify/shopifyProductsPublish.js
+++ shopify/shopifyProductsPublish.js
@@ -1,53 +1,19 @@
 const { funcApi, logDeep } = require('../utils');
-const { shopifyClient } = require('../shopify/shopify.utils');
-
-const defaultAttrs = `id`;
+const { shopifyProductsGet } = require('../shopify/shopifyProductsGet');
 
 const shopifyProductsPublish = async (
   credsPath,
-  arg,
   {
     apiVersion,
-    option,
+    fetchOptions,
   } = {},
 ) => {
 
-  const query = `
-    query GetProduct($id: ID!) {
-      product(id: $id) {
-        ${ attrs }
-      }
-    }
-  `;
-
-  const variables = {
-    id: `gid://shopify/Product/${ arg }`,
-  };
-
-  const response = await shopifyClient.fetch({
-    method: 'post',
-    body: { query, variables },
-    context: {
-      credsPath,
-      apiVersion,
-    },
-    interpreter: async (response) => {
-      // console.log(response);
-      return {
-        ...response,
-        ...response.result ? {
-          result: response.result.product,
-        } : {},
-      };
-    },
-  });
-
-  logDeep(response);
-  return response;
+  return true;
 };
 
 const shopifyProductsPublishApi = funcApi(shopifyProductsPublish, {
-  argNames: ['credsPath', 'arg'],
+  argNames: ['credsPath'],
 });
 
 module.exports = {
@@ -55,4 +21,4 @@ module.exports = {
   shopifyProductsPublishApi,
 };
 
-// curl localhost:8000/shopifyProductsPublish -H "Content-Type: application/json" -d '{ "credsPath": "au", "arg": "6979774283848" }'
+// curl localhost:8000/shopifyProductsPublish -H "Content-Type: application/json" -d '{ "credsPath": "au" }'
