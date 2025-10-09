f90d5791fb74280afa1a9cd07432f571a1171421
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-10-10 02:09:23 +1100

    args setup and function initial

--- starshipit/starshipitProductAdd.js
+++ starshipit/starshipitProductAdd.js
@@ -1,27 +1,74 @@
+// https://api-docs.starshipit.com/#8cfbd1b2-bd08-4cec-966f-7104cc147aee
+ 
 const { funcApi, logDeep } = require('../utils');
 const { starshipitClient } = require('../starshipit/starshipit.utils');
 
 const starshipitProductAdd = async (
   credsPath,
-  arg,
+  sku,
+  {
+    title,
+    customsDescription,
+    description,
+    country,
+    weight,
+    height,
+    length,
+    width,
+    hsCode,
+    color,
+    size,
+    barcode,
+    binLocation,
+    brand,
+    usage,
+    material,
+    model,
+    mid,
+    price,
+    dangerousGoodsType,
+  } = {},
 ) => {
 
   const response = await starshipitClient.fetch({
-    url: '/things',
-    params: {
-      arg_value: arg,
+    url: '/products',
+    method: 'post',
+    body: {
+      product: {
+        sku,
+        ...title && { title },
+        ...customsDescription && { customs_description: customsDescription },
+        ...description && { description },
+        ...country && { country },
+        ...weight && { weight },
+        ...height && { height },
+        ...length && { length },
+        ...width && { width },
+        ...hsCode && { hs_code: hsCode },
+        ...color && { color },
+        ...size && { size },
+        ...barcode && { barcode },
+        ...binLocation && { bin_location: binLocation },
+        ...brand && { brand },
+        ...usage && { usage },
+        ...material && { material },
+        ...model && { model },
+        ...mid && { mid },
+        ...price && { price },
+        ...dangerousGoodsType && { dangerous_goods_type: dangerousGoodsType },
+      },
     },
     context: {
       credsPath,
     },
-    interpreter: (response) => {
-      return {
-        ...response,
-        ...response.result ? {
-          result: response.result.arg_value,
-        } : {},
-      };
-    },
+    // interpreter: (response) => {
+    //   return {
+    //     ...response,
+    //     ...response.result ? {
+    //       result: response.result.arg_value,
+    //     } : {},
+    //   };
+    // },
   });
 
   logDeep(response);
@@ -29,7 +76,7 @@ const starshipitProductAdd = async (
 };
 
 const starshipitProductAddApi = funcApi(starshipitProductAdd, {
-  argNames: ['credsPath', 'arg', 'options'],
+  argNames: ['credsPath', 'sku', 'options'],
 });
 
 module.exports = {
