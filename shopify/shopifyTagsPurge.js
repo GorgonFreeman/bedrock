d5611e663627f17d586452d3a87eaaa29da96edf
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-10-10 00:41:48 +1100

    attempt to implement v2 actionMultipleOrSingle

--- shopify/shopifyTagsPurge.js
+++ shopify/shopifyTagsPurge.js
@@ -1,8 +1,8 @@
-const { funcApi, logDeep } = require('../utils');
+const { funcApi, logDeep, actionMultipleOrSingleV2 } = require('../utils');
 const { shopifyGet } = require('../shopify/shopify.utils');
 const { shopifyTagsRemove } = require('../shopify/shopifyTagsRemove');
 
-const shopifyTagsPurge = async (
+const shopifyTagsPurgeSingle = async (
   credsPath,
   {
     tags,
@@ -51,6 +51,28 @@ const shopifyTagsPurge = async (
   return response;
 };
 
+const shopifyTagsPurge = async (
+  credsPath,
+  tagsIdentifier,
+  resource,
+  {
+    queueRunOptions,
+    ...options
+  } = {},
+) => {
+  return actionMultipleOrSingleV2(
+    [credsPath, tagsIdentifier], 
+    shopifyTagsPurgeSingle, 
+    (credsPath, tagsIdentifier) => ({
+      args: [credsPath, tagsIdentifier, resource],
+      options,
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
+    },
+  );
+};
+
 const shopifyTagsPurgeApi = funcApi(shopifyTagsPurge, {
   argNames: ['credsPath', 'tagsIdentifier', 'resource', 'options'],
 });
@@ -61,3 +83,4 @@ module.exports = {
 };
 
 // curl localhost:8000/shopifyTagsPurge -H "Content-Type: application/json" -d '{ "credsPath": "au", "tagsIdentifier": { "tags": ["demo_will_publish"] }, "resource": "product" }'
+// curl localhost:8000/shopifyTagsPurge -H "Content-Type: application/json" -d '{ "credsPath": ["au", "us", "uk"], "tagsIdentifier": [{ "tags": ["demo_will_publish"] }, { "tags": ["fetch"] }], "resource": "product" }'
