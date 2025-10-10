cab159e6b71140455ae60bd8cc34d6ea4ab3ccc3
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-10-10 22:48:36 +1100

    compose stylearcade utils

---
+++ stylearcade/stylearcade.utils.js
@@ -0,0 +1,99 @@
+const { credsByPath, CustomAxiosClient, Getter, getterAsGetFunction, logDeep, askQuestion } = require('../utils');
+
+const stylearcadeRequestSetup = ({ credsPath } = {}) => {
+  const creds = credsByPath(['stylearcade', credsPath]);
+  const { 
+    BASE_URL, 
+    API_KEY, 
+  } = creds;
+
+  return {
+    baseUrl: BASE_URL,
+    headers: {
+      Authorization: `Bearer ${ API_KEY }`,
+    },
+  };
+};
+
+const stylearcadeClient = new CustomAxiosClient({
+  preparer: stylearcadeRequestSetup,
+  requiredContext: ['credsPath'],
+  baseHeaders: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const stylearcadeGetterPaginator = async (customAxiosPayload, response, additionalPaginationData) => {
+  console.log('stylearcadeGetterPaginator', customAxiosPayload, response, additionalPaginationData);
+  await askQuestion('Continue?');
+
+  return [true, null];
+
+  // Supplement payload with next pagination info
+  // const paginatedPayload = {
+  //   ...customAxiosPayload,
+  //   params: {
+  //     ...customAxiosPayload.params,
+  //     // Add pagination parameters based on what the API expects
+  //     ...(nextPage && { page: nextPage }),
+  //     ...(nextCursor && { cursor: nextCursor }),
+  //   },
+  // };
+  
+  // Logic to determine done
+  // const done = !hasNextPage;
+  
+  // return [done, paginatedPayload];
+};
+
+const stylearcadeGetterDigester = async (response) => {
+  
+  const { success, result } = response;
+  
+  // Return if error
+  if (!success) {
+    return null;
+  }
+
+  console.log('stylearcadeGetterDigester', response);
+  await askQuestion('Continue?');
+
+  // return items;
+};
+
+const stylearcadeGetter = async (
+  url,
+  {
+    params,
+    perPage = 100,
+    context,
+    ...getterOptions
+  } = {},
+) => {
+  return new Getter(
+    {
+      url,
+      payload: {
+        params: {
+          ...params,
+          limit: perPage,
+        },
+      },
+      paginator: stylearcadeGetterPaginator,
+      digester: stylearcadeGetterDigester,
+      client: stylearcadeClient,
+      clientArgs: {
+        context,
+      },
+      ...getterOptions,
+    },
+  );
+};
+
+const stylearcadeGet = getterAsGetFunction(stylearcadeGetter);
+
+module.exports = {
+  stylearcadeClient,
+  stylearcadeGetter,
+  stylearcadeGet,
+};
