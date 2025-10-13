26cf28137b463cf2b8917c2c5f8a8f0d566dae54
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-10-13 16:19:02 +1100

    start with stylearcade getter

--- collabs/collabsCustomsDataSweep.js
+++ collabs/collabsCustomsDataSweep.js
@@ -1,5 +1,7 @@
-const { funcApi } = require('../utils');
+const { funcApi, logDeep, surveyNestedArrays } = require('../utils');
 const { REGIONS_WF } = require('../constants');
+
+const { stylearcadeDataGetter } = require('../stylearcade/stylearcadeDataGet');
 
 const collabsCustomsDataSweep = async (
   {
@@ -11,6 +13,26 @@ const collabsCustomsDataSweep = async (
   // 1a. Get all customs data from Peoplevox.
   // 1b. Get all customs data from Starshipit.
   // 1c. Get all customs data from Shopify.
+
+  const piles = {
+    inStylearcade: [],
+    inPeoplevox: [],
+    inStarshipit: [],
+    inShopify: [],
+  };
+
+  const stylearcadeGetter = await stylearcadeDataGetter({
+    onItems: (items) => {
+      piles.inStylearcade.push(...items);
+    },
+  });
+
+  await Promise.all([
+    stylearcadeGetter.run(),
+  ]);
+
+  logDeep(piles);
+  logDeep(surveyNestedArrays(piles));
 
   // 2. Assess all data and identify updates.
 
