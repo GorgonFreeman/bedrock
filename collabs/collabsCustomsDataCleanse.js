59ac12795bed498c18a4eca7ff2e0c969ae275b7
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-11 09:30:49 +1100

    planning

--- collabs/collabsCustomsDataCleanse.js
+++ collabs/collabsCustomsDataCleanse.js
@@ -1,10 +1,15 @@
 const { funcApi } = require('../utils');
 
 const collabsCustomsDataCleanse = async () => {
+  
+  // 1. Get all data from Style Arcade, Shopify, Peoplevox and Starshipit.
 
-  return { 
-    arg, 
-    option,
+  // 2. Find any HS codes that are not 6-13 digit long numbers, and prepare removal payloads.
+
+  // 3. Provide an import sheet for Style Arcade to finalise the cleanse, as Style Arcade does not have an edit API.
+
+  return {
+    success: true,
   };
   
 };
