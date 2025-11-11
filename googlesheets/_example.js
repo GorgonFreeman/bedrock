afce0b1012547f50c5260b729931a7a773a374b8
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-11 16:53:52 +1100

    add example function for google sheets

---
+++ googlesheets/_example.js
@@ -0,0 +1,47 @@
+const { funcApi } = require('../utils');
+const { getGoogleSheetsClient } = require('../googlesheets/googlesheets.utils');
+
+const FUNC = async (
+  {
+    spreadsheetId,
+    spreadsheetHandle,
+  },
+  {
+    credsPath,
+  } = {},
+) => {
+
+  if (!spreadsheetId) {
+    spreadsheetId = spreadsheetHandleToSpreadsheetId[spreadsheetHandle];
+  }
+
+  if (!spreadsheetId) {
+    return {
+      success: false,
+      errors: [`Couldn't get a spreadsheet ID from ${ spreadsheetHandle }`],
+    };
+  }
+
+  const sheetsClient = getGoogleSheetsClient({ credsPath });
+
+  const response = await sheetsClient.spreadsheets.get({
+    spreadsheetId,
+  });
+
+  return response;
+};
+
+const FUNCApi = funcApi(FUNC, {
+  argNames: ['spreadsheetIdentifier', 'options'],
+  validatorsByArg: {
+    spreadsheetIdentifier: p => objHasAny(p, ['spreadsheetId', 'spreadsheetHandle']),
+  },
+});
+
+module.exports = {
+  FUNC,
+  FUNCApi,
+};
+
+// curl localhost:8000/FUNC -H "Content-Type: application/json" -d '{ "spreadsheetIdentifier": { "spreadsheetId": "1RuI7MrZ0VPGBLd4EXRIfDy7DVdtcdDKKbA8C5UBJQTM" } }'
+
