9c16de125bbda88d64f4d959ee73b28c3abf78e2
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-13 12:34:22 +1100

    prepare args and options

--- collabs/collabsInventorySync.js
+++ collabs/collabsInventorySync.js
@@ -1,21 +1,24 @@
 const { funcApi } = require('../utils');
 
 const collabsInventorySync = async (
-  arg,
+  region,
   {
-    option,
+    skus,
+    shopifyVariantsFetchQueries,
+    minDiff = 0,
+    safeMode, // Only sync if Shopify has more than the WMS, or, there is no stock in Shopify. These syncs are unlikely to cause oversells.
   } = {},
 ) => {
 
-  return { 
-    arg, 
-    option,
+  return {
+    success: true,
+    result: `Didn't do anything c:`,
   };
   
 };
 
 const collabsInventorySyncApi = funcApi(collabsInventorySync, {
-  argNames: ['arg', 'options'],
+  argNames: ['region', 'options'],
 });
 
 module.exports = {
@@ -23,4 +26,4 @@ module.exports = {
   collabsInventorySyncApi,
 };
 
-// curl localhost:8000/collabsInventorySync -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsInventorySync -H "Content-Type: application/json" -d '{ "region": "au" }'
