2298b35ea9cdec3b1becfd83910fdfd51bffbfd5
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-18 13:56:53 +1100

    planning

--- collabs/collabsCustomerErase.js
+++ collabs/collabsCustomerErase.js
@@ -7,7 +7,14 @@ const collabsCustomerErase = async (
   } = {},
 ) => {
 
-  
+  // 1. Try deleting the customer from Shopify
+  // - all stores?
+
+  // 2. If the delete fails, 
+  // 2a. request data erasure
+  // 2b. delete name, email, addresses, and unsubscribe from marketing
+
+  // 3. Consider deleting from other platforms, e.g. Salesforce
 
   return true;
 };
