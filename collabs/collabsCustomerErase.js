59d6e78cc79d58858781b200af2bd7ac8cb24ed8
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-18 16:42:06 +1100

    try deleting customer, and failing that, request data erasure

--- collabs/collabsCustomerErase.js
+++ collabs/collabsCustomerErase.js
@@ -1,6 +1,9 @@
 const { funcApi } = require('../utils');
+const { shopifyCustomerDelete } = require('../shopify/shopifyCustomerDelete');
+const { shopifyCustomerRequestDataErasure } = require('../shopify/shopifyCustomerRequestDataErasure');
 
 const collabsCustomerErase = async (
+  shopifyRegion,
   shopifyCustomerId,
   {
     option,
@@ -8,19 +11,51 @@ const collabsCustomerErase = async (
 ) => {
 
   // 1. Try deleting the customer from Shopify
-  // - all stores?
+  // TODO: Consider deleting from all stores
+  // TODO: Support other Shopify customer identifiers, e.g. email
+
+  let customerDeleted = false;
+
+  const shopifyDeleteResponse = await shopifyCustomerDelete(shopifyRegion, shopifyCustomerId);
+
+  const { 
+    success: deleteSuccess,
+    result: deleteResult,
+  } = shopifyDeleteResponse;
+
+  customerDeleted = deleteSuccess;
 
   // 2. If the delete fails, 
   // 2a. request data erasure
   // 2b. delete name, email, addresses, and unsubscribe from marketing
 
+  if (!customerDeleted) {
+    const shopifyRequestDataErasureResponse = await shopifyCustomerRequestDataErasure(shopifyRegion, shopifyCustomerId);
+    const { 
+      success: requestDataErasureSuccess,
+      result: requestDataErasureResult,
+    } = shopifyRequestDataErasureResponse;
+
+    if (!requestDataErasureSuccess) {
+      return requestDataErasureResponse;
+    }
+
+    // TODO: 2b
+  }
+
   // 3. Consider deleting from other platforms, e.g. Salesforce
 
-  return true;
+  return {
+    success: true,
+    result: {
+      deleteResponse,
+      requestDataErasureResponse,
+    },
+  };
 };
 
 const collabsCustomerEraseApi = funcApi(collabsCustomerErase, {
-  argNames: ['shopifyCustomerId', 'options'],
+  argNames: ['shopifyRegion', 'shopifyCustomerId', 'options'],
 });
 
 module.exports = {
