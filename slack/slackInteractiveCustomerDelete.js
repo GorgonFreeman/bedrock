df2efd776135118c0d3390d8ecefc17b26432638
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-20 16:15:56 +1100

    slackInteractiveCustomerDelete stub

---
+++ slack/slackInteractiveCustomerDelete.js
@@ -0,0 +1,68 @@
+const { respond, logDeep, customAxios } = require('../utils');
+
+const COMMAND_NAME = 'slash_command'; // slash command
+
+const slackInteractiveCustomerDelete = async (req, res) => {
+  console.log('slackInteractiveCustomerDelete');
+
+  const { body } = req;
+  
+  // If no payload, this is an initiation, e.g. slash command - send the initial blocks
+  if (!body?.payload) {
+
+    const initialBlocks = [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `I don't do anything yet :hugging_face:`,
+        },
+      },
+    ];
+
+    return respond(res, 200, {
+      response_type: 'in_channel',
+      blocks: initialBlocks,
+    });
+  }
+
+  // Because we got to this point, we have a payload - handle as an interactive step
+  respond(res, 200); // Acknowledge immediately - we'll provide the next step to the response_url later
+
+  const payload = JSON.parse(body.payload);
+  logDeep('payload', payload);
+
+  const { 
+    response_url: responseUrl,
+    state, 
+    actions, 
+  } = payload;
+
+  const action = actions?.[0];
+  const { 
+    action_id: actionId,
+    value: actionValue,
+  } = action;
+
+  logDeep({
+    responseUrl,
+    state,
+    actionId,
+    actionValue,
+  });
+
+  let response;
+
+  response = {
+    replace_original: 'true',
+    text: `I don't do anything yet :hugging_face:`,
+  };
+
+  logDeep('response', response);
+  return customAxios(responseUrl, {
+    method: 'post',
+    body: response,
+  });
+};
+
+module.exports = slackInteractiveCustomerDelete;
