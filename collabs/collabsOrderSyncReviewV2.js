a092a3434a7aca14fa1a753cf3427312a7da3f55
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-25 08:45:13 +1100

    shopify order getter

--- collabs/collabsOrderSyncReviewV2.js
+++ collabs/collabsOrderSyncReviewV2.js
@@ -1,4 +1,6 @@
-const { funcApi } = require('../utils');
+const { funcApi, logDeep } = require('../utils');
+
+const { shopifyOrdersGetter } = require('../shopify/shopifyOrdersGet');
 
 const collabsOrderSyncReviewV2 = async (
   region,
@@ -6,6 +8,58 @@ const collabsOrderSyncReviewV2 = async (
     option,
   } = {},
 ) => {
+
+  const piles = {
+    shopify: [],
+    wms: [],
+    found: [],
+    missing: [],
+  };
+
+  const shopifyQueriesByRegion = {
+    au: [
+      `tag_not:'Sync:Confirmed'`,
+    ],
+    us: [
+      `tag:'sync-to-radial'`,
+    ],
+  };
+
+  const getters = [];
+
+  const getterShopify = await shopifyOrdersGetter(
+    region, 
+    {
+      attrs: `
+        id
+        name
+        createdAt
+      `,
+      queries: [
+        'created_at:>2025-07-01',
+        'fulfillment_status:unshipped OR fulfillment_status:partial',
+        'status:open',
+        'delivery_method:shipping',
+        `tag_not:'order_sync_review_exclude'`,
+        `tag_not:'sync_confirmed'`,
+        ...(shopifyQueriesByRegion?.[region] || []),
+      ],
+      sortKey: 'CREATED_AT',
+      reverse: true,
+
+      onItems: (items) => {
+        piles.shopify.push(...items);
+      },
+
+      logFlavourText: `shopify:getter:`,
+    },
+  );
+
+  getters.push(getterShopify);
+
+  await Promise.all(getters.map(getter => getter.run()));
+
+  logDeep(piles);
 
   return { 
     region, 
