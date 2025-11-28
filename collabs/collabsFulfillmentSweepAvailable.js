6dd9c24fc92c0dfd12bd5d155cee6aabd133e2ec
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-28 11:53:08 +1100

    shopify orders getter and structure

--- collabs/collabsFulfillmentSweepAvailable.js
+++ collabs/collabsFulfillmentSweepAvailable.js
@@ -1,12 +1,15 @@
 // Actions the fulfillments that are easiest to get from the WMS
 
-const { funcApi } = require('../utils');
+const { funcApi, logDeep, surveyNestedArrays } = require('../utils');
 
 const {
   REGIONS_PVX,
   REGIONS_LOGIWA,
   REGIONS_BLECKMANN,
 } = require('../constants');
+
+const { shopifyOrdersGetter } = require('../shopify/shopifyOrdersGet');
+const { bleckmannPickticketsGetter } = require('../bleckmann/bleckmannPickticketsGet');
 
 const collabsFulfillmentSweepAvailable = async (
   region,
@@ -30,6 +33,55 @@ const collabsFulfillmentSweepAvailable = async (
       message: 'Region not supported',
     };
   }
+
+  const piles = {
+    shopify: [],
+    wms: [],
+  };
+
+  const getters = [];
+
+  const getterShopify = await shopifyOrdersGetter(
+    region, 
+    {
+      attrs: `
+        id
+        name
+        shippingLine {
+          title
+        }
+      `,
+      queries: [
+        'created_at:>2025-10-01',
+        // 'created_at:<2025-10-01',
+        'fulfillment_status:unshipped',
+        'status:open',
+        'delivery_method:shipping',
+      ],
+      sortKey: 'CREATED_AT',
+      reverse: true,
+
+      onItems: (items) => {
+        piles.shopify.push(...items);
+      },
+
+      // onDone: getterFinish,
+
+      logFlavourText: `shopify:getter:`,
+    },
+  );
+
+  getters.push(getterShopify);
+
+  if (bleckmannRelevant) {
+
+  }
+
+  await Promise.all([
+    ...getters.map(getter => getter.run()),
+  ]);
+
+  logDeep(surveyNestedArrays(piles));
 
   return { 
     region, 
