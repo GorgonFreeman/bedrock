f49848baafba83b0d3066fd6aa9177ed1632d32c
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-28 11:32:45 +1100

    args setup and region support gate

--- collabs/collabsFulfillmentSweepAvailable.js
+++ collabs/collabsFulfillmentSweepAvailable.js
@@ -1,21 +1,45 @@
+// Actions the fulfillments that are easiest to get from the WMS
+
 const { funcApi } = require('../utils');
 
+const {
+  REGIONS_PVX,
+  REGIONS_LOGIWA,
+  REGIONS_BLECKMANN,
+} = require('../constants');
+
 const collabsFulfillmentSweepAvailable = async (
-  arg,
+  region,
   {
     option,
   } = {},
 ) => {
 
+  const pvxRelevant = REGIONS_PVX.includes(region);
+  const logiwaRelevant = REGIONS_LOGIWA.includes(region);
+  const bleckmannRelevant = REGIONS_BLECKMANN.includes(region);
+  const anyRelevant = [
+    // pvxRelevant, 
+    // logiwaRelevant, 
+    bleckmannRelevant,
+  ].some(Boolean);
+
+  if (!anyRelevant) {
+    return {
+      success: false,
+      message: 'Region not supported',
+    };
+  }
+
   return { 
-    arg, 
+    region, 
     option,
   };
   
 };
 
 const collabsFulfillmentSweepAvailableApi = funcApi(collabsFulfillmentSweepAvailable, {
-  argNames: ['arg', 'options'],
+  argNames: ['region', 'options'],
 });
 
 module.exports = {
@@ -23,4 +47,4 @@ module.exports = {
   collabsFulfillmentSweepAvailableApi,
 };
 
-// curl localhost:8000/collabsFulfillmentSweepAvailable -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsFulfillmentSweepAvailable -H "Content-Type: application/json" -d '{ "region": "uk" }'
