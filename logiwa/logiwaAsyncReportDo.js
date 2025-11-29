d32737c13803a381bccf2082b538d7e65a0d7e40
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-11-29 22:04:50 +1100

    support resuming report by id

--- logiwa/logiwaAsyncReportDo.js
+++ logiwa/logiwaAsyncReportDo.js
@@ -1,24 +1,35 @@
-const { funcApi, logDeep, wait, seconds } = require('../utils');
+const { funcApi, logDeep, wait, seconds, objHasAll } = require('../utils');
 const { logiwaAsyncReportCreate } = require('../logiwa/logiwaAsyncReportCreate');
 const { logiwaAsyncReportGet } = require('../logiwa/logiwaAsyncReportGet');
 
 const logiwaAsyncReportDo = async (
-  reportTypeCode,
-  filter,
+  {
+    // if creating
+    reportTypeCode,
+    filter,
+    
+    // if resuming
+    reportId,
+  },
   {
     credsPath,
     apiVersion = 'v3.1',
   } = {},
 ) => {
+  
+  // If not resuming, then creating
+  if (!reportId) {
+    const createResponse = await logiwaAsyncReportCreate(reportTypeCode, filter, { credsPath, apiVersion });
 
-  const createResponse = await logiwaAsyncReportCreate(reportTypeCode, filter, { credsPath, apiVersion });
+    const {
+      success: createSuccess,
+      result: createResult,
+    } = createResponse;
+    if (!createSuccess) {
+      return createResponse;
+    }
 
-  const {
-    success: createSuccess,
-    result: reportId,
-  } = createResponse;
-  if (!createSuccess) {
-    return createResponse;
+    reportId = createResult;
   }
 
   let finished;
@@ -26,22 +37,19 @@ const logiwaAsyncReportDo = async (
 
   do {
     await wait(seconds(5));
-
     const getResponse = await logiwaAsyncReportGet(reportId, { credsPath, apiVersion });
 
     const {
       success: getSuccess,
       result: getResult,
     } = getResponse;
-    if (!getSuccess) {
-      return getResponse;
+    if (!getSuccess) {      return getResponse;
     }
 
     const {
       // statusCode,
       completedDateTime,
     } = getResult;
-
     finished = completedDateTime !== null;
     finalReportResult = getResult;
   } while (!finished);
@@ -66,7 +74,13 @@ const logiwaAsyncReportDo = async (
 };
 
 const logiwaAsyncReportDoApi = funcApi(logiwaAsyncReportDo, {
-  argNames: ['reportTypeCode', 'filter', 'options'],
+  argNames: [ 
+    'reportPayload',
+    'options',
+  ],
+  validatorsByArg: {
+    reportPayload: p => (objHasAll(p, ['reportTypeCode', 'filter']) || objHasAll(p, ['reportId'])),
+  },
 });
 
 module.exports = {
@@ -74,4 +88,5 @@ module.exports = {
   logiwaAsyncReportDoApi,
 };
 
-// curl localhost:8000/logiwaAsyncReportDo -H "Content-Type: application/json" -d '{ "reportTypeCode": "available_to_promise", "filter": "WarehouseIdentifier.eq=cfbdf154-3052-4e18-84f3-b93b7cde2875" }'
+// curl localhost:8000/logiwaAsyncReportDo -H "Content-Type: application/json" -d '{ "reportPayload": { "reportTypeCode": "available_to_promise", "filter": "WarehouseIdentifier.eq=cfbdf154-3052-4e18-84f3-b93b7cde2875" } }'
+// curl localhost:8000/logiwaAsyncReportDo -H "Content-Type: application/json" -d '{ "reportPayload": { "reportId": "1ed3ad7f-fe66-429f-8e06-ac691aef1ece" } }'
