a76731daf999162035e461756fc8df31c57548b9
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-12-04 21:22:33 +1100

    store key gate - start with US support

--- collabs/collabsFulfillmentSweepV4.js
+++ collabs/collabsFulfillmentSweepV4.js
@@ -1,11 +1,34 @@
 const { funcApi } = require('../utils');
 
+const {
+  REGIONS_PVX,
+  REGIONS_LOGIWA,
+  REGIONS_BLECKMANN,
+} = require('../constants');
+
 const collabsFulfillmentSweepV4 = async (
   store,
   {
     option,
   } = {},
 ) => {
+
+  const peoplevoxRelevant = REGIONS_PVX.includes(store);
+  const logiwaRelevant = REGIONS_LOGIWA.includes(store);
+  const bleckmannRelevant = REGIONS_BLECKMANN.includes(store);
+
+  const anyRelevant = [
+    // peoplevoxRelevant, 
+    logiwaRelevant, 
+    // bleckmannRelevant,
+  ].some(Boolean);
+
+  if (!anyRelevant) {
+    return {
+      success: false,
+      message: 'Store not supported',
+    };
+  }
 
   return { 
     store, 
