15e6987ea45208910d70589c0a9d9697ba5fbdb5
John <29110105+GorgonFreeman@users.noreply.github.com>
2025-12-10 22:40:37 +1100

    add options to pipe17InventoryItemsGet

--- pipe17/pipe17InventoryItemsGet.js
+++ pipe17/pipe17InventoryItemsGet.js
@@ -6,16 +6,82 @@ const { pipe17Get } = require('../pipe17/pipe17.utils');
 const pipe17InventoryItemsGet = async (
   {
     credsPath,
-    // TODO: Add query params
+
+    // API options
+    available,
+    available_gt,
+    available_lt,
+    count,
+    entityId,
+    event,
+    integration,
+    inventoryId,
+    inventoryNotTracked,
+    keys,
+    ledger,
+    locationId,
+    onHand,
+    onHand_gt,
+    onHand_lt,
+    order,
+    pagination,
+    requestorId,
+    since,
+    skip,
+    sku,
+    sku_gt,
+    sku_lt,
+    tags,
+    totals,
+    type,
+    until,
+    updatedSince,
+    updatedUntil,
+    vendorSKU,
+
     ...getterOptions
   } = {},
 ) => {
+
+  const params = {
+    ...available && { available },
+    ...available_gt && { available_gt },
+    ...available_lt && { available_lt },
+    ...count && { count },
+    ...entityId && { entityId },
+    ...event && { event },
+    ...integration && { integration },
+    ...inventoryId && { inventoryId },
+    ...inventoryNotTracked && { inventoryNotTracked },
+    ...keys && { keys },
+    ...ledger && { ledger },
+    ...locationId && { locationId },
+    ...onHand && { onHand },
+    ...onHand_gt && { onHand_gt },
+    ...onHand_lt && { onHand_lt },
+    ...order && { order },
+    ...pagination && { pagination },
+    ...requestorId && { requestorId },
+    ...since && { since },
+    ...skip && { skip },
+    ...sku && { sku },
+    ...sku_gt && { sku_gt },
+    ...sku_lt && { sku_lt },
+    ...tags && { tags },
+    ...totals && { totals },
+    ...type && { type },
+    ...until && { until },
+    ...updatedSince && { updatedSince },
+    ...updatedUntil && { updatedUntil },
+    ...vendorSKU && { vendorSKU },
+  };
 
   const response = await pipe17Get(
     '/inventory', 
     'inventory', 
     {
       credsPath,
+      ...params && { params },
       ...getterOptions,
     },
   );
@@ -48,3 +114,4 @@ module.exports = {
 };
 
 // curl localhost:8000/pipe17InventoryItemsGet -H "Content-Type: application/json" -d '{ "options": { "limit": 300 } }'
+// curl localhost:8000/pipe17InventoryItemsGet -H "Content-Type: application/json" -d '{ "options": { "sku": ["WFMD94-2-S", "FSBOM172-15-S", "EXKM199-1-XXS"] } }'
