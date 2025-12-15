cf9e2f918d414ba4ce9048d01d40a786262f979d
Zwe <zwe@whitefoxboutique.com>
2025-12-16 10:09:02 +1100

    sort out general flow of collabs order sync check

--- collabs/collabsOrderSyncCheck.js
+++ collabs/collabsOrderSyncCheck.js
@@ -1,21 +1,51 @@
-const { funcApi } = require('../utils');
+// Check the latest WMS orders to establish how far behind the sync is
+
+const { funcApi, gidToId, askQuestion, logDeep } = require('../utils');
+const {
+  REGIONS_PVX,
+  REGIONS_LOGIWA,
+  REGIONS_BLECKMANN,
+} = require('../constants');
+
+const { shopifyOrderGet } = require('../shopify/shopifyOrderGet');
+
+const { peoplevoxReportGet } = require('../peoplevox/peoplevoxReportGet');
+
+const { bleckmannPickticketsGetter } = require('../bleckmann/bleckmannPickticketsGet');
+
+const { logiwaOrdersGetter } = require('../logiwa/logiwaOrdersGet');
 
 const collabsOrderSyncCheck = async (
-  arg,
+  region,
   {
     option,
   } = {},
 ) => {
 
-  return { 
-    arg, 
-    option,
-  };
-  
+  const pvxRelevant = REGIONS_PVX.includes(region);
+  const logiwaRelevant = REGIONS_LOGIWA.includes(region);
+  const bleckmannRelevant = REGIONS_BLECKMANN.includes(region);
+  const anyRelevant = [pvxRelevant, logiwaRelevant, bleckmannRelevant].some(Boolean);
+  if (!anyRelevant) {
+    return {
+      success: false,
+      error: ['No platform finder relevant to this region'],
+    };
+  }
+
+  if (pvxRelevant) {
+  }
+
+  if (logiwaRelevant) {
+  }
+
+  if (bleckmannRelevant) {
+  }
+
 };
 
 const collabsOrderSyncCheckApi = funcApi(collabsOrderSyncCheck, {
-  argNames: ['arg', 'options'],
+  argNames: ['region', 'options'],
 });
 
 module.exports = {
@@ -23,4 +53,6 @@ module.exports = {
   collabsOrderSyncCheckApi,
 };
 
-// curl localhost:8000/collabsOrderSyncCheck -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/collabsOrderSyncCheck -H "Content-Type: application/json" -d '{ "region": "au" }'
+// curl localhost:8000/collabsOrderSyncCheck -H "Content-Type: application/json" -d '{ "region": "us" }'
+// curl localhost:8000/collabsOrderSyncCheck -H "Content-Type: application/json" -d '{ "region": "uk" }'
