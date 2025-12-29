8218e19fd8306779db7bb1553d9ec3523b3f9e33
Zwe <zwe@whitefoxboutique.com>
2025-12-29 12:59:04 +1100

    update delivery profiles getter function

--- shopify/shopifyDeliveryProfilesGet.js
+++ shopify/shopifyDeliveryProfilesGet.js
@@ -1,37 +1,85 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/queries/orders
+// https://shopify.dev/docs/api/admin-graphql/latest/queries/deliveryProfiles
 
-const { funcApi, logDeep } = require('../utils');
-const { shopifyGet } = require('../shopify/shopify.utils');
+const { respond, mandateParam, logDeep } = require('../utils');
+const { shopifyGet, shopifyGetter } = require('../shopify/shopify.utils');
 
-const defaultAttrs = `id name`;
+const defaultAttrs = `
+  id
+  name
+  profileLocationGroups {
+    locationGroupZones(first: 10) {
+      edges {
+        node {
+          zone {
+            id
+            name
+          }
+          methodDefinitions(first: 10) {
+            edges {
+              node {
+                id
+                active
+                description
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
 
-const shopifyDeliveryProfilesGet = async (
+const payloadMaker = (
   credsPath,
   {
     attrs = defaultAttrs,
     ...options
   } = {},
 ) => {
-
-  const response = await shopifyGet(
-    credsPath, 
-    'order', 
+  return [
+    credsPath,
+    'deliveryProfile',
     {
       attrs,
       ...options,
     },
-  );
+  ];
+};
 
+const shopifyDeliveryProfilesGet = async (...args) => {
+  const response = await shopifyGet(...payloadMaker(...args));
+  logDeep(response);
   return response;
 };
 
-const shopifyDeliveryProfilesGetApi = funcApi(shopifyDeliveryProfilesGet, {
-  argNames: ['credsPath', 'options'],
-});
+const shopifyDeliveryProfilesGetter = async (...args) => {
+  const response = await shopifyGetter(...payloadMaker(...args));
+  return response;
+};
+
+const shopifyDeliveryProfilesGetApi = async (req, res) => {
+  const { 
+    credsPath,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'credsPath', credsPath),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await shopifyDeliveryProfilesGet(
+    credsPath,
+    options,
+  );
+  respond(res, 200, result);
+};
 
 module.exports = {
   shopifyDeliveryProfilesGet,
+  shopifyDeliveryProfilesGetter,
   shopifyDeliveryProfilesGetApi,
 };
 
-// curl localhost:8000/shopifyDeliveryProfilesGet -H "Content-Type: application/json" -d '{ "credsPath": "au", "options": { "limit": 2 } }'
+// curl localhost:8000/shopifyDeliveryProfilesGet -H "Content-Type: application/json" -d '{ "credsPath": "au" }'
