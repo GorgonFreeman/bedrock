bb8c4449dd9a95318f6206c3909ca11cdc3818e6
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-01-06 12:42:30 +1100

    shopifyFulfillmentOrderSubmitCancellationRequest complete function

--- shopify/shopifyFulfillmentOrderSubmitCancellationRequest.js
+++ shopify/shopifyFulfillmentOrderSubmitCancellationRequest.js
@@ -1,29 +1,36 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageCreate
+// https://shopify.dev/docs/api/admin-graphql/2024-10/mutations/fulfillmentOrderSubmitCancellationRequest
 
 const { funcApi, logDeep } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
 
-const defaultAttrs = `id title handle`;
+const defaultAttrs = `status requestStatus`;
 
 const shopifyFulfillmentOrderSubmitCancellationRequest = async (
   credsPath,
-  pageInput,
+  fulfillmentOrderId,
   {
     apiVersion,
     returnAttrs = defaultAttrs,
+    message,
   } = {},
 ) => {
 
   const response = await shopifyMutationDo(
     credsPath,
-    'pageCreate',
+    'fulfillmentOrderSubmitCancellationRequest',
     {
-      page: {
-        type: 'PageCreateInput!',
-        value: pageInput,
+      id: {
+        type: 'ID!',
+        value: `gid://shopify/FulfillmentOrder/${ fulfillmentOrderId }`,
+      },
+      ...message && {
+        message: {
+          type: 'String',
+          value: message,
+        },
       },
     },
-    `page { ${ returnAttrs } }`,
+    `fulfillmentOrder { ${ returnAttrs } }`,
     { 
       apiVersion,
     },
@@ -33,7 +40,11 @@ const shopifyFulfillmentOrderSubmitCancellationRequest = async (
 };
 
 const shopifyFulfillmentOrderSubmitCancellationRequestApi = funcApi(shopifyFulfillmentOrderSubmitCancellationRequest, {
-  argNames: ['credsPath', 'pageInput', 'options'],
+  argNames: ['credsPath', 'fulfillmentOrderId', 'options'],
+  validatorsByArg: {
+    credsPath: Boolean,
+    fulfillmentOrderId: Boolean,
+  },
 });
 
 module.exports = {
@@ -41,4 +52,4 @@ module.exports = {
   shopifyFulfillmentOrderSubmitCancellationRequestApi,
 };
 
-// curl http://localhost:8000/shopifyFulfillmentOrderSubmitCancellationRequest -H 'Content-Type: application/json' -d '{ "credsPath": "au", "pageInput": { "title": "Batarang Blueprints", "body": "<strong>Good page!</strong>" }, "options": { "returnAttrs": "id" } }'
+// curl http://localhost:8000/shopifyFulfillmentOrderSubmitCancellationRequest -H 'Content-Type: application/json' -d '{ "credsPath": "au", "fulfillmentOrderId": "12341234" }'
