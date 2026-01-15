019ef11301714892bb8a98da10f59d22f3bee327
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-01-15 11:21:52 +1100

    bulk fetch orders from shopify

--- collabs/collabsOrderSyncReviewV3.js
+++ collabs/collabsOrderSyncReviewV3.js
@@ -1,4 +1,6 @@
-const { funcApi } = require('../utils');
+const { funcApi, surveyNestedArrays } = require('../utils');
+
+const { shopifyBulkOperationDo } = require('../shopify/shopifyBulkOperationDo');
 
 const collabsOrderSyncReviewV3 = async (
   region,
@@ -7,9 +9,56 @@ const collabsOrderSyncReviewV3 = async (
   } = {},
 ) => {
 
+  const piles = {};
+
+  const shopifyQueriesByRegion = {
+    au: [
+      `tag_not:'Sync:Confirmed'`,
+    ],
+    us: [
+      `tag:'sync-to-radial'`,
+    ],
+  };
+
+  const shopifyQueries = [
+    'created_at:>2025-07-01',
+    '(fulfillment_status:unshipped OR fulfillment_status:partial)',
+    'status:open',
+    'delivery_method:shipping',
+    `tag_not:'order_sync_review_exclude'`,
+    `tag_not:'sync_confirmed'`,
+    ...(shopifyQueriesByRegion?.[region] || []),
+  ];
+
+  const shopifyOrdersQuery = `{
+    orders(query: "${ shopifyQueries.join(' AND ') }") {
+      edges {
+        node {
+          id
+          name
+          createdAt
+        }
+      }
+    }
+  }`;
+
+  const shopifyOrdersResponse = await shopifyBulkOperationDo(
+    region,
+    'query',
+    shopifyOrdersQuery,
+  );
+  const { success: shopifyOrdersSuccess, result: shopifyOrders } = shopifyOrdersResponse;
+  if (!shopifyOrdersSuccess) {
+    return shopifyOrdersResponse;
+  }
+
+  piles.in = shopifyOrders;
+
+  console.log(surveyNestedArrays(piles));
+
   return { 
-    region, 
-    option,
+    success: true,
+    result: piles,
   };
   
 };
