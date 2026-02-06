0e8e9113f0ccdfb77f47c4128cdff8afd42e8260
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-06 12:49:03 +1100

    compose asana paginator

--- asana/asana.utils.js
+++ asana/asana.utils.js
@@ -1,4 +1,4 @@
-const { CustomAxiosClient, credsByPath, askQuestion, logDeep } = require('../utils');
+const { CustomAxiosClient, credsByPath, askQuestion, logDeep, Getter, getterAsGetFunction } = require('../utils');
 
 const asanaRequestSetup = ({ credsPath } = {}) => {
   const creds = credsByPath(['asana', credsPath]);
@@ -32,9 +32,15 @@ const asanaClient = new CustomAxiosClient({
       interpretedResult = interpretedResult?.[resultsNode];
     }
 
+    // Preserve pagination metadata (next_page) alongside the data
     const interpretedResponse = {
       ...response,
-      ...response?.result && { result: interpretedResult },
+      ...response?.result && { 
+        result: {
+          data: interpretedResult,
+          ...(result?.next_page && { next_page: result.next_page }),
+        },
+      },
     };
 
     // logDeep({ interpretedResponse });
@@ -44,6 +50,80 @@ const asanaClient = new CustomAxiosClient({
   },
 });
 
+const MAX_PER_PAGE = 100;
+
+const asanaGetter = async (
+  url,
+  {
+    credsPath,
+    params,
+    perPage = MAX_PER_PAGE,
+    ...getterOptions
+  } = {},
+) => {
+  return new Getter(
+    {
+      url,
+      payload: {
+        params: {
+          limit: Math.min(perPage, MAX_PER_PAGE),
+          ...params,
+        },
+      },
+      paginator: async (customAxiosPayload, response, additionalPaginationData) => {
+        // logDeep('paginator', customAxiosPayload, response);
+        // await askQuestion('continue?');
+
+        const { success, result } = response;
+        if (!success) {
+          return [true, null];
+        }
+
+        // Extract pagination info from response
+        const { next_page } = result || {};
+        const offset = next_page?.offset;
+
+        // Determine if we're done
+        const done = !offset;
+
+        // Supplement payload with next pagination info
+        const paginatedPayload = {
+          ...customAxiosPayload,
+          params: {
+            ...customAxiosPayload?.params,
+            ...(offset ? { offset } : {}),
+          },
+        };
+
+        return [done, paginatedPayload];
+      },
+      digester: async (response) => {
+        // logDeep('digester', response);
+        // await askQuestion('continue?');
+
+        const { success, result } = response;
+        if (!success) {
+          return null;
+        }
+
+        const items = result?.data;
+        return items;
+      },
+      client: asanaClient,
+      clientArgs: {
+        context: {
+          credsPath,
+        },
+      },
+      ...getterOptions,
+    },
+  );
+};
+
+const asanaGet = getterAsGetFunction(asanaGetter);
+
 module.exports = {
   asanaClient,
+  asanaGetter,
+  asanaGet,
 };
