0e42190e4a558438d0cb82e963706ba17c2f1a8d
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-18 14:26:04 +1100

    timing and spacing tweaks

--- etsy/etsyAuthFlowWalkthrough.js
+++ etsy/etsyAuthFlowWalkthrough.js
@@ -1,4 +1,4 @@
-const { funcApi, logDeep, askQuestion } = require('../utils');
+const { funcApi, logDeep, askQuestion, wait, seconds } = require('../utils');
 const { etsyAuthCodeRequest } = require('../etsy/etsyAuthCodeRequest');
 const { etsyAccessTokenRequest } = require('../etsy/etsyAccessTokenRequest');
 
@@ -9,13 +9,19 @@ const etsyAuthFlowWalkthrough = async (
 ) => {
 
   console.log(`1. I'm going to run etsyAuthCodeRequest. A webpage will open. Once you have approved, you will be sent to the redirect url.`);
+  await wait(seconds(1));
   await askQuestion('Do it! (Enter to continue)');
   
   // Opens a url, so no proper response payload
   await etsyAuthCodeRequest({ credsPath });
+  await wait(seconds(1));
 
-  const authCodeVerifier = await askQuestion(`2. Enter codeVerifier (from logs above):`);
-  const authCode = await askQuestion(`3. Enter code (from redirect url params):`);
+  const authCodeVerifier = await askQuestion(`2. Enter codeVerifier (from logs above): `);
+  await wait(seconds(1));
+  const authCode = await askQuestion(`3. Enter code (from redirect url params): `);
+  await wait(seconds(1));
+
+  console.log('\n');
 
   const accessTokenRequestResponse = await etsyAccessTokenRequest({ 
     credsPath, 
@@ -28,12 +34,21 @@ const etsyAuthFlowWalkthrough = async (
     return accessTokenRequestResponse;
   }
   
+  console.log('\n');
   console.log(`AUTH_CODE: ${ authCode }`);
   console.log(`AUTH_CODEVERIFIER: ${ authCodeVerifier }`);
-  console.log(`4. Enter these in .creds.yml for posterity.`);
-  await askQuestion('Done! (Enter to continue)');
+  console.log('\n');
 
-  console.log(`5. All sorted!`);
+  await wait(seconds(1));
+
+  console.log(`4. Enter the above in .creds.yml for posterity.`);
+  await wait(seconds(1));
+  await askQuestion('Done! (Enter to continue)');
+  console.log('\n');
+
+  await wait(seconds(1));
+
+  console.log(`Ok, you're good to go! o7`);
 
   return {
     success: true,
