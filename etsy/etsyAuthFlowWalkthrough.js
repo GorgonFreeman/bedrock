b4406f726b8fb13c046e39b6ed82bc7115fc994a
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-18 13:57:21 +1100

    args setup

--- etsy/etsyAuthFlowWalkthrough.js
+++ etsy/etsyAuthFlowWalkthrough.js
@@ -1,24 +1,15 @@
-const { funcApi, logDeep } = require('../utils');
-const { etsyClient } = require('../etsy/etsy.utils');
+const { funcApi, logDeep, askQuestion } = require('../utils');
 
 const etsyAuthFlowWalkthrough = async (
-  arg,
   {
     credsPath,
   } = {},
 ) => {
-  const response = await etsyClient.fetch({ 
-    url: `/application/things/${ arg }`,
-    context: {
-      credsPath,
-    },
-  });
-  logDeep(response);
-  return response;
+  
 };
 
 const etsyAuthFlowWalkthroughApi = funcApi(etsyAuthFlowWalkthrough, {
-  argNames: ['arg', 'options'],
+  argNames: ['options'],
 });
 
 module.exports = {
