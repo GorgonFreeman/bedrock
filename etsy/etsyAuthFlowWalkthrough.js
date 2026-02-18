5f6b36c15cdb54beb03caf16ce38f44c83840d31
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-18 14:04:14 +1100

    begin etsy auth walkthrough

--- etsy/etsyAuthFlowWalkthrough.js
+++ etsy/etsyAuthFlowWalkthrough.js
@@ -1,11 +1,24 @@
 const { funcApi, logDeep, askQuestion } = require('../utils');
+const { etsyAuthCodeRequest } = require('../etsy/etsyAuthCodeRequest');
 
 const etsyAuthFlowWalkthrough = async (
   {
     credsPath,
   } = {},
 ) => {
-  
+
+  console.log(`1. I'm going to run etsyAuthCodeRequest. A webpage will open. Once you have approved, you will be sent to the redirect url.`);
+  await askQuestion('Do it! (Enter to continue)');
+
+  const authCodeRequestResponse = await etsyAuthCodeRequest({ credsPath });
+
+  if (!authCodeRequestResponse?.success) {
+    logDeep(authCodeRequestResponse);
+    return authCodeRequestResponse;
+  }
+
+  console.log(`2. Copy the codeVerifier above, and the code out of your url. Enter them in .creds.yml as AUTH_CODEVERIFIER and AUTH_CODE.`);
+  await askQuestion('Done! (Enter to continue)');
 };
 
 const etsyAuthFlowWalkthroughApi = funcApi(etsyAuthFlowWalkthrough, {
