685b48a74c19d572c117af72da13b64ab697c5d2
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-18 14:18:28 +1100

    complete walkthrough

--- etsy/etsyAuthFlowWalkthrough.js
+++ etsy/etsyAuthFlowWalkthrough.js
@@ -1,5 +1,6 @@
 const { funcApi, logDeep, askQuestion } = require('../utils');
 const { etsyAuthCodeRequest } = require('../etsy/etsyAuthCodeRequest');
+const { etsyAccessTokenRequest } = require('../etsy/etsyAccessTokenRequest');
 
 const etsyAuthFlowWalkthrough = async (
   {
@@ -9,16 +10,38 @@ const etsyAuthFlowWalkthrough = async (
 
   console.log(`1. I'm going to run etsyAuthCodeRequest. A webpage will open. Once you have approved, you will be sent to the redirect url.`);
   await askQuestion('Do it! (Enter to continue)');
+  
+  // Opens a url, so no proper response payload
+  await etsyAuthCodeRequest({ credsPath });
 
-  const authCodeRequestResponse = await etsyAuthCodeRequest({ credsPath });
+  const authCodeVerifier = await askQuestion(`2. Enter codeVerifier (from logs above):`);
+  const authCode = await askQuestion(`3. Enter code (from redirect url params):`);
 
-  if (!authCodeRequestResponse?.success) {
-    logDeep(authCodeRequestResponse);
-    return authCodeRequestResponse;
+  const accessTokenRequestResponse = await etsyAccessTokenRequest({ 
+    credsPath, 
+    authCode, 
+    authCodeVerifier, 
+  });
+
+  if (!accessTokenRequestResponse?.success) {
+    logDeep(accessTokenRequestResponse);
+    return accessTokenRequestResponse;
   }
-
-  console.log(`2. Copy the codeVerifier above, and the code out of your url. Enter them in .creds.yml as AUTH_CODEVERIFIER and AUTH_CODE.`);
+  
+  console.log(`AUTH_CODE: ${ authCode }`);
+  console.log(`AUTH_CODEVERIFIER: ${ authCodeVerifier }`);
+  console.log(`4. Enter these in .creds.yml for posterity.`);
   await askQuestion('Done! (Enter to continue)');
+
+  console.log(`5. All sorted!`);
+
+  return {
+    success: true,
+    result: {
+      authCode,
+      authCodeVerifier,
+    },
+  };
 };
 
 const etsyAuthFlowWalkthroughApi = funcApi(etsyAuthFlowWalkthrough, {
