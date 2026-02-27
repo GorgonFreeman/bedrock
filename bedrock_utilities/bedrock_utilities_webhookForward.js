d65cd21fa0d2f321619d61bd6fab281ece585064
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-27 15:32:51 +1100

    facilitate forwarding url from a number of sources

--- bedrock_utilities/bedrock_utilities_webhookForward.js
+++ bedrock_utilities/bedrock_utilities_webhookForward.js
@@ -1,15 +1,56 @@
 // A function that responds quickly to a webhook, and sends the webhook where it needs to go. 
 // This is to separate the response from the action taken.
 
+require('dotenv').config();
+
 const { funcApi, customAxios } = require('../utils');
 
 const bedrock_utilities_webhookForward = async (req) => {
 
-  const { body } = req;
+  const { headers, body, query } = req;
 
-  const forwardUrl = 'TODO';
+  let forwardUrl;
 
-  customAxios('post', forwardUrl, { originalHeaders: headers, ...body });
+  // Allow passing forward url in a number of ways, by preference.
+
+  // 1. From options arg in body
+  if (!forwardUrl) {
+    const { options } = body;
+    const { forwardUrl: optionsForwardUrl } = options;
+    if (optionsForwardUrl) {
+      forwardUrl = optionsForwardUrl;
+    }
+  }
+
+  // 2. From query string
+  if (!forwardUrl) {
+    const { forwardUrl: queryStringForwardUrl } = query;
+    if (queryStringForwardUrl) {
+      forwardUrl = queryStringForwardUrl;
+    }
+  }
+
+  // 3. From custom header
+  if (!forwardUrl) {
+    const { 'x-forward-url': customHeaderForwardUrl } = headers;
+    if (customHeaderForwardUrl) {
+      forwardUrl = customHeaderForwardUrl;
+    }
+  }
+  
+  // 4. From environment variables
+  const { env } = process;
+  const { FORWARD_URL } = env;
+  if (!forwardUrl) {
+    forwardUrl = FORWARD_URL;
+  }
+
+  customAxios('post', forwardUrl, { 
+    metadata: { 
+      headers,
+    }, 
+    ...body,
+  });
 
   return { 
     success: true,
