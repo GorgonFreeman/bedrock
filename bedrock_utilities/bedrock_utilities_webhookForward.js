f05b86517bf5cb483b87f371ece22d1293fdf3ba
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-02-27 12:51:57 +1100

    bedrock_utilities_webhookForward stub

---
+++ bedrock_utilities/bedrock_utilities_webhookForward.js
@@ -0,0 +1,26 @@
+const { funcApi } = require('../utils');
+
+const bedrock_utilities_webhookForward = async (
+  arg,
+  {
+    option,
+  } = {},
+) => {
+
+  return { 
+    arg, 
+    option,
+  };
+  
+};
+
+const bedrock_utilities_webhookForwardApi = funcApi(bedrock_utilities_webhookForward, {
+  argNames: ['arg', 'options'],
+});
+
+module.exports = {
+  bedrock_utilities_webhookForward,
+  bedrock_utilities_webhookForwardApi,
+};
+
+// curl localhost:8000/bedrock_utilities_webhookForward -H "Content-Type: application/json" -d '{ "arg": "1234" }'
