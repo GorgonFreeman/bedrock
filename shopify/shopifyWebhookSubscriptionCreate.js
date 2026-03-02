64a0828f67c0aec98d515816a7d47bb0a8212b3e
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-03-02 14:18:32 +1100

    fix miscased example command option

--- shopify/shopifyWebhookSubscriptionCreate.js
+++ shopify/shopifyWebhookSubscriptionCreate.js
@@ -60,4 +60,4 @@ module.exports = {
 };
 
 // curl http://localhost:8000/shopifyWebhookSubscriptionCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "topic": "CUSTOMERS_UPDATE", "uri": "https://..." }'
-// curl http://localhost:8000/shopifyWebhookSubscriptionCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "topic": "CUSTOMERS_UPDATE", "uri": "https://...", "options": { "format": "json", "includeFields": ["id", "note"], "metafields": [{ "namespace": "facts", "key": "date_of_birth" }] } }'
+// curl http://localhost:8000/shopifyWebhookSubscriptionCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "topic": "CUSTOMERS_UPDATE", "uri": "https://...", "options": { "format": "JSON", "includeFields": ["id", "note"], "metafields": [{ "namespace": "facts", "key": "date_of_birth" }] } }'
