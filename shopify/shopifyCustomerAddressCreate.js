728810fb5f4c321ac47716d70b0131ca622de1e1
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-03-09 11:37:42 +1100

    shopifyCustomerAddressCreate complete function

--- shopify/shopifyCustomerAddressCreate.js
+++ shopify/shopifyCustomerAddressCreate.js
@@ -1,29 +1,42 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageCreate
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/customerAddressCreate
 
 const { funcApi, logDeep } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
 
-const defaultAttrs = `id title handle`;
+const defaultAttrs = `address1 address2 city countryCode provinceCode zip`;
 
 const shopifyCustomerAddressCreate = async (
   credsPath,
-  pageInput,
+  customerId,
+  addressInput,
   {
     apiVersion,
     returnAttrs = defaultAttrs,
+
+    setAsDefault = false,
   } = {},
 ) => {
 
   const response = await shopifyMutationDo(
     credsPath,
-    'pageCreate',
+    'customerAddressCreate',
     {
-      page: {
-        type: 'PageCreateInput!',
-        value: pageInput,
+      customerId: {
+        type: 'ID!',
+        value: `gid://shopify/Customer/${ customerId }`,
       },
+      address: {
+        type: 'MailingAddressInput!',
+        value: addressInput,
+      },
+      ...(setAsDefault && {
+        setAsDefault: {
+          type: 'Boolean',
+          value: setAsDefault,
+        },
+      }),
     },
-    `page { ${ returnAttrs } }`,
+    `address { ${ returnAttrs } }`,
     { 
       apiVersion,
     },
@@ -33,7 +46,12 @@ const shopifyCustomerAddressCreate = async (
 };
 
 const shopifyCustomerAddressCreateApi = funcApi(shopifyCustomerAddressCreate, {
-  argNames: ['credsPath', 'pageInput', 'options'],
+  argNames: ['credsPath', 'customerId', 'addressInput', 'options'],
+  validatorsByArg: {
+    credsPath: Boolean,
+    customerId: Boolean,
+    addressInput: Boolean,
+  },
 });
 
 module.exports = {
@@ -41,4 +59,4 @@ module.exports = {
   shopifyCustomerAddressCreateApi,
 };
 
-// curl http://localhost:8000/shopifyCustomerAddressCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "pageInput": { "title": "Batarang Blueprints", "body": "<strong>Good page!</strong>" }, "options": { "returnAttrs": "id" } }'
+// curl http://localhost:8000/shopifyCustomerAddressCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "customerId": "gid://shopify/Customer/1234567890", "addressInput": { "address1": "123 Main St", "city": "Anytown", "countryCode": "US", "provinceCode": "CA", "zip": "12345" } }'
