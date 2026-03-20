83f115cbf7a2de5167a52f8af9284550a4f0c58a
Zwe <zwe@whitefoxboutique.com>
2026-03-20 14:39:09 +1100

    add command reference

--- slack/slackInteractiveBirthdayDiscountCodeGenerate.js
+++ slack/slackInteractiveBirthdayDiscountCodeGenerate.js
@@ -1,6 +1,6 @@
 const { respond, logDeep, customAxios } = require('../utils');
 
-const COMMAND_NAME = 'slash_command'; // slash command
+const COMMAND_NAME = 'customer_bday_code_create'; // slash command
 
 const slackInteractiveBirthdayDiscountCodeGenerate = async (req, res) => {
   console.log('slackInteractiveBirthdayDiscountCodeGenerate');
