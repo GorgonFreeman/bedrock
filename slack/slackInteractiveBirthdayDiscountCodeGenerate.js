fe246bbbfcbc05cbceae46627b7e718a32159675
Zwe <zwe@whitefoxboutique.com>
2026-03-20 14:40:41 +1100

    add blocks configs

--- slack/slackInteractiveBirthdayDiscountCodeGenerate.js
+++ slack/slackInteractiveBirthdayDiscountCodeGenerate.js
@@ -2,6 +2,85 @@ const { respond, logDeep, customAxios } = require('../utils');
 
 const COMMAND_NAME = 'customer_bday_code_create'; // slash command
 
+const blocks = {
+  email_select: {
+    ask: [
+      {
+        type: 'input',
+        block_id: 'email_input',
+        label: {
+          type: 'plain_text',
+          text: 'Email Address',
+        },
+        element: {
+          type: 'plain_text_input',
+          action_id: 'email',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Enter email address',
+          },
+        },
+      },
+      {
+        type: 'actions',
+        block_id: 'email_actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Save',
+            },
+            action_id: `${ COMMAND_NAME }:email:save`,
+            style: 'primary',
+          },
+        ],
+      },
+    ],
+    display: (emailAddress) => {
+      return {
+        type: 'section',
+        block_id: 'state:email',
+        text: {
+          type: 'mrkdwn',
+          text: `Email: ${ emailAddress }`,
+        },
+      };
+    },
+  },
+  store_select: {
+    ask: [
+      {
+        type: 'actions',
+        block_id: 'store_select_actions',
+        elements: REGIONS_WF.map(region => ({
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: region.toUpperCase(),
+          },
+          value: region,
+          action_id: `${ COMMAND_NAME }:store_select:${ region }`,
+        })),
+      },
+    ],
+  },
+  cancel: {
+    type: 'actions',
+    elements: [
+      {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Cancel',
+        },
+        value: 'cancel',
+        action_id: `${ COMMAND_NAME }:cancel`,
+      },
+    ],
+  },
+};
+
 const slackInteractiveBirthdayDiscountCodeGenerate = async (req, res) => {
   console.log('slackInteractiveBirthdayDiscountCodeGenerate');
 
