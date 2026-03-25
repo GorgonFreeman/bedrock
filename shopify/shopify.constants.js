8926b414cbd482fe84c962254a64753767c84057
Zwe <zwe@whitefoxboutique.com>
2026-03-25 16:31:41 +1100

    add region constants in bedrock

--- shopify/shopify.constants.js
+++ shopify/shopify.constants.js
@@ -1,5 +1,48 @@
 module.exports = {
   REGIONS_WF: ['au', 'us', 'uk'],
+  EU_REGION_COUNTRIES: [
+    'AT', // Austria
+    'BE', // Belgium
+    'HR', // Croatia
+    'DK', // Denmark
+    'FI', // Finland
+    'FR', // France
+    'DE', // Germany
+    'GR', // Greece
+    'HU', // Hungary
+    'IE', // Ireland
+    'IT', // Italy
+    'LT', // Lithuania
+    'LU', // Luxembourg
+    'MT', // Malta
+    'NL', // Netherlands
+    'NO', // Norway
+    'PL', // Poland
+    'PT', // Portugal
+    'SI', // Slovenia
+    'ES', // Spain
+    'SE', // Sweden
+    'CH', // Switzerland
+  ],
+  AE_REGION_COUNTRIES: [
+    'AE', // United Arab Emirates
+    'SA', // Saudi Arabia
+  ],
+  UK_REGION_COUNTRIES: [
+    'GB', // United Kingdom
+    'GG', // Guernsey
+    'JE', // Jersey
+    'IM', // Isle of Man
+  ],
+  US_REGION_COUNTRIES: [
+    'US', // United States
+  ],
+  AU_REGION_COUNTRIES: [
+    'AU', // Australia
+    'NZ', // New Zealand
+    'CA', // Canada
+    'MX', // Mexico
+  ],
   HOSTNAME_TO_CREDSPATH: {
     'whitefoxboutique.com.au': 'au',
     'whitefoxboutique.com': 'us',
