a6adca9c5afbd95d5fec0581ea7004cc18bfb21e
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-04-14 16:07:47 +1000

    compose shopifyMetafieldValuesDelete with reference

--- shopify/shopifyMetafieldValuesDelete.js
+++ shopify/shopifyMetafieldValuesDelete.js
@@ -1,53 +1,180 @@
-const { funcApi, logDeep } = require('../utils');
-const { shopifyClient } = require('../shopify/shopify.utils');
+const { funcApi, logDeep, surveyNestedArrays, Processor, FakeGetter } = require('../utils');
+const { shopifyGetter } = require('../shopify/shopify.utils');
+const { shopifyMetafieldsDelete } = require('../shopify/shopifyMetafieldsDelete');
+const { shopifyBulkOperationDo } = require('../shopify/shopifyBulkOperationDo');
 
-const defaultAttrs = `id`;
+const {
+  MAX_METAFIELDS_PER_SET,
+} = require('../shopify/shopify.constants');
 
 const shopifyMetafieldValuesDelete = async (
-  credsPath,
-  arg,
+  store,
+  resource,
+  metafieldPath,
   {
     apiVersion,
-    option,
+    useBulkFetch = false,
   } = {},
 ) => {
 
-  const query = `
-    query GetProduct($id: ID!) {
-      product(id: $id) {
-        ${ attrs }
-      }
+  logDeep({
+    store,
+    resource,
+    metafieldPath,
+  });
+
+  const piles = {
+    resources: [],
+    shopifyMetafieldsDelete: [],
+    results: [],
+  };
+
+  const [mfNamespace, mfKey] = metafieldPath.split('.');
+
+  const attrs = `
+    id
+    targetMetafield: metafield(namespace: "${ mfNamespace }", key: "${ mfKey }") {
+      id
+      type
+      value
     }
   `;
 
-  const variables = {
-    id: `gid://shopify/Product/${ arg }`,
-  };
+  let getter;
 
-  const response = await shopifyClient.fetch({
-    method: 'post',
-    body: { query, variables },
-    context: {
-      credsPath,
-      apiVersion,
+  if (useBulkFetch) {
+
+    const shopifyBulkQuery = `{
+      ${ resource }s(query: "metafields.${ mfNamespace }.${ mfKey }:*") {
+        edges {
+          node {
+            ${ attrs }
+          }
+        }
+      }
+    }`;
+
+    getter = new FakeGetter(
+      shopifyBulkOperationDo,
+      [
+        store,
+        'query',
+        shopifyBulkQuery,
+        {
+          apiVersion,
+        },
+      ],
+      {
+        onItems: (items) => {
+          for (const item of items) {
+            if (item?.targetMetafield) {
+              piles.resources.push(item);
+            }
+          }
+        },
+        onDone: () => {
+          logDeep(surveyNestedArrays(piles));
+        },
+      },
+    );
+
+  } else {
+    getter = await shopifyGetter(
+      store,
+      resource,
+      {
+        queries: [`metafields.${ mfNamespace }.${ mfKey }:*`],
+        attrs,
+
+        apiVersion,
+
+        onItems: (items) => {
+          piles.resources.push(...items);
+
+          logDeep(surveyNestedArrays(piles));
+          logDeep(piles.resources[piles.resources.length - 1]);
+        },
+      },
+    );
+  }
+
+  const assessor = new Processor(
+    piles.resources,
+    async (pile) => {
+      const resourceRow = pile.shift();
+      const { id: resourceGid, targetMetafield } = resourceRow;
+
+      if (!targetMetafield) {
+        return;
+      }
+
+      piles.shopifyMetafieldsDelete.push({
+        ownerId: resourceGid,
+        namespace: mfNamespace,
+        key: mfKey,
+      });
     },
-    interpreter: async (response) => {
-      // console.log(response);
-      return {
-        ...response,
-        ...response.result ? {
-          result: response.result.product,
-        } : {},
-      };
+    pile => pile.length === 0,
+    {
+      canFinish: false,
     },
+  );
+
+  getter.on('done', () => {
+    assessor.canFinish = true;
   });
 
-  logDeep(response);
-  return response;
+  const actioner = new Processor(
+    piles.shopifyMetafieldsDelete,
+    async (pile) => {
+      const metafieldPayloads = pile.splice(0, MAX_METAFIELDS_PER_SET);
+
+      logDeep(metafieldPayloads);
+
+      const response = await shopifyMetafieldsDelete(
+        store,
+        metafieldPayloads,
+        {
+          apiVersion,
+        },
+      );
+
+      if (!response?.success) {
+        console.error(response);
+      }
+
+      piles.results.push(response);
+    },
+    pile => pile.length === 0,
+    {
+      canFinish: false,
+      logFlavourText: 'shopifyMetafieldsDelete',
+    },
+  );
+
+  assessor.on('done', () => {
+    actioner.canFinish = true;
+  });
+
+  await Promise.all([
+    getter.run(),
+    assessor.run(),
+    actioner.run(),
+  ]);
+
+  return {
+    success: true,
+    result: surveyNestedArrays(piles),
+  };
 };
 
 const shopifyMetafieldValuesDeleteApi = funcApi(shopifyMetafieldValuesDelete, {
-  argNames: ['credsPath', 'arg', 'options'],
+  argNames: ['store', 'resource', 'metafieldPath', 'options'],
+  validatorsByArg: {
+    store: Boolean,
+    resource: Boolean,
+    metafieldPath: p => p.includes('.'),
+  },
 });
 
 module.exports = {
@@ -55,4 +182,4 @@ module.exports = {
   shopifyMetafieldValuesDeleteApi,
 };
 
-// curl localhost:8000/shopifyMetafieldValuesDelete -H "Content-Type: application/json" -d '{ "credsPath": "au", "arg": "6979774283848" }'
+// curl localhost:8000/shopifyMetafieldValuesDelete -H "Content-Type: application/json" -d '{ "store": "au", "resource": "customer", "metafieldPath": "facts.birth_date", "options": { "useBulkFetch": true } }'
