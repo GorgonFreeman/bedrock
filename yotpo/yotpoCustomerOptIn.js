344678ee13454d6df145710157d409a21ed30d90
Zwe <zwe@whitefoxboutique.com>
2026-04-28 13:17:10 +1000

    completed customer opt in/out function

--- yotpo/yotpoCustomerOptIn.js
+++ yotpo/yotpoCustomerOptIn.js
@@ -1,15 +1,46 @@
+// https://loyaltyapi.yotpo.com/reference/createupdate-customer-records
+
 const { funcApi, logDeep } = require('../utils');
+const { yotpoCustomerGet } = require('../yotpo/yotpoCustomerGet');
 const { yotpoClient } = require('../yotpo/yotpo.utils');
 
 const yotpoCustomerOptIn = async ( 
   credsPath,
   {
-    apiVersion,
+    customerId,
+    customerEmail,
+    customerPhone,
+    posAccountId,
+  },
+  {
+    apiVersion = 'v2',
+    countryCodeIso,
+    optedIn = true,
   } = {},
 ) => {
 
+  if (!customerEmail) {
+    const customerGetResponse = await yotpoCustomerGet(credsPath, {
+      ...(customerId && { customerId }),
+      ...(customerPhone && { customerPhone }),
+      ...(posAccountId && { posAccountId }),
+    }, { apiVersion, countryCodeIso });
+    const { success, result: customer } = customerGetResponse;
+    if (!success) {
+      return customerGetResponse;
+    }
+    customerEmail = customer.email;
+  }
+
+  const params = {
+    email: customerEmail,
+    opted_in: optedIn,
+  }
+
   const response = await yotpoClient.fetch({
-    url: `/endpoint`,
+    url: `/customers`,
+    method: 'post',
+    params,
     context: {
       credsPath,
       apiVersion,
@@ -20,7 +51,7 @@ const yotpoCustomerOptIn = async (
 };
 
 const yotpoCustomerOptInApi = funcApi(yotpoCustomerOptIn, {
-  argNames: ['credsPath', 'options'],
+  argNames: ['credsPath', 'customerIdentifier', 'options'],
 });
 
 module.exports = {
@@ -28,4 +59,6 @@ module.exports = {
   yotpoCustomerOptInApi,
 };
 
-// curl localhost:8000/yotpoCustomerOptIn -H "Content-Type: application/json" -d '{ "credsPath": "au" }'
+// curl localhost:8000/yotpoCustomerOptIn -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerIdentifier": { "customerId": "8275235995720" } }'
+// curl localhost:8000/yotpoCustomerOptIn -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerIdentifier": { "customerEmail": "zwe+test@whitefoxboutique.com" } }'
+// curl localhost:8000/yotpoCustomerOptIn -H "Content-Type: application/json" -d '{ "credsPath": "au", "customerIdentifier": { "customerEmail": "zwe+test@whitefoxboutique.com" }, "options": { "optedIn": false } }'
