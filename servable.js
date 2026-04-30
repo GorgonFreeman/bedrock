466774f75f01143df756a83903f99458939bc348
Zwe <zwe@whitefoxboutique.com>
2026-04-30 10:51:49 +1000

    add databases get servable endpoint

--- servable.js
+++ servable.js
@@ -51,6 +51,7 @@ module.exports = {
   starshipitProductsDelete: lazy(() => require('./starshipit/starshipitProductsDelete').starshipitProductsDeleteApi),
   starshipitProductsGet: lazy(() => require('./starshipit/starshipitProductsGet').starshipitProductsGetApi),
   starshipitTrackingGet: lazy(() => require('./starshipit/starshipitTrackingGet').starshipitTrackingGetApi),
+  snowflakeDatabasesGet: lazy(() => require('./snowflake/snowflakeDatabasesGet').snowflakeDatabasesGetApi),
   snowflakeAuthGet: lazy(() => require('./snowflake/snowflakeAuthGet').snowflakeAuthGetApi),
   slackMessageDelete: lazy(() => require('./slack/slackMessageDelete').slackMessageDeleteApi),
   slackMessagePost: lazy(() => require('./slack/slackMessagePost').slackMessagePostApi),
