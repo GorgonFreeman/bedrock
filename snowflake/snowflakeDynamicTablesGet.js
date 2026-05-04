8e1b71ebe0ea826154b4c7202a252ffad5d66a5b
Zwe <zwe@whitefoxboutique.com>
2026-05-04 17:02:49 +1000

    snowflakeDynamicTablesGet stub

---
+++ snowflake/snowflakeDynamicTablesGet.js
@@ -0,0 +1,57 @@
+// https://docs.snowflake.com/en/developer-guide/snowflake-rest-api/reference
+
+const { funcApi, logDeep } = require('../utils');
+const { snowflakeGet, snowflakeGetter } = require('../snowflake/snowflake.utils');
+
+// Payload maker function
+const payloadMaker = (
+  {
+    credsPath,
+    apiVersion = 'v2',
+
+    // params
+
+    // Used by pagination in snowflake.utils.js
+    showLimit, // items per page
+    fromName, // pagination cursor
+  } = {},
+) => {
+
+  const params = {
+    // params
+    ...(showLimit && { showLimit }),
+    ...(fromName && { fromName }),
+  }
+
+  return [
+    `/api/${ apiVersion }/resourceName`,
+    {
+      params
+    },
+  ];
+}
+
+// Get function
+const snowflakeDynamicTablesGet = async (...args) => {
+  const response = await snowflakeGet(...payloadMaker(...args));
+  return response;
+};
+
+// Getter function
+// const snowflakeDynamicTablesGetter = async (...args) => {
+//   const response = await snowflakeGetter(...payloadMaker(...args));
+//   return response;
+// };
+
+const snowflakeDynamicTablesGetApi = funcApi(snowflakeDynamicTablesGet, {
+  argNames: ['options'],
+  validatorsByArg: {},
+});
+
+module.exports = {
+  snowflakeDynamicTablesGet,
+  snowflakeDynamicTablesGetApi,
+};
+
+// curl localhost:8000/snowflakeDynamicTablesGet
+// curl localhost:8000/snowflakeDynamicTablesGet -H "Content-Type: application/json" -d '{ "options": {  } }'
