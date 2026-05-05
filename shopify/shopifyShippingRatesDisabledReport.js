a5822e5f13336c8e8ba3fde9dabd1db841ad1871
Zwe <zwe@whitefoxboutique.com>
2026-05-05 10:56:22 +1000

    fetch all shipping rates from all regions

--- shopify/shopifyShippingRatesDisabledReport.js
+++ shopify/shopifyShippingRatesDisabledReport.js
@@ -10,6 +10,64 @@ const shopifyShippingRatesDisabledReport = async (
 ) => {
 
   // 1. Fetch all shipping rates from all regions
+  const shippingRates = await (async () => {
+    const deliveryProfileAttrs = `id name profileLocationGroups {
+      locationGroup {
+        id
+      }
+      locationGroupZones (first: 15) { edges { node {
+        zone {
+          id
+          name
+        }
+        methodDefinitions (first: 10) { edges { node {
+          id
+          name
+          active
+        } } }
+      } } }
+    }`;
+
+    // Flatten the shipping rates by store
+    const flatForStore = (deliveryProfiles, store) => {
+      return deliveryProfiles.map(dp => {
+        const { id: deliveryProfileId, name: deliveryProfileName } = dp;
+        return dp.profileLocationGroups.map(plg => {
+          const { locationGroup } = plg;
+          const { id: locationGroupId } = locationGroup;
+          return plg.locationGroupZones.map(lgz => {
+            const { zone } = lgz;
+            const { id: locationGroupZoneId, name: locationGroupZoneName } = zone;
+            return lgz.methodDefinitions.map(methodDef => {
+              return {
+                ...methodDef,
+                store,
+                deliveryProfileId,
+                deliveryProfileName,
+                locationGroupId,
+                locationGroupZoneId,
+                locationGroupZoneName,
+              };
+            });
+          });
+        });
+      }).flat(3);
+    };
+  
+    // Fetch all shipping rates from all regions
+    const shippingRates = await Promise.all(
+      regions.map(async (store) => {
+        const deliveryProfilesResponse = await shopifyDeliveryProfilesGet(store, { attrs: deliveryProfileAttrs });
+        const { success: deliveryProfilesGetSuccess, result: deliveryProfiles } = deliveryProfilesResponse;
+        if (!deliveryProfilesGetSuccess || !deliveryProfiles?.length) {
+          return [];
+        }
+        return flatForStore(deliveryProfiles, store);
+      }),
+    );
+    return shippingRates.flat();
+  })();
+  // logDeep({ shippingRates });
 
   // 2. Filter out the shipping rates that are disabled
 
@@ -22,7 +80,7 @@ const shopifyShippingRatesDisabledReport = async (
 };
 
 const shopifyShippingRatesDisabledReportApi = funcApi(shopifyShippingRatesDisabledReport, {
-  argNames: ['credsPath', 'arg', 'options'],
+  argNames: ['options'],
 });
 
 module.exports = {
@@ -30,4 +88,5 @@ module.exports = {
   shopifyShippingRatesDisabledReportApi,
 };
 
-// curl localhost:8000/shopifyShippingRatesDisabledReport -H "Content-Type: application/json" -d '{ "credsPath": "au", "arg": "6979774283848" }'
+// curl localhost:8000/shopifyShippingRatesDisabledReport
+// curl localhost:8000/shopifyShippingRatesDisabledReport -H "Content-Type: application/json" -d '{ "options": { "regions": ["au"] } }'
