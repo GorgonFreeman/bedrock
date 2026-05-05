0ae11240ec62a3fedc6bba6c587abd52e0403efb
Zwe <zwe@whitefoxboutique.com>
2026-05-05 16:11:20 +1000

    fetch all shipping rates to scan for snooze options

--- slack/slackInteractiveShippingRatesDisabledReport.js
+++ slack/slackInteractiveShippingRatesDisabledReport.js
@@ -1,6 +1,64 @@
-const { respond, logDeep, customAxios } = require('../utils');
+const { respond, logDeep, customAxios, arrayToObj } = require('../utils');
+const { REGIONS_WF } = require('../constants');
+const { shopifyDeliveryProfilesGet } = require('../shopify/shopifyDeliveryProfilesGet');
 
 const COMMAND_NAME = 'shipping_rates_disabled_report'; // slash command
+
+// Fetch a flattened lisst of shipping rates per store
+const deliveryProfilesGetFlatByStore = async () => {
+  const deliveryProfileAttrs = `id name profileLocationGroups {
+    locationGroup {
+      id
+    }
+    locationGroupZones (first: 15) { edges { node {
+      zone {
+        id
+        name
+      }
+      methodDefinitions (first: 10) { edges { node {
+        id
+        name
+        active
+      } } }
+    } } }
+  }`;
+  const flatForStore = (deliveryProfiles, store) => {
+    return deliveryProfiles.map(dp => {
+      const { id: deliveryProfileId, name: deliveryProfileName } = dp;
+      return dp.profileLocationGroups.map(plg => {
+        const { locationGroup } = plg;
+        const { id: locationGroupId } = locationGroup;
+        return plg.locationGroupZones.map(lgz => {
+          const { zone } = lgz;
+          const { id: locationGroupZoneId, name: locationGroupZoneName } = zone;
+          return lgz.methodDefinitions.map(methodDef => {
+            return {
+              ...methodDef,
+              store,
+              deliveryProfileId,
+              deliveryProfileName,
+              locationGroupId,
+              locationGroupZoneId,
+              locationGroupZoneName,
+            };
+          });
+        });
+      });
+    }).flat(3);
+  };
+
+  const shippingRatesByStore = await Promise.all(
+    REGIONS_WF.map(async (store) => {
+      const deliveryProfilesResponse = await shopifyDeliveryProfilesGet(store, { attrs: deliveryProfileAttrs });
+      const { success: deliveryProfilesGetSuccess, result: deliveryProfiles } = deliveryProfilesResponse;
+      if (!deliveryProfilesGetSuccess || !deliveryProfiles?.length) {
+        return [];
+      }
+      return flatForStore(deliveryProfiles, store);
+    }),
+  );
+  return shippingRatesByStore.flat();
+};
 
 const slackInteractiveShippingRatesDisabledReport = async (req, res) => {
   console.log('slackInteractiveShippingRatesDisabledReport');
@@ -14,6 +72,9 @@ const slackInteractiveShippingRatesDisabledReport = async (req, res) => {
 
   // Because we got to this point, we have a payload - handle as an interactive step
   respond(res, 200); // Acknowledge immediately - we'll provide the next step to the response_url later
+
+  // Fetch all shipping rates
+  const shippingRatesByStore = await deliveryProfilesGetFlatByStore();
 
   const payload = JSON.parse(body.payload);
   logDeep('payload', payload);
