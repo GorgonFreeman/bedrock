699039a18d7446ed0a764243d21254fd95abfce4
Zwe <zwe@whitefoxboutique.com>
2026-05-11 17:15:16 +1000

    add the most basic asana task create wrapper

--- asana/asanaTaskCreate.js
+++ asana/asanaTaskCreate.js
@@ -1,18 +1,41 @@
-// https://developers.asana.com/reference/getuser
+// https://developers.asana.com/reference/createtask
 
 const { HOSTED } = require('../constants');
+const { DEV_PROJECT_ID } = require('../asana/asana.constants');
 const { funcApi, logDeep } = require('../utils');
 const { asanaClient } = require('../asana/asana.utils');
 
 const asanaTaskCreate = async (
-  thingId,
   {
     credsPath,
+    
+    // params
+    fields,
+    pretty,
+    
+    // body
+    name = "New Task",
+    projects = [ DEV_PROJECT_ID ],
   } = {},
 ) => {
 
+  const data = {
+    ...(name ? { name } : {}),
+    ...(projects.length > 0 ? { projects: projects } : {}),
+  };
+
+  const params = {
+    ...(fields ? { opt_fields: fields } : {}),
+    ...(pretty ? { opt_pretty: pretty } : {}),
+  };
+
   const response = await asanaClient.fetch({
-    url: `/things/${ thingId }`,
+    url: `/tasks`,
+    method: 'post',
+    params,
+    body: {
+      data,
+    },
     context: {
       credsPath,
     },
@@ -23,10 +46,7 @@ const asanaTaskCreate = async (
 };
 
 const asanaTaskCreateApi = funcApi(asanaTaskCreate, {
-  argNames: ['thingId', 'options'],
-  validatorsByArg: {
-    thingId: Boolean,
-  },
+  argNames: ['options'],
 });
 
 module.exports = {
@@ -34,4 +54,4 @@ module.exports = {
   asanaTaskCreateApi,
 };
 
-// curl localhost:8000/asanaTaskCreate
+// curl localhost:8000/asanaTaskCreate -H "Content-Type: application/json" -d '{ "options": { "name": "New Task", "projects": ["1208942389126559"] } }'
