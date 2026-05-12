e5e834a4ade0f0fc6df217b8040e3f0a9c25b491
Zwe <zwe@whitefoxboutique.com>
2026-05-12 16:47:07 +1000

    add action types for specific message shortcut actions

--- slack/slack.constants.js
+++ slack/slack.constants.js
@@ -2,4 +2,9 @@ module.exports = {
   TEAM_DOMAIN_TO_CREDSPATH: {
     'whitefoxchat': '', // Default creds path
   },
+  MESSAGE_ACTION_TYPES: [
+    'message_action',
+    'view_submission',
+    'view_closed',
+  ],
 };
