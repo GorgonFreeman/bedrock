7ef546177c38886bbc9c0bdae9b7b6d428eb223a
Zwe <zwe@whitefoxboutique.com>
2026-05-13 09:24:06 +1000

    use private_metaobject to pass data across different interaction payloads

--- slack/slackInteractiveAsanaDevTaskCreate.js
+++ slack/slackInteractiveAsanaDevTaskCreate.js
@@ -27,24 +27,27 @@ const blocks = {
 
 const modal = {
 
-  initial: {
-    type: 'modal',
-    callback_id: COMMAND_NAME,
-    title: {
-      type: 'plain_text',
-      text: 'Create new task',
-    },
-    blocks: [
-      blocks.name_input,
-    ],
-    close: {
-      type: 'plain_text',
-      text: 'Close',
-    },
-    submit: {
-      type: 'plain_text',
-      text: 'Create Task',
-    },
+  initial: (metadataObject) => {
+    return {
+      type: 'modal',
+      callback_id: COMMAND_NAME,
+      title: {
+        type: 'plain_text',
+        text: 'Create new task',
+      },
+      blocks: [
+        blocks.name_input,
+      ],
+      private_metadata: JSON.stringify(metadataObject),
+      close: {
+        type: 'plain_text',
+        text: 'Close',
+      },
+      submit: {
+        type: 'plain_text',
+        text: 'Create Task',
+      },
+    };
   }
 }
 
@@ -79,17 +82,26 @@ const slackInteractiveAsanaDevTaskCreate = async (req, res) => {
   // No actions in payload
   if (!payload?.actions) {
 
+    let metadataObject;
+
     switch(payloadType) {
 
       // this is a message action initiation - send the initial blocks in a modal
       case 'message_action':
 
-        repsonse = await slackClient.fetch({
+        metadataObject = {
+          messageText: payload?.message?.text,
+          messageBlocks: payload?.message?.blocks,
+          channelId: payload?.channel?.id,
+          channelName: payload?.channel?.name,
+        };
+
+        return slackClient.fetch({
           url: '/views.open',
           method: 'post',
           body: {
             trigger_id: triggerId,
-            view: modal.initial,
+            view: modal.initial(metadataObject),
           },
           context: {
             credsPath,
@@ -100,13 +112,25 @@ const slackInteractiveAsanaDevTaskCreate = async (req, res) => {
       // this is a view submission - process the form data
       case 'view_submission':
 
+        metadataObject = JSON.parse(payload.view.private_metadata);
+
+        const {
+          messageText,
+          messageBlocks,
+          channelId,
+          channelName,
+        } = metadataObject;
+
         const {
           name_input: nameInput,
         } = payload.view.state.values;
   
         const taskName = nameInput[`${ COMMAND_NAME }:name_input`]?.value;
-  
-        console.log('taskName', taskName);
+
+        // Create the dev task in Asana
+
+        // Respond to the user with the task details in slack
+
         break;
 
       default:
