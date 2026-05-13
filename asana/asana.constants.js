a562a7a27fa89515a3d884cf66ae778e017cf815
Zwe <zwe@whitefoxboutique.com>
2026-05-13 13:25:47 +1000

    add dev assignees in constants

--- asana/asana.constants.js
+++ asana/asana.constants.js
@@ -2,4 +2,10 @@ module.exports = {
   MAX_PER_PAGE: 100,
   DEV_PROJECT_ID: '1208942389126559', // Dev Team Tasks
   DEV_DEFAULT_SECTION_ID: '1208942389126560', // Fresh In
+  DEV_ASSIGNEE_EMAILS: {
+    "John": "john@whitefoxboutique.com", // default assignee
+    "Ashlyn": "ashlynhogan@whitefoxboutique.com",
+    "Ben": "benjamin@whitefoxboutique.com",
+    "Zwe": "zwe@whitefoxboutique.com",
+  },
 };
