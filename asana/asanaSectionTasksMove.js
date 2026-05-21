41ce76b9c0c2678e6c10a8553f3c7df8055a45ac
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-05-22 09:43:08 +1000

    asanaSectionTasksMove args

--- asana/asanaSectionTasksMove.js
+++ asana/asanaSectionTasksMove.js
@@ -1,21 +1,34 @@
 const { funcApi } = require('../utils');
 
+const isValidSectionIdentifier = (p) => (p?.sectionName && p?.projectIdentifier) || p?.sectionId;
+const isValidSectionIdentifierProjectOptional = (p) => p?.sectionName || p?.sectionId;
+
 const asanaSectionTasksMove = async (
-  arg,
   {
-    option,
+    fromSectionId,
+
+    fromSectionName,
+    fromProjectIdentifier,
+  },
+  {
+    toSectionId,
+
+    toSectionName,
+    toProjectIdentifier = fromProjectIdentifier, // if moving within the same project, no need to supply projectId a second time
+  },
+  {
+    credsPath,
   } = {},
 ) => {
-
-  return { 
-    arg, 
-    option,
-  };
-  
+  return true;
 };
 
 const asanaSectionTasksMoveApi = funcApi(asanaSectionTasksMove, {
-  argNames: ['arg', 'options'],
+  argNames: ['fromSectionIdentifier', 'toSectionIdentifier', 'options'],
+  validatorsByArg: {
+    fromSectionIdentifier: isValidSectionIdentifier,
+    toSectionIdentifier: isValidSectionIdentifierProjectOptional,
+  },
 });
 
 module.exports = {
@@ -23,4 +36,4 @@ module.exports = {
   asanaSectionTasksMoveApi,
 };
 
-// curl localhost:8000/asanaSectionTasksMove -H "Content-Type: application/json" -d '{ "arg": "1234" }'
+// curl localhost:8000/asanaSectionTasksMove -X POST -H "Content-Type: application/json" -d '{ "fromSectionIdentifier": { "sectionId": "..." }, "toSectionIdentifier": { "sectionName": "...", "projectIdentifier": { "projectHandle": "..." } } }'
