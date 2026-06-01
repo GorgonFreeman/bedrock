fa6c13b71cecde8b12d7770df10927fae146f649
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-01 17:33:58 +1000

    actionMultipleOrSingle support

--- shopify/shopifyOrderCancel.js
+++ shopify/shopifyOrderCancel.js
@@ -1,11 +1,11 @@
 // https://shopify.dev/docs/api/admin-graphql/latest/mutations/ordercancel
 
-const { funcApi, logDeep, customNullish } = require('../utils');
+const { funcApi, customNullish, actionMultipleOrSingle } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
 
 const defaultAttrs = `job { id done }`;
 
-const shopifyOrderCancel = async (
+const shopifyOrderCancelSingle = async (
   credsPath,
   orderId,
   {
@@ -59,7 +59,43 @@ const shopifyOrderCancel = async (
       apiVersion,
     },
   );
-  logDeep(response);
+
+  return response;
+};
+
+const shopifyOrderCancel = async (
+  credsPath,
+  orderId,
+  {
+    apiVersion,
+    returnAttrs = defaultAttrs,
+    reason = 'OTHER',
+    refundMethod,
+    restock = true,
+    staffNote,
+    notifyCustomer = false,
+    queueRunOptions,
+  } = {},
+) => {
+  const response = await actionMultipleOrSingle(
+    orderId,
+    shopifyOrderCancelSingle,
+    (orderId) => ({
+      args: [credsPath, orderId],
+      options: {
+        apiVersion,
+        returnAttrs,
+        reason,
+        refundMethod,
+        restock,
+        staffNote,
+        notifyCustomer,
+      },
+    }),
+    {
+      ...(queueRunOptions ? { queueRunOptions } : {}),
+    },
+  );
   return response;
 };
 
@@ -67,17 +103,23 @@ const shopifyOrderCancelApi = funcApi(shopifyOrderCancel, {
   argNames: [
     'credsPath', 
     'orderId', 
-    'options'
+    'options',
   ],
   validatorsByArg: {
     credsPath: Boolean,
-    orderId: Boolean,
+    orderId: (p) => {
+      if (Array.isArray(p)) {
+        return p.length > 0 && p.every(Boolean);
+      }
+      return Boolean(p);
+    },
   },
 });
 
 module.exports = {
   shopifyOrderCancel,
+  shopifyOrderCancelSingle,
   shopifyOrderCancelApi,
 };
 
-// curl http://localhost:8000/shopifyOrderCancel -H 'Content-Type: application/json' -d '{ "credsPath": "us", "orderId": "1797615550524", "options": { "restock": false } }'
+// curl http://localhost:8000/shopifyOrderCancel -H 'Content-Type: application/json' -d '{ "credsPath": "us", "orderId": "1797615550524", "options": { "restock": false, "refundMethod": { "originalPaymentMethodsRefund": false } } }'
