8c1b1fe2f7190b6d76c7c38daf775a14146618a5
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-11 14:46:55 +1000

    document event sample

--- starshipit/starshipitWebhookTrackingEventHandle.js
+++ starshipit/starshipitWebhookTrackingEventHandle.js
@@ -20,3 +20,28 @@ module.exports = {
   starshipitWebhookTrackingEventHandle,
   starshipitWebhookTrackingEventHandleApi,
 };
+
+/* Event sample
+{
+  headers: {
+    host: '1xxx1-11-111-111-111.ngrok-free.app',
+    'content-length': '270',
+    'content-type': 'application/json',
+    expect: '100-continue',
+    'x-forwarded-for': '11.11.11.11',
+    'x-forwarded-host': '1xxx1-11-111-111-111.ngrok-free.app',
+    'x-forwarded-proto': 'https',
+    'accept-encoding': 'gzip'
+  },
+  body: {
+    order_number: '12345678',
+    order_reference: 'REF123456',
+    carrier_name: 'DHL Express',
+    carrier_service: 'P1',
+    shipment_date: '2026-06-10T12:03:42.1543701Z',
+    tracking_number: 'T01234567890',
+    tracking_status: 'InTransit',
+    last_updated_date: '2026-06-11T04:43:42.1543701Z'
+  }
+}
+*/
