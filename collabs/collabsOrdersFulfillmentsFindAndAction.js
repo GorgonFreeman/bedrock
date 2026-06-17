363b78cb0cb5f21e76ed29a3f50a7d23800f50ee
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-17 14:59:37 +1000

    get orders

--- collabs/collabsOrdersFulfillmentsFindAndAction.js
+++ collabs/collabsOrdersFulfillmentsFindAndAction.js
@@ -1,5 +1,7 @@
 const { funcApi, credsByPath } = require('../utils');
 
+const { shopifyOrdersGet } = require('../shopify/shopifyOrdersGet');
+
 const collabsOrdersFulfillmentsFindAndAction = async (
   store,
   {
@@ -22,7 +24,25 @@ const collabsOrdersFulfillmentsFindAndAction = async (
     savedSearchId = trackingCheckViewId;
   }
 
+  const orderGetOptions = {
+    ...savedSearchId ? { savedSearchId } : {},
+    ...!savedSearchId && orderQueries ? { queries: orderQueries } : {},
+  };
+
   // Get all orders in Shopify that are not completely fulfilled, or, without tracking
+  const ordersResponse = await shopifyOrdersGet(store, {
+    ...orderGetOptions,
+  });
+
+  const { success: ordersSuccess, result: orders } = ordersResponse;
+
+  if (!ordersSuccess) {
+    return ordersResponse;
+  }
+
+  logDeep(orders);
+  await askQuestion('?');
+
   // For each order, run collabsOrderFulfillmentFindV2 with autofulfill true
 
   return { 
