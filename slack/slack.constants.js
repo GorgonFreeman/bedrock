d39f3e58da74280deae2dc307d443bc4d90d7ebf
Zwe <zwe@whitefoxboutique.com>
2026-07-17 14:59:50 +1000

    add message action types to allow message shortcut slack interactions

--- slack/slack.constants.js
+++ slack/slack.constants.js
@@ -2,4 +2,10 @@ module.exports = {
   TEAM_DOMAIN_TO_CREDSPATH: {
     'whitefoxchat': '', // Default creds path
   },
+  MESSAGE_ACTION_TYPES: [
+    'message_action',
+    'view_submission',
+    'view_closed',
+  ],
+  DEV_CHANNEL_ID: 'C07NM89BMPW',
 };
