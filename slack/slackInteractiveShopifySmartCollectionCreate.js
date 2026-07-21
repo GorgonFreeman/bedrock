1e4c626b251e46edfe1981b5b985e667e85bc0d1
Zwe <zwe@whitefoxboutique.com>
2026-07-21 14:34:59 +1000

    add initial blocks and initial allowed channel list

--- slack/slackInteractiveShopifySmartCollectionCreate.js
+++ slack/slackInteractiveShopifySmartCollectionCreate.js
@@ -1,24 +1,125 @@
 const { respond, logDeep, customAxios } = require('../utils');
 
-const COMMAND_NAME = 'slash_command'; // slash command
+// const { SLACK_CHANNELS_DEV } = require('../slack/slack.constants'); // Only available on another channel at the moment
+
+const { slackCommandRestrictToChannels } = require('../slack/slack.utils');
+
+const COMMAND_NAME = 'smart_collection_create'; // slash command
+const ALLOWED_CHANNELS = [
+  `foxtron_${ COMMAND_NAME }`,
+  'foxtron_testing',
+  // ...SLACK_CHANNELS_DEV,
+];
+
+const blocks = {
+
+  initial: {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*Create a new smart collection*`,
+    },
+  },
+
+  title_input: {
+    type: 'input',
+    label: {
+      type: 'plain_text',
+      text: 'Title',
+    },
+    element: {
+      type: 'plain_text_input',
+    },
+  },
+
+  description_input: {
+    type: 'input',
+    label: {
+      type: 'plain_text',
+      text: 'Description',
+    },
+    element: {
+      type: 'plain_text_input',
+      multiline: true,
+    },
+  },
+
+  tag_input: {
+    type: 'input',
+    label: {
+      type: 'plain_text',
+      text: 'Tag',
+    },
+    element: {
+      type: 'plain_text_input',
+    },
+  },
+
+  buttons: {
+    type: 'actions',
+    elements: [
+      {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+        action_id: `${ COMMAND_NAME }:submit`,
+      },
+      {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Cancel',
+        },
+        action_id: `${ COMMAND_NAME }:cancel`,
+      },
+    ],
+  },
+
+  error: (message) => {
+    return {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*Error:* ${ message }`,
+      },
+    };
+  },
+
+  result: (message) => {
+    return {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `${ message }`,
+      },
+    };
+  },
+
+}
 
 const slackInteractiveShopifySmartCollectionCreate = async (req, res) => {
   console.log('slackInteractiveShopifySmartCollectionCreate');
 
   const { body } = req;
+
+  if (!slackCommandRestrictToChannels(req, res, ALLOWED_CHANNELS)) {
+    return;
+  }
   
   // If no payload, this is an initiation, e.g. slash command - send the initial blocks
   if (!body?.payload) {
 
     const initialBlocks = [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `I don't do anything yet :hugging_face:`,
-        },
-      },
+      blocks.initial,
+      blocks.title_input,
+      blocks.description_input,
+      blocks.tag_input,
+      blocks.buttons,
     ];
+
+    logDeep('initialBlocks', initialBlocks);
 
     return respond(res, 200, {
       response_type: 'in_channel',
