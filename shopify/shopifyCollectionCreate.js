a3952240c98fd7622d8373187e407369e8cc63d8
Zwe <zwe@whitefoxboutique.com>
2026-07-21 14:21:42 +1000

    final shopify collection create function

--- shopify/shopifyCollectionCreate.js
+++ shopify/shopifyCollectionCreate.js
@@ -1,13 +1,13 @@
-// https://shopify.dev/docs/api/admin-graphql/latest/mutations/pageCreate
+// https://shopify.dev/docs/api/admin-graphql/latest/mutations/collectionCreate
 
-const { funcApi, logDeep } = require('../utils');
+const { respond, mandateParam, logDeep } = require('../utils');
 const { shopifyMutationDo } = require('../shopify/shopify.utils');
 
 const defaultAttrs = `id title handle`;
 
 const shopifyCollectionCreate = async (
   credsPath,
-  pageInput,
+  collectionInput,
   {
     apiVersion,
     returnAttrs = defaultAttrs,
@@ -16,29 +16,77 @@ const shopifyCollectionCreate = async (
 
   const response = await shopifyMutationDo(
     credsPath,
-    'pageCreate',
+    'collectionCreate',
     {
-      page: {
-        type: 'PageCreateInput!',
-        value: pageInput,
+      input: {
+        type: 'CollectionInput!',
+        value: collectionInput,
       },
     },
-    `page { ${ returnAttrs } }`,
+    `collection { ${ returnAttrs } }`,
     { 
       apiVersion,
+
+      // client options
+      interpreter: async (response) => {
+        return {
+          ...response,
+          ...response?.result?.collection ? {
+            result: response.result.collection,
+          } : {},
+        };
+      },
     },
   );
   logDeep(response);
   return response;
 };
 
-const shopifyCollectionCreateApi = funcApi(shopifyCollectionCreate, {
-  argNames: ['credsPath', 'pageInput', 'options'],
-});
+const shopifyCollectionCreateApi = async (req, res) => {
+  const {
+    credsPath,
+    collectionInput,
+    options,
+  } = req.body;
+
+  const paramsValid = await Promise.all([
+    mandateParam(res, 'credsPath', credsPath),
+    mandateParam(res, 'collectionInput', collectionInput),
+  ]);
+  if (paramsValid.some(valid => valid === false)) {
+    return;
+  }
+
+  const result = await shopifyCollectionCreate(
+    credsPath,
+    collectionInput,
+    options,
+  );
+  respond(res, 200, result);
+};
 
 module.exports = {
   shopifyCollectionCreate,
   shopifyCollectionCreateApi,
 };
 
-// curl http://localhost:8000/shopifyCollectionCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "pageInput": { "title": "Batarang Blueprints", "body": "<strong>Good page!</strong>" }, "options": { "returnAttrs": "id" } }'
+// curl http://localhost:8000/shopifyCollectionCreate -H 'Content-Type: application/json' -d '{ "credsPath": "au", "collectionInput": { "title": "Summer Essentials" }, "options": { "returnAttrs": "id title handle" } }'
+/* curl http://localhost:8000/shopifyCollectionCreate -H 'Content-Type: application/json' -d
+'{
+  "credsPath": "au",
+  "collectionInput": {
+    "title": "Best Sellers",
+    "descriptionHtml": "Best Sellers from July 2026",
+    "ruleSet": {
+      "appliedDisjunctively": false,
+      "rules": [
+        { "column": "TAG",
+          "relation": "EQUALS",
+          "condition": "best-sellers"
+        },
+      ],
+    },
+    "sortOrder": "BEST_SELLING"
+  }
+}'
+*/
