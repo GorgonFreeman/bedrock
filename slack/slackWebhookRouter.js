490942fca48b5af1a46684c35f0c798705381aa7
Zwe <zwe@whitefoxboutique.com>
2026-07-21 14:34:46 +1000

    add smart collection create endpoint in slackwebhookrouter

--- slack/slackWebhookRouter.js
+++ slack/slackWebhookRouter.js
@@ -17,6 +17,7 @@ const commandNameToFunc = {
   'order_sync_check': require('../slack/slackInteractiveOrderSyncCheck'),
   'shipping_rates_disabled_report': require('../slack/slackInteractiveShippingRatesDisabledReport'),
   'dev_task_form': require('../slack/slackInteractiveLinearTaskCreate'),
+  'smart_collection_create': require('../slack/slackInteractiveShopifySmartCollectionCreate'),
 };
 
 const slackWebhookRouterApi = async (req, res) => {
